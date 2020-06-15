aaf3ae141139829d3cf2e3578991388ccec75e5e
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2020-06-15 17:13:45 +0100

    [ADD] helpdesk

---
+++ mcm_helpdesk/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'MCM Helpdesk',
+    'description': " Personaliser helpdesk ",
+    'author': "Houssem",
+    'maintainer': 'DIGIMOOV',
+    'category': 'Helpdesk',
+    'sequence': 15,
+    'summary': 'Helpdesk',
+    'depends': [
+        'website_helpdesk',
+        'helpdesk',
+    ],
+    'description': "Personaliser helpdesk",
+    'data': [
+        'views/helpdesk_template.xml',
+        'views/helpdesk_team.xml',
+    ],
+    'qweb': [],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
