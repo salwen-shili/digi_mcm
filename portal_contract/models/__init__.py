a387bffb6c7b72f66ae23b9b5a66532c542fc9c4
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2020-06-24 11:02:57 +0100

    [FIX] stock dependency

--- portal_contract/models/__init__.py
+++ portal_contract/models/__init__.py
@@ -1,5 +1,4 @@
 from . import res_company
-from . import product_template_details
 from . import product_template
 from . import action_formation
 from . import res_partner
--- portal_contract/models/product_template.py
+++ portal_contract/models/product_template.py
@@ -6,14 +6,5 @@ from odoo import api, fields, models
 class Session(models.Model):
     _inherit = 'product.template'
 
-    product_details_ids=fields.One2many('product.template.details','product_tmpl_id','Déscription')
     formation_price=fields.Monetary('Prix Formation',compute='_compute_price_formation',store=True)
 
-
-    def _compute_price_formation(self):
-        for rec in self:
-            if rec.product_details_ids:
-                total_price = 0
-                for product in rec.product_details_ids:
-                    total_price=total_price+product.price
-                rec.formation_price=total_price
