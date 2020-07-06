8e29795c83c65fc2e3f341584c7366b74c6bb7cd
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2020-07-06 10:33:21 +0100

    [FIX] calendly token

--- calendly_api_integration/controllers/main.py
+++ calendly_api_integration/controllers/main.py
@@ -15,18 +15,16 @@ class CalendlyController(http.Controller):
 
         url = "http://calendly.com/oauth/token"
 
-        data = {'client_id': '30619c87f05a76ccd4f44ee59d4281a3714395299a4b277492c7f0d149e09462',
-                'client_secret': '86bd046a39760594cc2fdd7036571842db01ac3122c4e4df8d6b4e43933ea2ba',
+        data = {'client_id': '90d14d3507c35d579924cf87a6de7fbc560211289ee1812e554c683c346f3a9c',
+                'client_secret': 'b863ae64bfa9ebd0d8aa8010028f3bf1041e1f678b682b710d4dec928d0fcca8',
                 'grant_type': 'authorization_code',
                 'code': code,
                 'redirect_uri': 'https://www.mcm-academy.fr/'}
 
         result1 = requests.post(url, data=data, headers=headers, timeout=60)
-
-        response = werkzeug.utils.unescape(result1.content.decode())
-        print(str(response))
-        json_data = json.loads(result1.text)
+        resp=json.loads(result1.content)
+        access_token = resp.get('access_token')
         calendly = request.env['calendly.api'].sudo().search([])
-        if "access_token" in json_data:
-            calendly.access_token=json_data['access_token']
-            calendly.refresh_token=json_data['refresh_token']
+        if access_token:
+            calendly.access_token = resp.get('access_token')
+            calendly.refresh_token = resp.get('refresh_token')
