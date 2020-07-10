06f41160633dca13cf8830168aa4a53053f38414
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2020-07-10 10:31:52 +0100

    [ADD] cpf informations to partner

---
+++ mcm_cpf_partner/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Partner CPF Informations',
+    'description': " Rajout des champs ",
+    'author': "Houssem",
+    'maintainer': 'DIGIMOOV',
+    'category': 'Partner',
+    'sequence': 15,
+    'summary': 'Partner',
+    'depends': [
+        'base',
+        'mcm_session',
+    ],
+    'description': "Rajout des champs",
+    'data': [
+        'views/partner.xml',
+    ],
+    'qweb': [],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ mcm_cpf_partner/models/partner.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+
+class resComapny(models.Model):
+    _inherit = "res.partner"
+
+    numero_cpf = fields.Char('Numéro CPF')
+    statut_cpf = fields.Selection(selection=[
+        ('untreated', 'Non Traité'),
+        ('validated', 'Validé'),
+        ('accepted', 'Accepté'),
+        ('in_training', 'En Formation'),
+        ('out_training', 'Sortie de Formation'),
+        ('service_declared', 'Service Fait Declaré'),
+        ('service_validated', 'Service Fait Validé'),
+        ('bill', 'Facturé'),
+    ], string='Statut CPF')
