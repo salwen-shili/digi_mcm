2b686a37b596b8973e631f588926be5f4e109214
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2020-10-14 13:28:35 +0100

    [FIX] check module ids

--- mcm_cpf_validation/models/session.py
+++ mcm_cpf_validation/models/session.py
@@ -8,13 +8,14 @@ class Session(models.Model):
 
     id_edof=fields.Char("ID Sesssion EDOF")
 
+    #compute available places for every module in session
     def write(self,values):
         session=super(Session,self).write(values)
-        print('write session')
-        for module in self.module_ids:
-            count=0
-            for partner in self.client_ids:
-                if partner.module_id==module and partner.statut=='won':
-                    count+=1
-            module.number_places_available=module.max_number_places-count
+        if self.module_ids:
+            for module in self.module_ids:
+                count=0
+                for partner in self.client_ids:
+                    if partner.module_id==module and partner.statut=='won':
+                        count+=1
+                module.number_places_available=module.max_number_places-count
         return session
