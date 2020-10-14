3366a4b466b70cbb74d5c506b6dfa89f93d44560
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2020-10-14 13:24:57 +0100

    [FIX] compute available places

--- mcm_cpf_validation/models/module.py
+++ mcm_cpf_validation/models/module.py
@@ -12,39 +12,28 @@ class Module(models.Model):
     published=fields.Boolean('publié',default=False)
     next_module=fields.Many2one('mcmacademy.module','Module suivant')
 
-    @api.onchange('max_number_places','session_id.client_ids')
+    @api.depends('max_number_places', 'session_id.client_ids')
     def get_number_places_available(self):
         for rec in self:
             count = 0
             if rec.max_number_places and rec.session_id:
                 for client in rec.session_id.client_ids:
-                    if client.module_id.id==rec.id and client.statut=='won':
-                        count+=1
-            rec.number_places_available=rec.max_number_places-count
-            # if rec.number_places_available ==3:
-            #     vals = {
-            #         'partner_email': '',
-            #         'partner_id': False,
-            #         'description': 'Bonjour /n Le nombre des places du module %s de la session %s est assez petit /n  Veuillez créer une nouvelle session ' % (rec.name,rec.session_id.name),
-            #         'name': '%s : Nombre des places assez petit pour  %s ' % (rec.session_id.name,rec.name),
-            #         'team_id': self.env['helpdesk.team'].sudo().search([('name', 'like', 'Clientèle')],
-            #                                                               limit=1).id,
-            #     }
-            #     new_ticket = self.env['helpdesk.ticket'].sudo().create(
-            #         vals)
+                    if client.module_id.id == rec.id and client.statut == 'won':
+                        count += 1
+            rec.number_places_available = rec.max_number_places - count
             if rec.number_places_available <= 3:
-                rec.published=True
+                rec.published = True
             else:
                 rec.published = False
-            if rec.number_places_available <= 0 :
-                rec.website_published=False
+            if rec.number_places_available <= 0:
+                rec.website_published = False
                 if rec.next_module:
-                    session=rec.session_id
+                    session = rec.session_id
                     for prospect in session.prospect_ids:
-                            prospect.module_id=rec.next_module
-                            prospect.mcm_session_id=rec.next_module.session_id
+                        prospect.module_id = rec.next_module
+                        prospect.mcm_session_id = rec.next_module.session_id
             else:
-                rec.website_published=True
+                rec.website_published = True
             return rec.number_places_available
     def write(self,values):
         module=super(Module,self).write(values)
--- mcm_cpf_validation/models/session.py
+++ mcm_cpf_validation/models/session.py
@@ -3,7 +3,18 @@
 
 from odoo import fields, models
 
-class resComapny(models.Model):
+class Session(models.Model):
     _inherit = "mcmacademy.session"
 
     id_edof=fields.Char("ID Sesssion EDOF")
+
+    def write(self,values):
+        session=super(Session,self).write(values)
+        print('write session')
+        for module in self.module_ids:
+            count=0
+            for partner in self.client_ids:
+                if partner.module_id==module and partner.statut=='won':
+                    count+=1
+            module.number_places_available=module.max_number_places-count
+        return session
