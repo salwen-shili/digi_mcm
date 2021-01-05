2b9555ad3e84950210594dc88fb5099d9c81b8e2
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2021-01-05 14:13:01 +0100

    [UPD] portal tasks

--- mcm_website_theme/controllers/main.py
+++ mcm_website_theme/controllers/main.py
@@ -8,6 +8,11 @@ from odoo.addons.portal.controllers.web import Home
 from werkzeug.exceptions import Forbidden, NotFound
 from odoo.addons.website.models.ir_http import sitemap_qs2dom
 from odoo.addons.payment.controllers.portal import PaymentProcessing
+from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager
+from odoo.tools import groupby as groupbyelem
+from odoo.osv.expression import OR
+from collections import OrderedDict
+from operator import itemgetter
 from odoo.exceptions import ValidationError
 from odoo import fields, http, SUPERUSER_ID, tools, _
 from odoo.osv import expression
@@ -534,3 +539,121 @@ class Payment3x(http.Controller):
             order.partner_id.mode_de_financement='cpf'
             order.partner_id.statut_cpf='untreated'
         return True
+
+class CustomerPortal(CustomerPortal):
+    @http.route(['/my/tasks', '/my/tasks/page/<int:page>'], type='http', auth="user", website=True)
+    def portal_my_tasks(self, page=1, date_begin=None, date_end=None, sortby=None, filterby=None, search=None,
+                        search_in='content', groupby='project', **kw):
+        values = self._prepare_portal_layout_values()
+        searchbar_sortings = {
+            'date': {'label': _('Newest'), 'order': 'create_date desc'},
+            'name': {'label': _('Title'), 'order': 'name'},
+            'stage': {'label': _('Stage'), 'order': 'stage_id'},
+            'update': {'label': _('Last Stage Update'), 'order': 'date_last_stage_update desc'},
+        }
+        searchbar_filters = {
+            'all': {'label': _('All'), 'domain': []},
+        }
+        searchbar_inputs = {
+            'content': {'input': 'content', 'label': _('Search <span class="nolabel"> (in Content)</span>')},
+            'message': {'input': 'message', 'label': _('Search in Messages')},
+            'customer': {'input': 'customer', 'label': _('Search in Customer')},
+            'stage': {'input': 'stage', 'label': _('Search in Stages')},
+            'all': {'input': 'all', 'label': _('Search in All')},
+        }
+        searchbar_groupby = {
+            'none': {'input': 'none', 'label': _('None')},
+            'project': {'input': 'project', 'label': _('Project')},
+        }
+
+        # extends filterby criteria with project the customer has access to
+        projects = request.env['project.project'].search([])
+        for project in projects:
+            searchbar_filters.update({
+                str(project.id): {'label': project.name, 'domain': [('project_id', '=', project.id)]}
+            })
+
+        # extends filterby criteria with project (criteria name is the project id)
+        # Note: portal users can't view projects they don't follow
+        project_groups = request.env['project.task'].read_group([('project_id', 'not in', projects.ids)],
+                                                                ['project_id'], ['project_id'])
+        for group in project_groups:
+            proj_id = group['project_id'][0] if group['project_id'] else False
+            proj_name = group['project_id'][1] if group['project_id'] else _('Others')
+            searchbar_filters.update({
+                str(proj_id): {'label': proj_name, 'domain': [('project_id', '=', proj_id)]}
+            })
+
+        # default sort by value
+        if not sortby:
+            sortby = 'date'
+        order = searchbar_sortings[sortby]['order']
+        # default filter by value
+        if not filterby:
+            filterby = 'all'
+        domain = searchbar_filters[filterby]['domain']
+
+        # archive groups - Default Group By 'create_date'
+        archive_groups = self._get_archive_groups('project.task', domain)
+        if date_begin and date_end:
+            domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
+
+        # search
+        if search and search_in:
+            search_domain = []
+            if search_in in ('content', 'all'):
+                search_domain = OR([search_domain, ['|', ('name', 'ilike', search), ('description', 'ilike', search)]])
+            if search_in in ('customer', 'all'):
+                search_domain = OR([search_domain, [('partner_id', 'ilike', search)]])
+            if search_in in ('message', 'all'):
+                search_domain = OR([search_domain, [('message_ids.body', 'ilike', search)]])
+            if search_in in ('stage', 'all'):
+                search_domain = OR([search_domain, [('stage_id', 'ilike', search)]])
+            domain += search_domain
+
+        # task count
+        task_count = request.env['project.task'].search_count(domain)
+        # pager
+        pager = portal_pager(
+            url="/my/tasks",
+            url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby, 'filterby': filterby,
+                      'search_in': search_in, 'search': search},
+            total=task_count,
+            page=page,
+            step=self._items_per_page
+        )
+        # content according to pager and archive selected
+        if groupby == 'project':
+            order = "project_id, %s" % order  # force sort on project first to group by project in view
+        tasks = request.env['project.task'].search(domain, order=order, limit=self._items_per_page,
+                                                   offset=(page - 1) * self._items_per_page)
+        request.session['my_tasks_history'] = tasks.ids[:100]
+        if groupby == 'project':
+            grouped_tasks = [request.env['project.task'].concat(*g) for k, g in
+                             groupbyelem(tasks, itemgetter('project_id'))]
+        else:
+            grouped_tasks = [tasks]
+        my_tasks=[]
+        user=request.env.user
+        for task in grouped_tasks:
+            if task.user_id==user.id or task.partner_id==user.partner_id.id:
+                my_tasks.append(task)
+
+        values.update({
+            'date': date_begin,
+            'date_end': date_end,
+            'grouped_tasks': my_tasks,
+            'page_name': 'task',
+            'archive_groups': archive_groups,
+            'default_url': '/my/tasks',
+            'pager': pager,
+            'searchbar_sortings': searchbar_sortings,
+            'searchbar_groupby': searchbar_groupby,
+            'searchbar_inputs': searchbar_inputs,
+            'search_in': search_in,
+            'sortby': sortby,
+            'groupby': groupby,
+            'searchbar_filters': OrderedDict(sorted(searchbar_filters.items())),
+            'filterby': filterby,
+        })
+        return request.render("project.portal_my_tasks", values)
