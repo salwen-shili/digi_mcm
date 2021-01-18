4240053a5ec905298e74d4d5210db01eff09539e
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2021-01-18 14:05:18 +0100

    [UPD] shop update

--- mcm_partner_department/controllers/main.py
+++ mcm_partner_department/controllers/main.py
@@ -19,9 +19,8 @@ PPR = 4  # Products Per Row
 
 class WebsiteSale(WebsiteSale):
 
-
     @http.route(['/shop/cart/update'], type='http', auth="public", methods=['GET', 'POST'], website=True, csrf=False)
-    def cart_update(self, product_id,module='',departement='', add_qty=1, set_qty=0, **kw):
+    def cart_update(self, product_id, module='', departement='', add_qty=1, set_qty=0, promo=None, **kw):
         """This route is called when adding a product to cart (no options)."""
         error_message = ''
         sale_order = request.website.sale_get_order(force_create=True)
@@ -38,7 +37,9 @@ class WebsiteSale(WebsiteSale):
         no_variant_attribute_values = None
         if kw.get('no_variant_attribute_values'):
             no_variant_attribute_values = json.loads(kw.get('no_variant_attribute_values'))
-
+        print('promo 1')
+        if promo:
+            print(promo)
         sale_order._cart_update(
             product_id=int(product_id),
             add_qty=1,
@@ -46,33 +47,36 @@ class WebsiteSale(WebsiteSale):
             product_custom_attribute_values=product_custom_attribute_values,
             no_variant_attribute_values=no_variant_attribute_values
         )
-        if(sale_order.partner_id.customer_rank==0):
-            sale_order.partner_id.customer_rank=1
+        if promo:
+            pricelist = request.env['product.pricelist'].sudo().search(
+                [('company_id', '=', 2), ('id', "=", promo)])
+            if pricelist:
+                request.website.sale_get_order(code=pricelist.code)
+        else:
+            request.website.sale_get_order(code='')
+        if (sale_order.partner_id.customer_rank == 0):
+            sale_order.partner_id.customer_rank = 1
         if product_id:
-            if module=='all':
+            if module == 'all':
                 product = request.env['product.template'].sudo().search(
                     [('id', '=', product_id)])
                 error_session = "error"
                 url = "/shop/product/" + str(slug(product))
                 values = self._prepare_product_values(product, category='', search='', **kw)
                 values['error_session'] = "error"
-                product = product_id
-                list = []
-                sale_order.write({'order_line': [(6, 0, list)]})
-                product_id = product
                 return request.render("website_sale.product", values)
 
-            if module != ''  and module!='all':
+            if module != '' and module != 'all':
                 module = request.env['mcmacademy.module'].sudo().search(
                     [('id', '=', module)])
-                session=module.session_id
+                session = module.session_id
                 if session:
-                    sale_order.session_id=session
+                    sale_order.session_id = session
                     sale_order.partner_id.mcm_session_id = session
                 if module:
-                    sale_order.module_id=module
+                    sale_order.module_id = module
                     sale_order.partner_id.module_id = module
-                list=[]
+                list = []
                 check_portal = False
                 if sale_order.partner_id.user_ids:
                     for user in sale_order.partner_id.user_ids:
@@ -85,31 +89,27 @@ class WebsiteSale(WebsiteSale):
                         list.append(partner.id)
                     list.append(sale_order.partner_id.id)
                     session.write({'panier_perdu_ids': [(6, 0, list)]})
-                    sale_order.partner_id.statut='panier_perdu'
-                    sale_order.partner_id.module_id=module
-                    sale_order.partner_id.mcm_session_id=session
+                    sale_order.partner_id.statut = 'panier_perdu'
+                    sale_order.partner_id.module_id = module
+                    sale_order.partner_id.mcm_session_id = session
         if product_id:
             if departement != 'all':
                 state = request.env['res.country.state'].sudo().search(
-                ['&',('code', "=" , str(departement)),('country_id.code', 'ilike', 'FR')])
+                    ['&', ('code', "=", str(departement)), ('country_id.code', 'ilike', 'FR')])
                 if state:
-                    sale_order.partner_id.state_id=state
-                if departement=='59':
-                    sale_order.partner_id.partner_departement='59000'
-                if departement=='62':
-                    sale_order.partner_id.partner_departement='62000'
+                    sale_order.partner_id.state_id = state
+                if departement == '59':
+                    sale_order.partner_id.partner_departement = '59000'
+                if departement == '62':
+                    sale_order.partner_id.partner_departement = '62000'
             else:
                 print('test false')
                 product = request.env['product.template'].sudo().search(
                     [('id', '=', product_id)])
                 error_message = "error"
-                url="/shop/product/"+str(slug(product))
-                values=self._prepare_product_values(product, category='', search='', **kw)
-                values['error_department']="error"
-                product = product_id
-                list = []
-                sale_order.write({'order_line': [(6, 0, list)]})
-                product_id = product
+                url = "/shop/product/" + str(slug(product))
+                values = self._prepare_product_values(product, category='', search='', **kw)
+                values['error_department'] = "error"
                 return request.render("website_sale.product", values)
 
         if kw.get('express'):
