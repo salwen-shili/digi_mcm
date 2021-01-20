a4077a3d62d76b7e070d7ca9695e3355b1a1ead3
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2021-01-20 10:53:46 +0100

    [UPD] sitemap

--- mcm_add_fields/models/account_move.py
+++ mcm_add_fields/models/account_move.py
@@ -32,4 +32,4 @@ class AccountMove(models.Model):
     def unlink(self):
         for move in self:
             move.line_ids.unlink()
-        # return super(AccountMove, self).unlink()
+        move.unlink()
