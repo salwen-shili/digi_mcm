0a49c29f2dca43dd878e29cb0f23da804ebee878
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2021-02-03 17:32:00 +0100

    [UPD] cpf mcm validation

--- mcm_cpf_validation/controllers/main.py
+++ mcm_cpf_validation/controllers/main.py
@@ -187,7 +187,6 @@ class ClientCPFController(http.Controller):
                 request.env.user.company_id = 1
                 order = module_id = request.env['sale.order'].sudo().search(
                     [('module_id', "=", module_id.id), ('state', 'in', ('sent', 'sale'))])
-                
                 if not order:
                     so = request.env['sale.order'].sudo().create({
                         'partner_id': user.partner_id.id,
