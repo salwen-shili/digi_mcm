a54ce9725b9128aa004b3d18b5025b3f7825fa29
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2021-02-09 17:05:23 +0100

    [UPD] shop cart url

--- digimoov_sessions_modules/controllers/main.py
+++ digimoov_sessions_modules/controllers/main.py
@@ -15,29 +15,99 @@ PPR = 4  # Products Per Row
 
 class WebsiteSale(WebsiteSale):
 
-    @http.route(['/shop/cart'], type='http', auth="public", website=True, sitemap=False)
-    def cart(self, access_token=None, revive='', **post):
+    @http.route(
+        ['''/<string:product>/<string:partenaire>/shop/cart''', '''/<string:product>/shop/cart''', '''/shop/cart'''],
+        type='http', auth="user", website=True, sitemap=False)
+    def cart(self, access_token=None, product=None, revive='', partenaire=None, **post):
         """
         Main cart management + abandoned cart revival
         access_token: Abandoned cart SO access token
         revive: Revival method when abandoned cart. Can be 'merge' or 'squash'
         """
         order = request.website.sale_get_order()
+        print(order)
+        if order.company_id.id == 1 and (partenaire or product):
+            return request.redirect("/shop/cart/")
+        if order and order.company_id.id == 2:
+            request.env.user.company_id = 2  # change default company
+            request.env.user.company_ids = [2]  # change default companies
+            product_id = False
+            if order:
+                for line in order.order_line:
+                    product_id = line.product_id
+
+            if not product and not partenaire and product_id:
+                product = True
+                partenaire = True
+            if product and not partenaire:
+                if product_id:
+                    slugname = (product_id.name).strip().strip('-').replace(' ', '-').lower()
+                    if str(slugname) != str(product):
+                        if order.pricelist_id and order.pricelist_id.name in ['ubereats', 'deliveroo', 'coursierjob']:
+                            return request.redirect("/%s/%s/shop/cart/" % (slugname, order.pricelist_id.name))
+                        else:
+                            return request.redirect("/%s/shop/cart/" % (slugname))
+                    else:
+                        if order.pricelist_id and order.pricelist_id.name in ['ubereats', 'deliveroo', 'coursierjob']:
+                            return request.redirect("/%s/%s/shop/cart/" % (slugname, order.pricelist_id.name))
+                else:
+                    return request.redirect("/pricing")
+            elif product and partenaire:
+                if product_id:
+                    slugname = (product_id.name).strip().strip('-').replace(' ', '-').lower()
+                    if str(slugname) != str(product):
+                        pricelist = request.env['product.pricelist'].sudo().search(
+                            [('company_id', '=', 2), ('name', "=", str(partenaire))])
+                        if not pricelist:
+                            pricelist_id = order.pricelist_id
+                            if pricelist_id.name in ['ubereats', 'deliveroo', 'coursierjob']:
+                                return request.redirect("/%s/%s/shop/cart/" % (slugname, pricelist_id.name))
+                            else:
+                                return request.redirect("/%s/shop/cart/" % (slugname))
+                        else:
+                            if pricelist.name in ['ubereats', 'deliveroo', 'coursierjob']:
+                                return request.redirect("/%s/%s/shop/cart/" % (slugname, order.pricelist_id.name))
+                            else:
+                                return request.redirect("/%s/shop/cart/" % (slugname))
+                    else:
+                        pricelist = request.env['product.pricelist'].sudo().search(
+                            [('company_id', '=', 2), ('name', "=", str(partenaire))])
+
+                        if not pricelist:
+                            pricelist_id = order.pricelist_id
+                            if pricelist_id.name in ['ubereats', 'deliveroo', 'coursierjob']:
+                                return request.redirect("/%s/%s/shop/cart/" % (slugname, pricelist_id.name))
+                            else:
+                                return request.redirect("/%s/shop/cart/" % (slugname))
+                        else:
+                            if pricelist.name in ['ubereats', 'deliveroo', 'coursierjob']:
+                                if pricelist.name != order.pricelist_id.name:
+                                    return request.redirect("/%s/%s/shop/cart/" % (slugname, order.pricelist_id.name))
+                            else:
+                                return request.redirect("/%s/shop/cart/" % (slugname))
+                else:
+                    pricelist = request.env['product.pricelist'].sudo().search(
+                        [('company_id', '=', 2), ('name', "=", str(partenaire))])
+                    if pricelist and pricelist.name in ['ubereats', 'deliveroo', 'coursierjob']:
+                        return request.redirect("/%s" % (pricelist.name))
+                    else:
+                        return request.redirect("/pricing")
         list_products = []
         if order:
             for line in order.order_line:
                 list_products.append(line.product_id)
-        all_digimoov_modules=False
+        all_digimoov_modules = False
         for product in list_products:
             all_digimoov_modules = request.env['mcmacademy.module'].sudo().search(
                 [('product_id', '=', product.product_tmpl_id.id),
                  ('company_id', '=', 2)])
         list_modules_digimoov = []
         today = date.today()
-        if(all_digimoov_modules):
+        if (all_digimoov_modules):
             for module in all_digimoov_modules:
                 if module.date_exam:
-                    if (module.date_exam - today).days > int(module.session_id.intervalle_jours) and module.session_id.website_published==True:
+                    if (module.date_exam - today).days > int(
+                            module.session_id.intervalle_jours) and module.session_id.website_published == True:
                         list_modules_digimoov.append(module)
         if order and order.state != 'draft':
             request.session['sale_order_id'] = None
@@ -49,13 +119,15 @@ class WebsiteSale(WebsiteSale):
                 raise NotFound()
             if abandoned_order.state != 'draft':  # abandoned cart already finished
                 values.update({'abandoned_proceed': True})
-            elif revive == 'squash' or (revive == 'merge' and not request.session.get('sale_order_id')):  # restore old cart or merge with unexistant
+            elif revive == 'squash' or (revive == 'merge' and not request.session.get(
+                    'sale_order_id')):  # restore old cart or merge with unexistant
                 request.session['sale_order_id'] = abandoned_order.id
                 return request.redirect('/shop/cart')
             elif revive == 'merge':
                 abandoned_order.order_line.write({'order_id': request.session['sale_order_id']})
                 abandoned_order.action_cancel()
-            elif abandoned_order.id != request.session.get('sale_order_id'):  # abandoned cart found, user have to choose what to do
+            elif abandoned_order.id != request.session.get(
+                    'sale_order_id'):  # abandoned cart found, user have to choose what to do
                 values.update({'access_token': abandoned_order.access_token})
         values.update({
             'website_sale_order': order,
@@ -72,10 +144,10 @@ class WebsiteSale(WebsiteSale):
             print(module.ville)
             print(module.date_exam)
         values.update({
-            'modules_digimoov':list_modules_digimoov,
-            'error_ville':'',
-            'error_exam_date':'',
-            'error_condition':'',
+            'modules_digimoov': list_modules_digimoov,
+            'error_ville': '',
+            'error_exam_date': '',
+            'error_condition': '',
         })
         if post.get('type') == 'popover':
             # force no-cache so IE11 doesn't cache this XHR
