e496a93c3cc6d36f68660b1d4ebf803865aba364
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2021-02-15 16:43:48 +0100

    [UPD] update sign document title & meta

--- digimoov_sessions_modules/__manifest__.py
+++ digimoov_sessions_modules/__manifest__.py
@@ -30,6 +30,7 @@
         'views/sale_order_report.xml',
         'views/sign_request.xml',
         'views/sign_template.xml',
+        'views/document_sign.xml',
         # 'data/sign_data.xml',
     ],
     'qweb': [],
