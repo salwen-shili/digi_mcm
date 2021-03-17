b1da00a6a1c6de809b0f1882904b0518cd25f4bb
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2021-03-17 16:16:06 +0100

    [UPD] change sign text portal contract

--- hide_auto_sign_portal/static/src/js/portal.js
+++ hide_auto_sign_portal/static/src/js/portal.js
@@ -15,13 +15,13 @@ publicWidget.registry.SignTemplate = publicWidget.Widget.extend({
          var self = this;
          this.$('a.o_web_sign_auto_button').css('display','none');
          this.$('a.o_web_sign_load_button').css('display','none');
-         this.$('a.o_web_sign_draw_button').html('Signature');
+         this.$('a.o_web_sign_draw_button').html('Dessiner votre signature');
     },
     hide_auto_sign: function (ev) {
          var self = this;
          this.$('a.o_web_sign_auto_button').css('display','none');
          this.$('a.o_web_sign_load_button').css('display','none');
-         this.$('a.o_web_sign_draw_button').html('Signature');
+         this.$('a.o_web_sign_draw_button').html('Dessiner votre signature');
     },
     });
 });
