d963e9764356874ccc001e806024351078649f5d
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2021-04-12 17:04:46 +0100

    [UPD] hide phone & first name in reset password view

--- mcm_auth_signup/models/partner.py
+++ mcm_auth_signup/models/partner.py
@@ -7,3 +7,12 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     firstname=fields.Char('Prénom')
+
+    @api.model
+    def signup_retrieve_info(self, token):
+        res = super(ResPartner, self).signup_retrieve_info(token)
+        partner = self._signup_retrieve_partner(token, raise_exception=True)
+        if partner.signup_valid:
+            res['firstname'] = partner.firstname
+            res['phone'] = partner.phone
+        return res
