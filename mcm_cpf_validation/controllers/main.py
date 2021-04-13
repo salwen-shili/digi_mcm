f4965b9b5f1cf497a2e073a9ac3e77d4f3790662
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2021-04-13 14:55:35 +0100

    [UPD] fix cpf_accepted

--- mcm_cpf_validation/controllers/main.py
+++ mcm_cpf_validation/controllers/main.py
@@ -57,10 +57,10 @@ class ClientCPFController(http.Controller):
         user=False
         if len(users) > 1 :
             for utilisateur in users:
-                if utilisateur.partner_id.id_edof and utilisateur.partner_id.date_exam_edof and utilisateur.partner_id.ville:
+                if utilisateur.partner_id.id_edof and utilisateur.partner_id.date_examen_edof and utilisateur.partner_id.ville:
                     user=utilisateur
                 else:
-                    user=users[1]
+                    user=utilisateur
         if user:
             user.partner_id.mode_de_financement = 'cpf'
             user.partner_id.statut_cpf = 'accepted'
