a8d20a772e99387e241cd86c74533dc7216240e2
digimoovtakwa <tmejri@digimoov.fr>
2021-06-16 15:04:04 +0200

    [UPD] Ajouter field region dans l'interface de company son valeur sera afficher dans l'attestation de suivi de formation.

--- partner_exam/models/add_field_company.py
+++ partner_exam/models/add_field_company.py
@@ -6,3 +6,5 @@ class InheritResCompany(models.Model):
     _description = "Ajouter une signature dans l'interface de la société"
 
     signature_convocation = fields.Binary('Signature', store=True)
+    region = fields.Char(default="Ile de France", string="Par le préfet de la région")
+
