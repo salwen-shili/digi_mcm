3e3527451ee2d6922ae460dde6c52c01c2ab50a1
digimoovtakwa <tmejri@digimoov.fr>
2021-06-29 14:30:29 +0200

    [ADD] Ajouter liste des villes dans l'interface fiche notes d'examen.

--- session_ville/models/__init__.py
+++ session_ville/models/__init__.py
@@ -1,3 +1,4 @@
 from . import session_ville
 from . import adresse_centre_examen
+from . import inherit_info_examen
 
--- session_ville/models/inherit_info_examen.py
+++ session_ville/models/inherit_info_examen.py
@@ -3,6 +3,6 @@ from odoo import api, fields, models
 
 class InheritInfoExamen(models.Model):
     _inherit = "info.examen"
-    _description = "Inherit this infi.examen to add list of villes"
+    _description = "Inherit this info.examen to add list of villes"
 
     ville_id = fields.Many2one('session.ville')
