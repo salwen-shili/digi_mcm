415267455f502cb01a19088f115451a004da160d
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2021-07-06 15:52:02 +0100

    [UPD] add intercom script in logged in user

--- digimoov_website_templates/static/src/js/intercom-user.js
+++ digimoov_website_templates/static/src/js/intercom-user.js
@@ -1,6 +1,9 @@
+var user_name=document.getElementById("intercom_user_name_connected").value;
+var user_email=document.getElementById("intercom_user_email_connected").value;
 window.intercomSettings = {
-  app_id: "waw4riyk",
-  name: "<%= current_user.name %>", // Full name
-  email: "<%= current_user.email %>", // Email address
-  created_at: "<%= current_user.created_at.to_i %>", // Signup date as a Unix timestamp
+    app_id: "waw4riyk",
+    name: user_name, // Full name
+    full_name: user_name, // Full name
+    email: user_email, // Email address
+    created_at: "<%= current_user.created_at.to_i %>" // Signup date as a Unix timestamp
 };
