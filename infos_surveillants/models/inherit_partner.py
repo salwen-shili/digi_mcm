7738eff1b98be812e180e1bcdde5371159343e5f
digimoovtakwa <tmejri@digimoov.fr>
2021-07-30 16:58:59 +0200

    [UPD] Add comments in supervisors module.

--- infos_surveillants/models/inherit_partner.py
+++ infos_surveillants/models/inherit_partner.py
@@ -5,4 +5,5 @@ class InheritResPartner(models.Model):
     _inherit = "res.partner"
     _description = "Les surveillants"
 
+    # This will be added in partner interface to filter if a partner is a supervisor or not.
     est_surveillant = fields.Boolean(default=False)
