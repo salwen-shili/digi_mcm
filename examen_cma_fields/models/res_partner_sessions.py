05b7a0053764b7b16c1c522cd18056881f7748b9
digimoovtakwa <tmejri@digimoov.fr>
2021-08-05 17:41:11 +0200

    [UPD] Add Menu in partner for historic session and action to delete duplicate records.

--- examen_cma_fields/models/res_partner_sessions.py
+++ examen_cma_fields/models/res_partner_sessions.py
@@ -18,3 +18,17 @@ class resPartnerSessions(models.Model):
         ('admitted', 'Admis'),
     ], string='Pratique')
     company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company)
+    
+    def remove_double_session_same_session(self):
+        """ Add this function to remove duplicate
+        sessions in partner interface"""
+        duplicate_sessions = []
+        for partner_sessions in self:
+            if partner_sessions.session_id.id and partner_sessions.id not in duplicate_sessions:
+                duplicates = self.env['partner.sessions'].search([('client_id', '=', partner_sessions.client_id.id), ('id', '!=', partner_sessions.id), ('session_id', '=', partner_sessions.session_id.id)])
+                print(duplicates)
+                for dup in duplicates:
+                    print("dup", dup)
+                    duplicate_sessions.append(dup.id)
+                    print("duplicate_contacts", duplicate_sessions)
+        self.browse(duplicate_sessions).unlink()
