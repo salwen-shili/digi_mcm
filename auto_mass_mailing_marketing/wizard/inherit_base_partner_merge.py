f580712a4de372def96eca203c810e300066f0a5
digimoovtakwa <tmejri@digimoov.fr>
2021-09-29 09:15:24 +0200

    [UPD] Add logger info for trace in logs _get_ordered_partner function.

--- auto_mass_mailing_marketing/wizard/inherit_base_partner_merge.py
+++ auto_mass_mailing_marketing/wizard/inherit_base_partner_merge.py
@@ -7,27 +7,29 @@ class BasePartnerMergeAutomaticWizard(models.TransientModel):
 
     @api.model
     def _get_ordered_partner(self, partner_ids):
-        """ Helper : returns a `res.partner` recordset ordered by create_date/active fields
-            :param partner_ids : list of partner ids to sort
+        """ inherit this function to add conditions based on status of partner to use automatic partner merge,
+        so the default field of dst_partner_id in wizard view will display as value from
+        the list with state "won" if not "won" will choose to display other status different to "won"
+        else if there is no state selected it will display the partner with no status
         """
         a = self.env['res.partner'].browse(partner_ids).sorted(
             key=lambda p: (p.active, (p.create_date or datetime.datetime(1970, 1, 1))),
             reverse=True,
         ).filtered(lambda r: r.statut == "won")
         if a:
-            print("a", a)
+            _logger.info("Duplicate partners a %s" % a)
             return a
         elif not a:
             b = self.env['res.partner'].browse(partner_ids).sorted(
                 key=lambda p: (p.active, (p.create_date or datetime.datetime(1970, 1, 1))),
                 reverse=True,
             ).filtered(lambda r: r.statut != "won")
-            print("b", b)
+            _logger.info("Duplicate partners b %s" % b)
             return b
         else:
             c = self.env['res.partner'].browse(partner_ids).sorted(
                 key=lambda p: (p.active, (p.create_date or datetime.datetime(1970, 1, 1))),
                 reverse=True,
             ).filtered(lambda r: r.statut == None)
-            print("c", c)
+            _logger.info("Duplicate partners c %s" % c)
             return c
