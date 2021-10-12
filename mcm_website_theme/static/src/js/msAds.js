379d7f0b6920ab89237f728c0f8285115ce3f5e7
Salwen sh <salwen-sh@gmail.com>
2021-10-12 16:59:07 +0100

    [ADD][MCM] Add microsoft ads on taxi, vtc, vmdtr

--- mcm_website_theme/static/src/js/msAds.js
+++ mcm_website_theme/static/src/js/msAds.js
@@ -95,3 +95,294 @@ function packVmdtrHome() {
   });
   console.log("after script");
 }
+
+//page formation taxi
+function packVtcPageFormationTaxi() {
+  (function (w, d, t, r, u) {
+    var f, n, i;
+    (w[u] = w[u] || []),
+      (f = function () {
+        var o = {
+          ti: document.getElementById("microsoft_tracking_key").value,
+        };
+        (o.q = w[u]), (w[u] = new UET(o)), w[u].push("pageLoad");
+      }),
+      (n = d.createElement(t)),
+      (n.src = r),
+      (n.async = 1),
+      (n.onload = n.onreadystatechange =
+        function () {
+          var s = this.readyState;
+          (s && s !== "loaded" && s !== "complete") ||
+            (f(), (n.onload = n.onreadystatechange = null));
+        }),
+      (i = d.getElementsByTagName(t)[0]),
+      i.parentNode.insertBefore(n, i);
+  })(window, document, "script", "//bat.bing.com/bat.js", "uetq");
+  console.log("inside script");
+  window.uetq = window.uetq || [];
+  window.uetq.push("event", "click sur s'inscrire pack vtc", {
+    event_category: "Pricing Table events",
+    event_label: "Inscrire pack vtc Page Formation Taxi",
+    event_value: "590",
+  });
+  console.log("after script");
+}
+
+function packTaxiPageFormationTaxi() {
+  (function (w, d, t, r, u) {
+    var f, n, i;
+    (w[u] = w[u] || []),
+      (f = function () {
+        var o = {
+          ti: document.getElementById("microsoft_tracking_key").value,
+        };
+        (o.q = w[u]), (w[u] = new UET(o)), w[u].push("pageLoad");
+      }),
+      (n = d.createElement(t)),
+      (n.src = r),
+      (n.async = 1),
+      (n.onload = n.onreadystatechange =
+        function () {
+          var s = this.readyState;
+          (s && s !== "loaded" && s !== "complete") ||
+            (f(), (n.onload = n.onreadystatechange = null));
+        }),
+      (i = d.getElementsByTagName(t)[0]),
+      i.parentNode.insertBefore(n, i);
+  })(window, document, "script", "//bat.bing.com/bat.js", "uetq");
+  console.log("inside script");
+  window.uetq = window.uetq || [];
+  window.uetq.push("event", "click sur s'inscrire pack taxi", {
+    event_category: "Pricing Table events",
+    event_label: "Inscrire pack taxi Page Formation Taxi",
+    event_value: "680",
+  });
+  console.log("after script");
+}
+
+function packVmdtrPageFormationTaxi() {
+  (function (w, d, t, r, u) {
+    var f, n, i;
+    (w[u] = w[u] || []),
+      (f = function () {
+        var o = {
+          ti: document.getElementById("microsoft_tracking_key").value,
+        };
+        (o.q = w[u]), (w[u] = new UET(o)), w[u].push("pageLoad");
+      }),
+      (n = d.createElement(t)),
+      (n.src = r),
+      (n.async = 1),
+      (n.onload = n.onreadystatechange =
+        function () {
+          var s = this.readyState;
+          (s && s !== "loaded" && s !== "complete") ||
+            (f(), (n.onload = n.onreadystatechange = null));
+        }),
+      (i = d.getElementsByTagName(t)[0]),
+      i.parentNode.insertBefore(n, i);
+  })(window, document, "script", "//bat.bing.com/bat.js", "uetq");
+  console.log("inside script");
+  window.uetq = window.uetq || [];
+  window.uetq.push("event", "click sur s'inscrire pack vmdtr", {
+    event_category: "Pricing Table events",
+    event_label: "Inscrire pack vmdtr Page Formation Taxi ",
+    event_value: "849",
+  });
+  console.log("after script");
+}
+
+//page formation VTC
+function packVtcPageFromationVtc() {
+  (function (w, d, t, r, u) {
+    var f, n, i;
+    (w[u] = w[u] || []),
+      (f = function () {
+        var o = {
+          ti: document.getElementById("microsoft_tracking_key").value,
+        };
+        (o.q = w[u]), (w[u] = new UET(o)), w[u].push("pageLoad");
+      }),
+      (n = d.createElement(t)),
+      (n.src = r),
+      (n.async = 1),
+      (n.onload = n.onreadystatechange =
+        function () {
+          var s = this.readyState;
+          (s && s !== "loaded" && s !== "complete") ||
+            (f(), (n.onload = n.onreadystatechange = null));
+        }),
+      (i = d.getElementsByTagName(t)[0]),
+      i.parentNode.insertBefore(n, i);
+  })(window, document, "script", "//bat.bing.com/bat.js", "uetq");
+  console.log("inside script");
+  window.uetq = window.uetq || [];
+  window.uetq.push("event", "click sur s'inscrire pack vtc", {
+    event_category: "Pricing Table events",
+    event_label: "Inscrire pack vtc Page Formation VTC",
+    event_value: "590",
+  });
+  console.log("after script");
+}
+
+function packTaxiPageFromationVtc() {
+  (function (w, d, t, r, u) {
+    var f, n, i;
+    (w[u] = w[u] || []),
+      (f = function () {
+        var o = {
+          ti: document.getElementById("microsoft_tracking_key").value,
+        };
+        (o.q = w[u]), (w[u] = new UET(o)), w[u].push("pageLoad");
+      }),
+      (n = d.createElement(t)),
+      (n.src = r),
+      (n.async = 1),
+      (n.onload = n.onreadystatechange =
+        function () {
+          var s = this.readyState;
+          (s && s !== "loaded" && s !== "complete") ||
+            (f(), (n.onload = n.onreadystatechange = null));
+        }),
+      (i = d.getElementsByTagName(t)[0]),
+      i.parentNode.insertBefore(n, i);
+  })(window, document, "script", "//bat.bing.com/bat.js", "uetq");
+  console.log("inside script");
+  window.uetq = window.uetq || [];
+  window.uetq.push("event", "click sur s'inscrire pack taxi", {
+    event_category: "Pricing Table events",
+    event_label: "Inscrire pack taxi Page Formation VTC",
+    event_value: "680",
+  });
+  console.log("after script");
+}
+
+function packVmdtrPageFromationVtc() {
+  (function (w, d, t, r, u) {
+    var f, n, i;
+    (w[u] = w[u] || []),
+      (f = function () {
+        var o = {
+          ti: document.getElementById("microsoft_tracking_key").value,
+        };
+        (o.q = w[u]), (w[u] = new UET(o)), w[u].push("pageLoad");
+      }),
+      (n = d.createElement(t)),
+      (n.src = r),
+      (n.async = 1),
+      (n.onload = n.onreadystatechange =
+        function () {
+          var s = this.readyState;
+          (s && s !== "loaded" && s !== "complete") ||
+            (f(), (n.onload = n.onreadystatechange = null));
+        }),
+      (i = d.getElementsByTagName(t)[0]),
+      i.parentNode.insertBefore(n, i);
+  })(window, document, "script", "//bat.bing.com/bat.js", "uetq");
+  console.log("inside script");
+  window.uetq = window.uetq || [];
+  window.uetq.push("event", "click sur s'inscrire pack vmdtr", {
+    event_category: "Pricing Table events",
+    event_label: "Inscrire pack vmdtr Page Formation VTC ",
+    event_value: "849",
+  });
+  console.log("after script");
+}
+
+//page formation TAXI MOTO
+function packVtcPageFormationTaxiMoto() {
+  (function (w, d, t, r, u) {
+    var f, n, i;
+    (w[u] = w[u] || []),
+      (f = function () {
+        var o = {
+          ti: document.getElementById("microsoft_tracking_key").value,
+        };
+        (o.q = w[u]), (w[u] = new UET(o)), w[u].push("pageLoad");
+      }),
+      (n = d.createElement(t)),
+      (n.src = r),
+      (n.async = 1),
+      (n.onload = n.onreadystatechange =
+        function () {
+          var s = this.readyState;
+          (s && s !== "loaded" && s !== "complete") ||
+            (f(), (n.onload = n.onreadystatechange = null));
+        }),
+      (i = d.getElementsByTagName(t)[0]),
+      i.parentNode.insertBefore(n, i);
+  })(window, document, "script", "//bat.bing.com/bat.js", "uetq");
+  console.log("inside script");
+  window.uetq = window.uetq || [];
+  window.uetq.push("event", "click sur s'inscrire pack vtc", {
+    event_category: "Pricing Table events",
+    event_label: "Inscrire pack vtc Page formation taxi moto",
+    event_value: "590",
+  });
+  console.log("after script");
+}
+
+function packTaxiPageFormationTaxiMoto() {
+  (function (w, d, t, r, u) {
+    var f, n, i;
+    (w[u] = w[u] || []),
+      (f = function () {
+        var o = {
+          ti: document.getElementById("microsoft_tracking_key").value,
+        };
+        (o.q = w[u]), (w[u] = new UET(o)), w[u].push("pageLoad");
+      }),
+      (n = d.createElement(t)),
+      (n.src = r),
+      (n.async = 1),
+      (n.onload = n.onreadystatechange =
+        function () {
+          var s = this.readyState;
+          (s && s !== "loaded" && s !== "complete") ||
+            (f(), (n.onload = n.onreadystatechange = null));
+        }),
+      (i = d.getElementsByTagName(t)[0]),
+      i.parentNode.insertBefore(n, i);
+  })(window, document, "script", "//bat.bing.com/bat.js", "uetq");
+  console.log("inside script");
+  window.uetq = window.uetq || [];
+  window.uetq.push("event", "click sur s'inscrire pack taxi", {
+    event_category: "Pricing Table events",
+    event_label: "Inscrire pack taxi Page formation taxi moto",
+    event_value: "680",
+  });
+  console.log("after script");
+}
+
+function packVmdtrPageFormationTaxiMoto() {
+  (function (w, d, t, r, u) {
+    var f, n, i;
+    (w[u] = w[u] || []),
+      (f = function () {
+        var o = {
+          ti: document.getElementById("microsoft_tracking_key").value,
+        };
+        (o.q = w[u]), (w[u] = new UET(o)), w[u].push("pageLoad");
+      }),
+      (n = d.createElement(t)),
+      (n.src = r),
+      (n.async = 1),
+      (n.onload = n.onreadystatechange =
+        function () {
+          var s = this.readyState;
+          (s && s !== "loaded" && s !== "complete") ||
+            (f(), (n.onload = n.onreadystatechange = null));
+        }),
+      (i = d.getElementsByTagName(t)[0]),
+      i.parentNode.insertBefore(n, i);
+  })(window, document, "script", "//bat.bing.com/bat.js", "uetq");
+  console.log("inside script");
+  window.uetq = window.uetq || [];
+  window.uetq.push("event", "click sur s'inscrire pack vmdtr", {
+    event_category: "Pricing Table events",
+    event_label: "Inscrire pack vmdtr Page formation taxi moto ",
+    event_value: "849",
+  });
+  console.log("after script");
+}
