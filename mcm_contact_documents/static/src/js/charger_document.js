96e229f631f01125013cca89e969629178a34dcd
seifeddinne <smabrouk@digimoov.fr>
2021-10-25 15:47:18 +0200

    Fix_charger document_1 js V2

--- mcm_contact_documents/static/src/js/charger_document.js
+++ mcm_contact_documents/static/src/js/charger_document.js
@@ -15,49 +15,52 @@ publicWidget.registry.digimoov_documents = publicWidget.Widget.extend({
         var self = this;
         var domicile_personnel_oui = document.getElementById('domicile_personnel_oui');
         var domicile_personnel_non = document.getElementById('domicile_personnel_non');
-        var identite_hebergeur = document.getElementById('identite_hebergeur_form');
+        var identite_hebergeur_recto = document.getElementById('identite_hebergeur_recto_form');
+        var identite_hebergeur_verso = document.getElementById('identite_hebergeur_verso_form');
         var attestation_hebergement = document.getElementById('attestation_hebergement_form');
-        var justificatif_domicile = document.getElementById('justificatif_domicile_form');
-        var identite_hebergeur_input = document.getElementById('identite_hebergeur_id');
+        var identite_hebergeur_recto_input = document.getElementById('identite_hebergeur_recto_id');
+        var identite_hebergeur_verso_input = document.getElementById('identite_hebergeur_verso_id');
         var attestation_hebergement_input = document.getElementById('attestation_hebergement_id');
-        var justificatif_domicile_input = document.getElementById('justificatif_domicile_id');
+        var justificatif_domicile_input_oui = document.getElementById('justificatif_domicile_id_oui');
+        var justificatif_domicile_input_non = document.getElementById('justificatif_domicile_id_non');
 
 
         if(domicile_personnel_oui.checked){
             domicile_personnel_non = false;
-            if(identite_hebergeur) {
-                identite_hebergeur.style.display='none';
-                identite_hebergeur.className='form-group row form-field';
-                identite_hebergeur_input.required = false;
+            if(identite_hebergeur_recto) {
+                identite_hebergeur_recto.style.display='none';
+                identite_hebergeur_recto.className='form-group row form-field';
+                identite_hebergeur_recto_input.required = false;
+            }
+            if(identite_hebergeur_verso) {
+                identite_hebergeur_verso.style.display='none';
+                identite_hebergeur_verso.className='form-group row form-field';
+                identite_hebergeur_verso_input.required = false;
             }
             if(attestation_hebergement) {
                 attestation_hebergement.style.display='none';
                 attestation_hebergement.className='form-group row form-field';
                 attestation_hebergement_input.required = false;
             }
-            if(justificatif_domicile) {
-                justificatif_domicile.style.display='block';
-                justificatif_domicile.className='form-group row form-field o_website_form_required';
-                justificatif_domicile_input.required = true;
-            }
         }
         else if(domicile_personnel_non.checked){
             domicile_personnel_oui.false;
-            if(identite_hebergeur) {
-                identite_hebergeur.style.display='block';
-                identite_hebergeur.className='form-group row form-field o_website_form_required';
-                identite_hebergeur_input.required = true;
+            if(identite_hebergeur_recto) {
+                identite_hebergeur_recto.style.display='block';
+                identite_hebergeur_recto.className='form-group row form-field o_website_form_required';
+                identite_hebergeur_recto_input.required = true;
+
+            }
+            if(identite_hebergeur_verso) {
+                identite_hebergeur_verso.style.display='block';
+                identite_hebergeur_verso.className='form-group row form-field o_website_form_required';
+                identite_hebergeur_verso_input.required = true;
 
             }
             if(attestation_hebergement) {
                 attestation_hebergement.style.display='block';
                 attestation_hebergement.className='form-group row form-field o_website_form_required';
                 attestation_hebergement_input.required = true;
-            }
-            if(justificatif_domicile) {
-                justificatif_domicile.style.display='none';
-                justificatif_domicile.className='form-group row form-field';
-                justificatif_domicile_input.required = false;
             }
         }
 
@@ -89,7 +92,7 @@ publicWidget.registry.check_digimoov_files = publicWidget.Widget.extend({
                  if (file != null)
                  {
                     var type=file['type'];
-                    var types = ['image/png' , 'image/jpg' , 'image/jpeg' , 'image/bmp' , 'image/gif' , 'image/svg+xml' , 'application/pdf'];
+                    var types = ['image/png' , 'image/jpg' , 'image/jpeg' , 'application/pdf'];
                     if (types.includes(type)){
                         check_type=true;
                     }
