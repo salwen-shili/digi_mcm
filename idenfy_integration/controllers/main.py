3821cf4b8081065e710112f2c2cdf2d9d5d3effe
TeqStars <info@teqstars.com>
2021-10-26 10:03:50 +0530

    [REM] Remove first documents capture screen.

--- idenfy_integration/controllers/main.py
+++ idenfy_integration/controllers/main.py
@@ -1,6 +1,8 @@
 import uuid
 from odoo import http
 from odoo.http import request
+from odoo import fields
+from datetime import datetime, date, time
 from odoo.addons.mcm_website_theme.controllers.main import Routes_Site
 from odoo.addons.mcm_contact_documents.controllers.main import CustomerPortal
 from odoo.addons.digimoov_sessions_modules.controllers.main import WebsiteSale
@@ -11,9 +13,24 @@ class IdenfyWebsiteSale(WebsiteSale):
         type='http', auth="user", website=True, sitemap=False)
     def cart(self, access_token=None, product=None, revive='', partenaire=None, **post):
         order = request.website.sale_get_order()
+        name = http.request.env.user.name
+        email = http.request.env.user.email
         if order.partner_id:
             order.partner_id.fetch_document_details_from_idenfy(request.website)
-        return super(IdenfyWebsiteSale, self).cart(access_token=access_token, product=product, revive=revive, partenaire=partenaire, **post)
+        if order.partner_id.idenfy_dl_data_id and order.partner_id.idenfy_dl_data_id.res_data:
+            docExpiry = eval(order.partner_id.idenfy_dl_data_id.res_data).get('docExpiry', '')
+            future_date = fields.Date.today().replace(year=fields.Date.today().year+3)
+            if docExpiry and fields.Date.from_string(docExpiry) < future_date:
+                print(True)
+                if request.website.id == 2:  # id 2 of website in database means website DIGIMOOV
+                    return http.request.render('mcm_contact_documents.mcm_contact_document_charger_mes_documents', {
+                        'email': email, 'name': name, 'partner_id': order.partner_id, 'expired_license':'true', 'error_identity': '', 'error_permis': '', 'error_permis_number': '',
+                        'error_domicile': ''})
+                elif request.website.id == 1:  # id 1 of website in database means website MCM ACADEMY
+                    return http.request.render('mcm_contact_documents.mcm_contact_documents_charger_mes_documents_mcm', {
+                        'email': email, 'name': name, 'partner_id': order.partner_id,'expired_license':'true', 'error_identity': '', 'error_permis': '', 'error_permis_number': '', 'error_domicile': ''})
+        res = super(IdenfyWebsiteSale, self).cart(access_token=access_token, product=product, revive=revive, partenaire=partenaire, **post)
+        return res
 
 
 class IdenfyIntegration(Routes_Site):
@@ -26,3 +43,20 @@ class IdenfyIntegration(Routes_Site):
         user = request.env['res.users'].sudo().search([('id', "=", user_id)])
         partner = user.partner_id
         return request.render("mcm_contact_documents.mcm_contact_documents_charger_mes_documents_mcm", {'partner_id': partner})
+
+class IdenfyCustomPortal(CustomerPortal):
+
+    @http.route('/charger_mes_documents', type="http", auth="user", website=True)
+    def create_documents_digimoov(self, **kw):
+        name = http.request.env.user.name
+        email = http.request.env.user.email
+        partner_id = http.request.env.user.partner_id
+        if partner_id and partner_id.check_status(request.website):
+            if request.website.id == 2:  # id 2 of website in database means website DIGIMOOV
+                return http.request.render('mcm_contact_documents.mcm_contact_document_charger_mes_documents', {
+                    'email': email, 'name': name, 'partner_id': partner_id, 'ex_warning':'','error_identity': '', 'error_permis': '', 'error_permis_number': '', 'error_domicile': ''})
+            elif request.website.id == 1:  # id 1 of website in database means website MCM ACADEMY
+                return http.request.render('mcm_contact_documents.mcm_contact_documents_charger_mes_documents_mcm', {
+                    'email': email, 'name': name, 'partner_id': partner_id, 'error_identity': '', 'error_permis': '', 'error_permis_number': '', 'error_domicile': ''})
+        else:
+            return request.redirect("/shop/cart/")
--- idenfy_integration/static/src/js/portal.js
+++ idenfy_integration/static/src/js/portal.js
@@ -14,44 +14,7 @@ window.addEventListener("message", receiveMessage, false);
             console.log(buttons);
             console.log(event.data.status);
             if (event.data.status == 'approved'){
-//                    console.log($('[href="#driving_licence"]'));
-                   if ($('[href="#driving_licence"]').hasClass("active")){
-
                         button.removeAttr("disabled")
-                   }
-                   else {
-                   $('[href="#driving_licence"]').tab('show');
-                   console.log($('[href="#driving_licence"]').hasClass("active"));
-                   console.log('tab details');
-                   }
-//                   button.removeAttr("disabled")
             }
-            console.log('end');
     }
-
-//window.addEventListener("message", receiveMessage, false);
-//            function receiveMessage(event) {
-//            console.log('start');
-//            console.log(event);
-//            console.log(this);
-//            var button = $('#submit_documents_next_button');
-//            var buttons = $('#document_next');
-//            console.log(button);
-//            console.log(buttons);
-//            console.log(event.data.status);
-//            if (event.data.status == 'approved'){
-////                    console.log($('[href="#driving_licence"]'));
-//                   if ($('[href="#driving_licence"]').hasClass("active")){
-//                        button.removeAttr("disabled")
-//                   }
-//                   else {
-//                   $('[href="#driving_licence"]').tab('show');
-//                   console.log($('[href="#driving_licence"]').hasClass("active"));
-//                   console.log('tab details');
-//                   }
-////                   button.removeAttr("disabled")
-//            }
-//            console.log('end');
-//    }
-
 });
