aa8fd55aa75283704eb3b229d10e37f29837b026
Salwen-sh <salwen.shili+github@gmail.com>
2021-11-22 10:42:47 +0100

    add popup on failed status

--- idenfy_integration/static/src/js/portal.js
+++ idenfy_integration/static/src/js/portal.js
@@ -1,20 +1,39 @@
-odoo.define('idenfy_integration.portal', function (require) {
-'use strict';
+odoo.define("idenfy_integration.portal", function (require) {
+  "use strict";
 
-var publicWidget = require('web.public.widget');
+  var publicWidget = require("web.public.widget");
 
-window.addEventListener("message", receiveMessage, false);
-            function receiveMessage(event) {
-            console.log('start');
-            console.log(event);
-            console.log(this);
-            var button = $('#submit_documents_next_button');
-            var buttons = $('#document_next');
-            console.log(button);
-            console.log(buttons);
-            console.log(event.data.status);
-            if (event.data.status == 'approved'){
-                        button.removeAttr("disabled")
-            }
+  window.addEventListener("message", receiveMessage, false);
+  function receiveMessage(event) {
+    var website = $("#website").val;
+    var url;
+    if (website == "1") {
+      url = "/charger_mes_documents";
+    } else url = "/charger_mes_documents";
+    console.log("start");
+    console.log(event);
+    console.log(this);
+    var button = $("#submit_documents_next_button");
+    var buttons = $("#document_next");
+    console.log(button);
+    console.log(buttons);
+    console.log(event.data.status);
+    if (event.data.status == "approved") {
+      button.removeAttr("disabled");
     }
+    if (event.data.status == "failed") {
+      setTimeout(() => {
+        window.location.href = "#popup1";
+        window.setInterval(function () {
+          var remainingTime = $("#spnSeconds").html();
+          remainingTime = parseInt(remainingTime);
+          if (remainingTime == 0) {
+            location.href = url;
+          } else {
+            $("#spnSeconds").html(remainingTime - 1);
+          }
+        }, 1000);
+      }, 2000);
+    }
+  }
 });
