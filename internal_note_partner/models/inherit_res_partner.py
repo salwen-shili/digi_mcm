2eb4c4f079d40b4688b36519620f7e3531f03f32
takwa <tmejri@digimoov.fr>
2021-12-15 13:58:38 +0100

    add color based on condition + result field in tree partner veiw

--- internal_note_partner/models/inherit_res_partner.py
+++ internal_note_partner/models/inherit_res_partner.py
@@ -8,11 +8,26 @@ class InheritResPartner(models.Model):
     last_internal_log = fields.Char(compute="_compute_get_last_internal_log", string="Commentaire Interne")
     company_id = fields.Many2one('res.company', default=lambda self: self.env.company)
     presence = fields.Char(readonly=True, compute="_compute_get_last_presence_values")
+    resultat = fields.Char(readonly=True, compute="_compute_get_last_resultat_values")
+    date_exam = fields.Date(related="mcm_session_id.date_exam", string="Date d'examen")
 
     def _compute_get_last_presence_values(self):
+        """ Function to get presence value of last session in tree partner view"""
         for rec in self:
-            last_line = rec.env['info.examen'].sudo().search([('partner_id', "=", rec.id)], limit=1)
-            rec.presence = last_line.presence
+            last_line = rec.env['info.examen'].sudo().search([('partner_id', "=", rec.id)], limit=1, order="id desc")
+            if last_line.presence:
+                rec.presence = str(last_line.presence).title()
+            else:
+                rec.presence == '_______'
+
+    def _compute_get_last_resultat_values(self):
+        """ Function to get result value of last session in tree partner view"""
+        for rec in self:
+            last_line = rec.env['info.examen'].sudo().search([('partner_id', "=", rec.id)], limit=1, order="id desc")
+            if last_line.resultat:
+                rec.resultat = str(last_line.resultat).title()
+            else:
+                rec.resultat == '_______'
 
     def _compute_get_last_internal_log(self):
         for record in self:
