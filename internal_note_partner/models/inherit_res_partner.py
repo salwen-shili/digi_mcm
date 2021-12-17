69cfb9d4959204d8993b1243d243126e5a1f205b
takwa <tmejri@digimoov.fr>
2021-12-17 11:35:29 +0100

    add store=TRue in presence

--- internal_note_partner/models/inherit_res_partner.py
+++ internal_note_partner/models/inherit_res_partner.py
@@ -7,7 +7,7 @@ class InheritResPartner(models.Model):
     composer_ids = fields.Many2one('mail.message', string='Composer')
     last_internal_log = fields.Char(compute="_compute_get_last_internal_log", string="Commentaire Interne")
     company_id = fields.Many2one('res.company', default=lambda self: self.env.company)
-    presence = fields.Char(readonly=True, compute="")
+    presence = fields.Char(readonly=True, compute="", store=True)
     resultat = fields.Char(readonly=True, compute="_compute_get_last_resultat_values")
     date_exam = fields.Date(related="mcm_session_id.date_exam", string="Date d'examen")
 
