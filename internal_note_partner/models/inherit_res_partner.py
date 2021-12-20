943e2d3f7fafd268a919f35fea4b464174004fcd
takwa <tmejri@digimoov.fr>
2021-12-20 14:06:45 +0100

    Add store=True resultat

--- internal_note_partner/models/inherit_res_partner.py
+++ internal_note_partner/models/inherit_res_partner.py
@@ -8,7 +8,7 @@ class InheritResPartner(models.Model):
     last_internal_log = fields.Char(compute="_compute_get_last_internal_log", string="Commentaire Interne")
     company_id = fields.Many2one('res.company', default=lambda self: self.env.company)
     presence = fields.Char(readonly=True, compute="_compute_get_last_presence_values", store=True)
-    resultat = fields.Char(readonly=True, compute="_compute_get_last_resultat_values")
+    resultat = fields.Char(readonly=True, store=True)
     date_exam = fields.Date(related="mcm_session_id.date_exam", string="Date d'examen")
 
     def _compute_get_last_presence_values(self):
