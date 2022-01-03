bab4ad81e598c06b6961eb003fc76bda97265bda
Ines Lahmar <ilahmar@digimoov.fr>
2022-01-03 16:30:35 +0100

    fix update cpf state

--- plateforme_pedagogique/models/partner.py
+++ plateforme_pedagogique/models/partner.py
@@ -1116,7 +1116,7 @@ class partner(models.Model):
                                                                      ('company_id', '=', 1),
                                                                      ('website_id', '=', 1),
                                                                      ('order_line.product_id', '=', product_id.id)])
-                        print('sale order', sale.id)
+                        
                         if not sale:
                             so = self.env['sale.order'].sudo().create({
                                 'partner_id': client.id,
