1714d2f8b10aca131a839e18086367a44adb8978
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2022-01-03 15:51:38 +0100

    [UPD] fix note

--- contract_send_documents/models/sale_order.py
+++ contract_send_documents/models/sale_order.py
@@ -11,14 +11,15 @@ class SaleOrder(models.Model):
     def write(self,values):
         order=super(SaleOrder,self).write(values)
         subtype_id = self.env['ir.model.data'].xmlid_to_res_id('mt_note')
-        message = self.env['mail.message'].sudo().create({
-            'subject': 'Contrat signé',
-            'model': 'res.partner',
-            'res_id': self.partner_id.id,
-            'message_type': 'notification',
-            'subtype_id': subtype_id,
-            'body': 'Contrat signé par ' + str(values['signed_by']),
-        })
-        for order in self:
-            order.partner_id.step = 'finish'
+        if 'signed_by' in values and 'signed_on' in values and 'signature' in values and self.state != 'cancel' and self.state != 'draft':
+            message = self.env['mail.message'].sudo().create({
+                'subject': 'Contrat signé',
+                'model': 'res.partner',
+                'res_id': self.partner_id.id,
+                'message_type': 'notification',
+                'subtype_id': subtype_id,
+                'body': 'Contrat signé par ' + str(values['signed_by']),
+            })
+            for order in self:
+                order.partner_id.step = 'finish'
         return order
