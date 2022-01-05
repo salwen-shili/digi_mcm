bfa718c459dddce51f5660bb4bc96c5fd8a306d1
takwa <tmejri@digimoov.fr>
2022-01-05 10:44:25 +0100

    Update change_state_cpf_partner

--- plateforme_pedagogique/models/partner.py
+++ plateforme_pedagogique/models/partner.py
@@ -749,7 +749,7 @@ class partner(models.Model):
 
     def change_state_cpf_partner(self):
         base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
-        if "localhost" not in str(base_url) and "dev.odoo" not in str(base_url):
+        if "localhost" not in str(base_url) and "dev.odoo" in str(base_url):
             params_wedof = (
                 ('order', 'desc'),
                 ('type', 'all'),
