b1093e361aed499bd311b0539ffeba38c99ba8bd
Ines Lahmar <ilahmar@digimoov.fr>
2022-01-10 12:16:17 +0100

    add loggger info in_create_setup_intent

--- mcm_website_theme/models/payment_acquirer.py
+++ mcm_website_theme/models/payment_acquirer.py
@@ -53,9 +53,10 @@ class PaymentAcquirer(models.Model):
         return result
 
     def _create_setup_intent(self, kwargs):
+        _logger.info("je suis laaaaaaaaaaaa ---------- %s" % str(self))
         _logger.info("je suis laaaaaaaaaaaa create ")
-       
-        result = super(PaymentAcquirer, self)._create_setup_intent()
+
+        result = super(PaymentAcquirer, self)._create_setup_intent(kwargs)
 
         return result
 
