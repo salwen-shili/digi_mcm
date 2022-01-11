d24e4a1f548a977f13f1fa7cab28dd117b90885f
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2022-01-11 16:35:13 +0100

    [UPD] t-set head

---
+++ schema_website_integration/static/js/noindex.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+if(window.location.pathname.includes('/shop/cart')){
+document.getElementsByTagName('head')[0].appendChild( '' );
+}
+});
