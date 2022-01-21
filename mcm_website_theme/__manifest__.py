e2e4442e0bb6fe436494077869517e6b997076ab
Salwen-sh <salwen.shili+github@gmail.com>
2022-01-21 15:13:41 +0100

    delete bolt inscription

--- mcm_website_theme/__manifest__.py
+++ mcm_website_theme/__manifest__.py
@@ -41,7 +41,7 @@ add css and js files to website model
         'views/website_sale/cart_multistep_integration.xml',
         'views/website_sale/template.xml',
         'views/custom_footer.xml',
-        'views/test-bold.xml'
+        'views/bolt.xml'
     ],
 
     'installable': True,
