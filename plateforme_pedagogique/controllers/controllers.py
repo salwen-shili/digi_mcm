f2d17d88b36e6222980662cd430cb9705e9c00e2
Ines Lahmar <ilahmar@digimoov.fr>
2022-01-28 16:58:00 +0100

    first commit

--- plateforme_pedagogique/controllers/controllers.py
+++ plateforme_pedagogique/controllers/controllers.py
@@ -1,11 +1,119 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
+from odoo.http import request
+from datetime import datetime, date
+import werkzeug
+import locale
+import json
+import logging
+import requests
 
+class WebhookController(http.Controller):
+    @http.route('/validate_cpf_digi', auth='public')
+    def validate_cpf_digi(self, **kw):
+        dossier = json.loads(request.httprequest.data)
+        header=json.loads(request.httprequest.header)
+        _logger.info("webhoooooooooook %s" % str(dossier))
+        _logger.info("header %s" % str(header))
+        params_wedof = (
+            ('order', 'desc'),
+            ('type', 'all'),
+            ('state', 'notProcessed'),
+            ('billingState', 'all'),
+            ('certificationState', 'all'),
+            ('sort', 'lastUpdate'),
+        )
+        headers = {
+            'accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-API-KEY': '026514d6bc7d880515a27eae4947bccef4fbbf03',
+        }
 
-# class Test(http.Controller):
-#     @http.route('/test/test/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+        externalid = dossier['externalId']
+        email = dossier['attendee']['email']
+        email = email.replace("%", ".")  # remplacer % par .
+        email = email.replace(" ", "")  # supprimer les espaces envoyés en paramètre email
+        email = str(email).lower()  # recupérer l'email en miniscule pour éviter la création des deux comptes
+        print('dossier', dossier)
+        idform = dossier['trainingActionInfo']['externalId']
+        training_id = ""
+        if "_" in idform:
+            idforma = idform.split("_", 1)
+            if idforma:
+                training_id = idforma[1]
+
+        print('training', training_id)
+        state = dossier['state']
+        lastupdatestr = str(dossier['lastUpdate'])
+        lastupdate = datetime.strptime(lastupdatestr, '%Y-%m-%dT%H:%M:%S.%fz')
+        newformat = "%d/%m/%Y %H:%M:%S"
+        lastupdateform = lastupdate.strftime(newformat)
+        lastupd = datetime.strptime(lastupdateform, "%d/%m/%Y %H:%M:%S")
+        residence = ""
+        if "residence" in dossier['attendee']['address']:
+            residence = dossier['attendee']['address']['residence']
+        num_voie = ""
+        if "number" in dossier['attendee']['address']:
+            num_voie = dossier['attendee']['address']['number']
+
+        voie = ""
+        if "roadTypeLabel" in dossier['attendee']['address']:
+            voie = dossier['attendee']['address']['roadTypeLabel']
+        nom_voie = ""
+        if "roadName" in dossier['attendee']['address']:
+            nom_voie = dossier['attendee']['address']['roadName']
+        street = str(num_voie) + ' ' + str(voie) + ' ' + str(nom_voie)
+        if "phoneNumber" in dossier['attendee']:
+            tel = dossier['attendee']['phoneNumber']
+        else:
+            tel = ""
+        if "zipCode" in dossier['attendee']['address']:
+            code_postal = dossier['attendee']['address']['zipCode']
+        else:
+            code_postal = ""
+        if "city" in dossier['attendee']['address']:
+            ville = dossier['attendee']['address']['city']
+        else:
+            ville = ""
+        if 'firstName' in dossier['attendee']['firstName']:
+            nom = dossier['attendee']['firstName']
+        else:
+            nom = ""
+
+        if "lastName" in dossier['attendee']['lastName']:
+            prenom = dossier['attendee']['lastName']
+        else:
+            prenom = ""
+        diplome = dossier['trainingActionInfo']['title']
+
+        today = date.today()
+        datedebut = today + timedelta(days=15)
+        datefin = str(datedebut + relativedelta(months=3) + timedelta(days=1))
+        datedebutstr = str(datedebut)
+        data = '{"trainingActionInfo":{"sessionStartDate":"' + datedebutstr + '","sessionEndDate":"' + datefin + '" }}'
+        dat = '{\n  "weeklyDuration": 14,\n  "indicativeDuration": 102\n}'
+        response_put = requests.put('https://www.wedof.fr/api/registrationFolders/' + externalid,
+                                    headers=headers, data=data)
+
+        status = str(response_put.status_code)
+        statuss = str(json.loads(response_put.text))
+        _logger.info("validate put _________ %s" % str(status))
+        _logger.info("validate_________ %s" % str(statuss))
+        response_post = requests.post('https://www.wedof.fr/api/registrationFolders/' + externalid + '/validate',
+            headers=headers, data=dat)
+        status = str(response_post.status_code)
+        statuss = str(json.loads(response_post.text))
+        _logger.info("validate_________ %s" % str(status))
+        _logger.info("validate_________ %s" % str(statuss))
+        """Si dossier passe à l'etat validé on met à jour statut cpf sur la fiche client"""
+        if status == "200":
+            print('validate', email)
+            self.cpf_validate(training_id, email, residence, num_voie, nom_voie, voie, street, tel, code_postal,
+                              ville,
+                              diplome, dossier['attendee']['lastName'], dossier['attendee']['firstName'],
+                              dossier['externalId'], lastupd)
+
+        return "Hello, world"
 
 #     @http.route('/test/test/objects/', auth='public')
 #     def list(self, **kw):
