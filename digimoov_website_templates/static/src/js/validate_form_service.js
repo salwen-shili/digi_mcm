b32d682019266e2cba3418b6ddd896908ac560a7
Salwen-sh <salwen.shili+github@gmail.com>
2022-02-03 16:06:35 +0100

    update service client

--- digimoov_website_templates/static/src/js/validate_form_service.js
+++ digimoov_website_templates/static/src/js/validate_form_service.js
@@ -1,31 +1,129 @@
-$(document).ready(function () {
-  var validSubmit = {
-    phone_valid: false,
-  };
-    function checkPhone(prop, value) {
-    if (value.length > 0) {
-      const pattern = /^(07|06)[0-9]\d{7}$/;
-      if (value.match(pattern)) return false;
-      else
-        return `Votre numéro de téléphone doit commencer par 06 ou 07 suivie par 8 chiffres`;
-    } else return '';
+// move label on focus
+
+const listId = [
+  'contact_lastname',
+  'contact_name',
+  'phone',
+  'address',
+  'email_from',
+  'objet',
+  'message',
+];
+listId.map((id) => {
+  console.log($(`#${id}`).val());
+  if ($(`#${id}`).val()) {
+    $(`#${id} ~ label`).animate(
+      {
+        top: '0',
+        fontSize: '12px',
+      },
+      80
+    );
   }
-    $(`#phone`).keyup(function (e) {
+});
+
+listId.map((id) => {
+  $(`#${id}`).focusin(function () {
+    $(`#${id} ~ label`).animate(
+      {
+        top: '0',
+        fontSize: '12px',
+      },
+      80
+    );
+  });
+  $(`#${id}`).focusout(function () {
+    if ($(this).val() === '') {
+      $(`#${id} ~ label`).animate(
+        {
+          top: '17px',
+          fontSize: '16px',
+        },
+        80
+      );
+    }
+  });
+});
+
+//validation functions
+
+function checkLetters(prop, value) {
+  if (value.length > 0) {
+    const pattern = /^[A-Z,a-z,À-ÿ '-]{2,}$/;
+    if (value.match(pattern)) return false;
+    else return `Votre ${prop} doit contenir des lettres seulement!`;
+  } else return 'Ce champs est obligatoire!';
+}
+function checkemail(value) {
+  if (value.length > 0) {
+    const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (value.match(pattern)) return false;
+    else return `Votre email est incorrecte!`;
+  } else return 'Ce champs est obligatoire!';
+}
+function checkPhone(prop, value) {
+  if (value.length > 0) {
+    const pattern = /^(07|06)[0-9]\d{7}$/;
+    if (value.match(pattern)) return false;
+    else
+      return `Votre numéro de téléphone doit commencer par 06 ou 07 suivie par 8 chiffres`;
+  } else return '';
+}
+
+$(document).ready(function () {
+  $(`#contact_lastname`).keyup(function (e) {
+    const contact_lastname = $('#contact_lastname').val();
+    const errorMessage = checkLetters('nom', contact_lastname);
+    if (errorMessage === false) {
+      $(`#contact_lastname_container `).removeClass('error-input-field');
+      $(`#contact_lastname_helper `).append('');
+    } else {
+      $(`#contact_lastname_container `).addClass('error-input-field');
+      $(`#contact_lastname_helper span `).text(errorMessage);
+    }
+  });
+
+  $(`#contact_name`).keyup(function (e) {
+    const contact_name = $('#contact_name').val();
+    const errorMessage = checkLetters('prenom', contact_name);
+    if (errorMessage === false) {
+      $(`#contact_name_container `).removeClass('error-input-field');
+      $(`#contact_name_helper `).append('');
+    } else {
+      $(`#contact_name_container `).addClass('error-input-field');
+      $(`#contact_name_helper span `).text(errorMessage);
+    }
+  });
+  $(`#email_from`).keyup(function (e) {
+    const email_from = $('#email_from').val();
+    const errorMessage = checkemail(email_from);
+    if (errorMessage === false) {
+      $(`#email_from_container `).removeClass('error-input-field');
+      $(`#email_from_helper `).append('');
+    } else {
+      $(`#email_from_container `).addClass('error-input-field');
+      $(`#email_from_helper span `).text(errorMessage);
+    }
+  });
+
+  $(`#phone`).keyup(function (e) {
     const phone = $('#phone').val();
+    console.log($('#phone').val());
     const errorMessage = checkPhone('numero de téléphone', phone);
     if (errorMessage === false) {
-      validSubmit['phone_valid'] = true;
-
-      if (checkValidationButton(validSubmit)) {
-        // buttonInscrire.removeAttribute("disabled");
-      }
-
       $(`#phone_container `).removeClass('error-input-field');
       $(`#phone_helper `).append('');
     } else {
-      validSubmit['phone_valid'] = false;
       $(`#phone_container `).addClass('error-input-field');
       $(`#phone_helper span `).text(errorMessage);
     }
   });
+});
+
+$('#form-check').submit(function (e) {
+  const check = document.querySelector('.error-input-field');
+  if (check != null) {
+    e.preventDefault();
+    return;
+  }
 });
