2b3aad9d3833d84a48ba2c9479aacfcfad0ad470
takwa <tmejri@digimoov.fr>
2022-02-10 15:27:45 +0100

    feature_projet_repassage_siteweb

--- digimoov_website_templates/controlleurs/main.py
+++ digimoov_website_templates/controlleurs/main.py
@@ -216,17 +216,14 @@ class DIGIEXAMEN(http.Controller):
             partner = request.env.user.partner_id  # Récupérer id de l'apprenant connecté
             session = request.env['partner.sessions'].search([('client_id', '=', partner.id)], order='id asc', limit=1)
             date_exam = session.session_id.date_exam  # Récupérer date d'examen à partir de la première session
-            is_public_user = request.website.is_public_user()
-            print("is_public_user", is_public_user)
-            if date_exam:
+            is_public_user = request.website.is_public_user()  # PUBLIC USER = VISITOR OR USER ODOO NOT CONNECTED, return true or false
+            if date_exam:  # Si date examen exist
                 now = date.today()  # Date d'aujourd'hui
                 date_dateutil = date_exam + dateutil.relativedelta.relativedelta(
                     months=6)  # Calcule la durée de temps à partir de la première date d'examen de l'apprenant en ajoutant 6 mois
                 exam_count = partner.note_exam_count
-                print("exam_count", exam_count)
-                if exam_count < 3:
+                if exam_count < 3:  # Si nombre de passage < 3
                     if now < date_dateutil and is_public_user is not True:  # Comparer si date d'aujourd'hui inférieur à date d'examen + 6 mois
-                        print("months=6")
                         echec_examen = request.env['product.product'].sudo().search(
                             [('company_id', '=', 2), ('default_code', "=", 'examen')])
                         values = {
@@ -235,6 +232,7 @@ class DIGIEXAMEN(http.Controller):
                             'message': "Vous avez dépassé la durée règlementaire de 6 mois pour réserver votre nouvelle date d'examen ."
                                        "Vous devez à présent, vous réinscrire à la formation pour retenter votre chance.",
                         }
+                    return request.render("digimoov_website_templates.digimoov_template_examen", values) # Envoyer les données vers xml dans la page examen
                 elif exam_count > 3:
                     values = {
                         'url': '/#pricing',
@@ -242,28 +240,18 @@ class DIGIEXAMEN(http.Controller):
                                    "Vous devez à présent, vous réinscrire à la formation pour retenter votre chance."
                     }
                     print("now < date_dateutil and is_public_user is not True and exam_count > 3", exam_count)
+                    return request.render('digimoov_website_templates.digimoov_template_examen', values)
+            else:
+                default = False
                 if is_public_user:
                     print("user not connected 2")
                     values = {
                         'url': '/web/signup',
                         'message': 'Pour réserver votre nouvelle tentative, '
                                    'merci de vous connecter ou de créer votre compte client.',
+                        'default': True
                     }
-                    return request.redirect("/web/signup")
-            else:
-                print("elseee")
-                values = {
-                    'url': '/web/signup',
-                    'message': 'Pour réserver votre nouvelle tentative, '
-                               'merci de vous connecter ou de créer votre compte client.',
-                }
-                return request.redirect("/web/signup")
-            # echec_examen = request.env['product.product'].sudo().search(
-            # [('company_id', '=', 2), ('default_code', "=", 'examen')])
-        #     values = {
-        #         'echec_examen': echec_examen,
-        #     }
-        #     return request.render("digimoov_website_templates.digimoov_template_examen", values)
+                return request.render("digimoov_website_templates.digimoov_template_examen", values)
         else:
             return request.redirect("/preparation-examen-taxi/vtc")
 
