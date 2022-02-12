a50eb80143af40ad42b1b7f23fee091975c6de5f
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2022-02-12 17:57:28 +0100

    [UPD] bolt_contract

--- mcm_website_theme/models/survey/survey.py
+++ mcm_website_theme/models/survey/survey.py
@@ -12,10 +12,34 @@ class SurveyUserInput(models.Model):
 
     quizz_corrected = fields.Boolean(default=False,string="Examen corrigé")
     def update_partner_exam_result(self):
+        for record in self:
+            return {
+                'type': 'ir.actions.act_window',
+                'view_mode': 'form',
+                'res_model': 'survey.user_input.wizard',
+                'target': 'new',
+                'context': {
+                    'default_survey_user_input_id': self.ids[0],
+                    'default_partner_id': record.partner_id.id,
+                    'default_score': record.quizz_score,
+                },
+            }
+
+
+class SurveyUserInputWizard(models.TransientModel):
+    _name = 'survey.user_input.wizard'
+    _description = 'confirm corrected exam'
+
+    survey_user_input_id = fields.Many2one('survey.user_input', string="Examen")
+    partner_id=fields.Many2one('res.partner',string="Client")
+    score = fields.Float('Score (%)')
+
+    def action_validate_correction_exam(self):
         for rec in self:
-            rec.quizz_corrected = True
-            rec.partner_id.note_exam = str(rec.quizz_score)
-            if rec.quizz_score < 40 :
-                print('email echec')
+            rec.survey_user_input_id.quizz_corrected = True
+            rec.partner_id.note_exam  = str(rec.score)
+            if rec.score < 40 :
+                print('envoi mail echec')
             else:
-                print('email success')
+                print("envoi mail sucess")
+
