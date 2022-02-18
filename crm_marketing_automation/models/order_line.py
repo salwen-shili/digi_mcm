7dc1b1a6cc39c1311adb1a649097e4375d48c68c
ines <ilahmar@digimoov.fr>
2022-02-18 09:37:49 +0100

    fix write crm

--- crm_marketing_automation/models/order_line.py
+++ crm_marketing_automation/models/order_line.py
@@ -7,20 +7,20 @@ from datetime import date, datetime
 import logging
 _logger = logging.getLogger(__name__)
 class SaleLine(models.Model):
-    _inherit = 'sale.order.line'
-    @api.model
-    def create(self, vals):
-        print('********',vals)
-        res = super(SaleLine, self).create(vals)
-        print('drafttt', vals)
-        order_id = vals['order_id']
-        product_id=vals['product_id']
-        product=self.env['product.product'].sudo().search([('id',"=",product_id)])
-        if product:
-            code=product.default_code
-            if code=="vtc_bolt":
-                sale=self.env['sale.order'].sudo().search([('id',"=",order_id)])
-                if sale:
-                    partner_id=sale.partner_id
-                    sale.change_stage_lead("Bolt-Prospection", partner_id)
-        return res
+     _inherit = 'sale.order.line'
+    # @api.model
+    # def create(self, vals):
+    #     print('********',vals)
+    #     res = super(SaleLine, self).create(vals)
+    #     print('drafttt', vals)
+    #     order_id = vals['order_id']
+    #     product_id=vals['product_id']
+    #     product=self.env['product.product'].sudo().search([('id',"=",product_id)])
+    #     if product:
+    #         code=product.default_code
+    #         if code=="vtc_bolt":
+    #             sale=self.env['sale.order'].sudo().search([('id',"=",order_id)])
+    #             if sale:
+    #                 partner_id=sale.partner_id
+    #                 sale.change_stage_lead("Bolt-Prospection", partner_id)
+    #     return res
