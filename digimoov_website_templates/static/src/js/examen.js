ac5c3b602f2dc34644e982223e51a5e6caf606d7
Salwen-sh <salwen.shili+github@gmail.com>
2022-02-23 08:51:54 +0100

    add documentation

--- digimoov_website_templates/static/src/js/examen.js
+++ digimoov_website_templates/static/src/js/examen.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+  //get message and url redirection from backend
   var url;
   var message;
   if (document.getElementById('url')) {
@@ -8,16 +9,21 @@ document.addEventListener('DOMContentLoaded', function () {
   if (document.getElementById('message')) {
     message = document.getElementById('message').value;
   }
+  //check in case of error to prevent displaying odoo js error
   if (!document.getElementById('echec_examen').value) {
+    //change popup to the adequate message
+    //setting the url
     document.getElementById('notifMessage').innerHTML = message;
     document.getElementById('btnRepassage').href = url;
   }
   return;
 });
+// to open popup
 function openPopup() {
   //console.log(document.getElementById('popup1'));
   document.getElementById('popup1').style.display = 'unset';
 }
+//to close popup
 function closePopup() {
   //console.log(document.getElementById('popup1'));
   document.getElementById('popup1').style.display = 'none';
