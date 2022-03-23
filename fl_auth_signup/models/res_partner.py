42f5803715e5150c8788c0ac47521f49ee2e1d55
ines <ilahmar@digimoov.fr>
2022-03-23 11:44:08 +0100

    add birthday

--- fl_auth_signup/models/res_partner.py
+++ fl_auth_signup/models/res_partner.py
@@ -6,5 +6,5 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    # birthday = fields.Date('Date of Birth')
+    birthday = fields.Date('Date of Birth')
     question_signup = fields.Char('Comment avez vous découvert notre site')
