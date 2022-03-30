8f1b3ca280d77f3450695cdf11152928b7797af5
Salwen-sh <salwen.shili+github@gmail.com>
2022-03-30 10:03:18 +0100

    fix calendly positiong on top of action btn

--- digimoov_website_templates/static/src/js/continue_wizard.js
+++ digimoov_website_templates/static/src/js/continue_wizard.js
@@ -31,19 +31,110 @@ document.addEventListener('DOMContentLoaded', function () {
   const btnContinuer = document.getElementById('button-continuer');
   const questionnaireUrl = '/coordonnees';
 
+  const messageAction = {
+    coordonnees: {
+      message: `<b>Félicitations!</b> Vous avez terminé la première étape de votre inscription. Cliquez sur <b>continuer</b> pour passer à l'<b>étape suivante<b/>. `,
+      url: `/coordonnees?${Math.floor(Math.random() * 100)}`,
+    },
+    documents: {
+      message: `<b>Félicitations!</b> Vous avez terminé l'étape <b>Coordonnées</b> de votre inscription. Pour passer à l'étape suivante merci de vous munir d'une copie originale de votre carte d'identité, et veuillez choisir le mode de téléchargement souhaité :`,
+      url: `/charger_mes_documents?${Math.floor(Math.random() * 100)}`,
+    },
+    financement: {
+      message: `<b>Félicitations!</b> Vous avez chargé vos documents. Vous pourrez désormais choisir votre date et centre d'examen et financer votre formation.`,
+      url: `/shop/cart?${Math.floor(Math.random() * 100)}`,
+    },
+    isNotSigned: {
+      message: `Nous vous remercions pour votre confiance, votre paiement a été effectué avec succès! Vous pouvez maintenant finaliser votre inscription en signant votre contrat pour avoir accès à notre plateforme de formation.`,
+      url: '',
+    },
+
+    cartIsEmpty: {
+      bolt: {
+        message: `Votre panier est vide. Vous devez choisir votre formation en cliquant sur continuer.`,
+        url: '/bolt#pricing',
+      },
+      nonBolt: {
+        message: `Votre panier est vide. Vous devez choisir votre formation en cliquant sur continuer.`,
+        url: '/#pricing',
+      },
+    },
+    boltWrongProduct: {
+      message: `Vous n'avez pas choisit la <b>formation VTC BOLT</b>. Vous devez cliquer sur continuer pour mettre à jour votre panier`,
+      url: '/bolt#pricing',
+    },
+    boltExamen: {
+      inProcess: {
+        message: `La correction de votre examen est en cours. Vous recevrez votre résultat dans 24 heures.`,
+        url: `/coordonnees?${Math.floor(Math.random() * 100)}`,
+      },
+      notpassed: {
+        message: `<b>Félicitations!</b> Vous avez terminé la première étape de votre inscription. Cliquez sur <b>continuer</b> pour passer votre <b> examen blanc<b/>. `,
+        url: `/coordonnees?${Math.floor(Math.random() * 100)}`,
+      },
+      succed: {
+        message:
+          'Félicitations, vous avez réussi votre examen. Cliquez sur continuer pour finaliser votre inscription.',
+        url: `/coordonnees?${Math.floor(Math.random() * 100)}`,
+      },
+      failed: {
+        message: "Malheureusement vous n'avez pas réussi votre examen.",
+        url: `#`,
+      },
+    },
+  };
+
   var step = 1;
   //console.log('step', current.value);
   switch (current.value) {
     case 'coordonnées':
       step = 1;
 
-      btnContinuer.setAttribute('href', questionnaireUrl);
+      // btnContinuer.setAttribute('href', questionnaireUrl);
+      btnContinuer.setAttribute('href', messageAction.coordonnees.url);
+      textDescription.innerHTML = messageAction.coordonnees.message;
 
       break;
     case 'document':
       step = 2;
       documents.classList.add('active');
-      btnContinuer.setAttribute('href', documentsUrl);
+      textDescription.innerHTML = messageAction.documents.message;
+
+      // btnContinuer.setAttribute('href', documentsUrl);
+      const uploadDocumentBtns = `
+        <div style="
+  
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    align-items: flex-end;
+    flex-wrap: wrap;
+    flex-direction: row;
+">
+                                    <div class="">
+                                        <a id="button-continuer" href="/charger_mes_documents_manual?${Math.floor(
+                                          Math.random() * 100
+                                        )}" style="margin-right: 8px;">
+                                            <button id="btn-action" class="rkmd-btn btn-blue  ripple-effect ripple-yellow" type="submit" style="font-size: 11px;width:116px">
+                                            
+                                            Manuel
+                                        </button>
+                                        </a>
+                                    </div>
+
+                                    <div class="">
+
+                                        <a id="button-continuer" href="/charger_mes_documents?${Math.floor(
+                                          Math.random() * 100
+                                        )}">
+                                            <button id="btn-action" class="rkmd-btn btn-blue  ripple-effect ripple-yellow" type="submit" style="font-size: 11px;width:116px;">
+                                            
+                                            Auto
+                                        </button>
+                                        </a>
+                                    </div>
+                                </div>`;
+      finished.innerHTML = uploadDocumentBtns;
 
       break;
     case 'financement':
@@ -51,7 +142,8 @@ document.addEventListener('DOMContentLoaded', function () {
       documents.classList.add('active');
       financement.classList.add('active');
 
-      btnContinuer.setAttribute('href', financementUrl);
+      textDescription.innerHTML = messageAction.financement.message;
+      btnContinuer.setAttribute('href', messageAction.financement.url);
 
       break;
     case 'validation':
