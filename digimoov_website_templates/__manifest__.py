accf092a63a8ff6e71a1bd08efd642c99a57d370
Salwen-sh <salwen.shili+github@gmail.com>
2022-04-04 03:56:55 +0100

    optimize deliveroo

--- digimoov_website_templates/__manifest__.py
+++ digimoov_website_templates/__manifest__.py
@@ -70,7 +70,7 @@
         'views/capacitaire-transport-bordeaux.xml',
         # page de blog
         # 'views/blog/website_blog_posts.xml',
-        'views/partners/ubereats.xml'
+        'views/partners/ubereats.xml',
         'views/partners/deliveroo.xml'
 
     ],
