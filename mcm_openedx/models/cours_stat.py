fa18f1a9806cea8fa8fb1a784d787adb0be15377
Test <45214298+khouloud97@users.noreply.github.com>
2022-05-04 17:33:01 +0200

    stat search

--- mcm_openedx/models/cours_stat.py
+++ mcm_openedx/models/cours_stat.py
@@ -7,8 +7,7 @@ from odoo import models, fields, api
 class Cours_stat(models.Model):
     _name = 'mcm_openedx.course_stat'
     _description = "importer les listes des cours pour calculer les statestiques"
-    name = fields.Char('Name')
-    nomutilisateur = fields.Char(string="Nom Utilisateur")
+    name = fields.Char(string="Nom Utilisateur")
     email = fields.Char(string="Email")
     color = fields.Char(string="color")
     idcour = fields.Char(string="ID Cours")
@@ -27,31 +26,43 @@ class Cours_stat(models.Model):
     def recherche(self):
         temppassetotale = 0
         # chercher dans la partie cour le mail et calculer le temps passer sur moocit
-        for exist in self.env['mcm_openedx.course_stat'].sudo().search(
-                [('email', "=", self.email)]):
-            # eliminer repetion in days
+        countdup = 0
+        countexist = 0
+        for exist in self.env['mcm_openedx.course_stat'].sudo().search([('email', "=", self.email)]):
+            listcourduplicated = []
+            for existee in exist:
+                if existee.idcour and existee.jour not in listcourduplicated:
+                    duplicates = self.env['mcm_openedx.course_stat'].search(
+                        [('email', "=", existee.email), ('idcour', '=', existee.idcour),('jour','=',existee.jour)])
 
-            if (exist):
-                print(exist.seconde)
-                temppassetotale = exist.seconde + temppassetotale
-                print("totale22", temppassetotale)
-                q, s = divmod(temppassetotale, 60)
-                h, m = divmod(q, 60)
-            print("%d:%d:%d" % (h, m, s))
-            heure = int((temppassetotale / 3600))
-            minute = int((temppassetotale - (3600 * heure)) / 60)
-            secondes = int(temppassetotale - (3600 * heure) - (60 * minute))
-            timee = (heure, minute, secondes)
-            print("timmmmme", minute)
-            # chercher ddans res partner l'user qui possede le meme email pour lui affecter les valeurs
-            for apprenant in self.env['res.partner'].sudo().search([
-                ('company_id', '!=', 2),
-                ('email', 'ilike', exist.email)]):
-                apprenant.date_imortation_stat = date.today()
-                apprenant.mooc_temps_passe_heure = heure
-                apprenant.mooc_temps_passe_min = minute
-                apprenant.mooc_temps_passe_seconde = secondes
-                apprenant.mooc_dernier_coonx = exist.jour
-                exist.partner = apprenant.id
-                self.partner = exist.partner
-                # self.sudo().with_context(key=existt.id).sudo().unlink()
+                    print(existee.jour)
+                    for dup in duplicates:
+                        listcourduplicated.append(dup.id)
+                        print("duuppp", dup.jour)
+                        print("duuppp", dup.idcour)
+            self.browse(listcourduplicated).unlink()
+
+        # if (exist):
+        #     print(exist.seconde)
+        #     temppassetotale = exist.seconde + temppassetotale
+        #     print("totale22", temppassetotale)
+        #     q, s = divmod(temppassetotale, 60)
+        #     h, m = divmod(q, 60)
+        # print("%d:%d:%d" % (h, m, s))
+        # heure = int((temppassetotale / 3600))
+        # minute = int((temppassetotale - (3600 * heure)) / 60)
+        # secondes = int(temppassetotale - (3600 * heure) - (60 * minute))
+        # timee = (heure, minute, secondes)
+        # print("timmmmme", minute)
+        # # chercher ddans res partner l'user qui possede le meme email pour lui affecter les valeurs
+        # for apprenant in self.env['res.partner'].sudo().search([
+        #     ('company_id', '!=', 2),
+        #     ('email', 'ilike', exist.email)]):
+        #     apprenant.date_imortation_stat = date.today()
+        #     apprenant.mooc_temps_passe_heure = heure
+        #     apprenant.mooc_temps_passe_min = minute
+        #     apprenant.mooc_temps_passe_seconde = secondes
+        #     apprenant.mooc_dernier_coonx = exist.jour
+        #     exist.partner = apprenant.id
+        #     self.partner = exist.partner
+        #     # self.sudo().with_context(key=existt.id).sudo().unlink()
