5a9faefa11585c04f3777c791ed1cd628a06de84
Salwen-sh <salwen.shili+github@gmail.com>
2022-05-18 09:46:42 +0100

    add blog snippets

--- theme_centric/__manifest__.py
+++ theme_centric/__manifest__.py
@@ -48,7 +48,7 @@
         # 'views/footers/footer_template_7.xml',
         # 'views/footers/footer_template_8.xml',
 
-        #Centric Snippets
+        # Centric Snippets
         'views/snippets/s_centric_feature.xml',
         'views/snippets/s_centric_aboutus.xml',
         'views/snippets/s_centric_service.xml',
@@ -62,7 +62,7 @@
         'views/snippets/s_centric_subscriber.xml',
         'views/snippets/s_centric_banner.xml',
 
-        #Cando Snippets
+        # Cando Snippets
         'views/snippets/s_cando_home.xml',
         'views/snippets/s_cando_service.xml',
         'views/snippets/s_cando_service_carousel.xml',
@@ -72,8 +72,8 @@
         'views/snippets/s_cando_quote.xml',
         'views/snippets/s_cando_achievement.xml',
         'views/snippets/s_cando_customers.xml',
-        
-        #Soft Snippets
+
+        # Soft Snippets
         'views/snippets/s_soft_home_banner.xml',
         'views/snippets/s_soft_about_us.xml',
         'views/snippets/s_soft_about_use.xml',
@@ -108,8 +108,8 @@
         'views/snippets/s_creative_left_para_two_imgs.xml',
         'views/snippets/s_creative_bg_img_with_para_btns.xml',
         'views/snippets/s_creative_mixing_img_para.xml',
-        
-       # Business Demo
+
+        # Business Demo
         'views/snippets/s_business_banner.xml',
         'views/snippets/s_business_digital_marketing.xml',
         'views/snippets/s_business_img_with_paragraph.xml',
@@ -122,7 +122,7 @@
         'views/snippets/s_business_features_with_image.xml',
         'views/snippets/s_business_pricing_plan.xml',
         'views/snippets/s_business_app_store.xml',
-        
+
         # Medical & Health template
         'views/snippets/s_medical_banner.xml',
         'views/snippets/s_medical_three_feature_box.xml',
@@ -132,7 +132,7 @@
         'views/snippets/s_medical_img_with_features.xml',
         'views/snippets/s_medical_testimonial.xml',
         'views/snippets/s_medical_icons_block.xml',
-        
+
         # Charity template
         'views/snippets/s_charity_banner.xml',
         'views/snippets/s_charity_help_block.xml',
@@ -141,8 +141,8 @@
         'views/snippets/s_charity_news_letter.xml',
         'views/snippets/s_charity_carousel_testimonial.xml',
         'views/snippets/s_charity_img_with_center_text.xml',
-       
-       # Software template
+
+        # Software template
         'views/snippets/s_software_banner.xml',
         'views/snippets/s_software_two_img_with_features.xml',
         'views/snippets/s_software_with_three_blocks.xml',
@@ -153,7 +153,7 @@
         'views/snippets/s_software_pricing_plan.xml',
         'views/snippets/s_software_logo_partners.xml',
         'views/snippets/s_software_contact_with_img.xml',
-        
+
         # Real Estate template
         'views/snippets/s_real_estate_banner.xml',
         'views/snippets/s_estate_carousel_images.xml',
@@ -163,8 +163,8 @@
         'views/snippets/s_estate_team.xml',
         'views/snippets/s_estate_logo_partners.xml',
         'views/snippets/s_estate_img_with_para.xml',
-        
-         # Restaurant template
+
+        # Restaurant template
         'views/snippets/s_restaturant_banner.xml',
         'views/snippets/s_restaurant_video_image.xml',
         'views/snippets/s_restaurant_img_para_btn.xml',
@@ -172,8 +172,8 @@
         'views/snippets/s_restaturant_app_store.xml',
         'views/snippets/s_reataurant_carousel_images.xml',
         'views/snippets/s_restaturant_bg_img_with_center_box.xml',
-        
-         # Web Agency template
+
+        # Web Agency template
         'views/snippets/web_agency_banner.xml',
         'views/snippets/s_web_agency_three_blocks.xml',
         'views/snippets/s_web_agency_img_text.xml',
@@ -181,7 +181,7 @@
         'views/snippets/s_web_agency_images_with_center_para.xml',
         'views/snippets/s_web_agency_tesimonial.xml',
         'views/snippets/s_web_agency_video_with_three_block_image.xml',
-        
+
         # Accounting template
         'views/snippets/s_accounting_banner.xml',
         'views/snippets/s_accounting_three_blocks.xml',
@@ -190,8 +190,8 @@
         'views/snippets/s_accounting_parallex.xml',
         'views/snippets/s_accounting_testimonial.xml',
         'views/snippets/s_accounting_offer.xml',
-       
-       # One Page template
+
+        # One Page template
         'views/snippets/s_one_page_banner.xml',
         'views/snippets/s_one_page_six_blocks.xml',
         'views/snippets/s_one_page_half_img_para.xml',
@@ -200,7 +200,7 @@
         'views/snippets/s_one_page_testimonial.xml',
         'views/snippets/s_one_page_pricing_plan.xml',
         'views/snippets/s_contact_us_one_page_bg_map.xml',
-        
+
         # Marketing & SEO template
         'views/snippets/s_marketting_seo_banner.xml',
         'views/snippets/s_marketing_para_with_two_blocks.xml',
@@ -208,7 +208,7 @@
         'views/snippets/s_marketing_parallex_with_para.xml',
         'views/snippets/s_marketing_counter.xml',
         'views/snippets/s_marketing_carousel_images.xml',
-        
+
         # Cryptocurrency template
         'views/snippets/s_crypto_banner.xml',
         'views/snippets/s_crypto_half_para_four_blocks.xml',
@@ -218,7 +218,7 @@
         'views/snippets/s_crypto_team_block.xml',
         'views/snippets/s_crypto_carousel_testimonial.xml',
         'views/snippets/s_crypto_offer_section.xml',
-        
+
         # Industrail template
         'views/snippets/s_industrial_banner.xml',
         'views/snippets/s_industrial_half_test_four_imgs_block.xml',
@@ -227,7 +227,7 @@
         'views/snippets/s_ind_projects_block.xml',
         'views/snippets/s_industrial_offer_section.xml',
         'views/snippets/s_industrial_testimonial.xml',
-        
+
         # Fitness template
         'views/snippets/s_fitness_banner.xml',
         'views/snippets/s_fitness_three_blocks.xml',
@@ -237,7 +237,7 @@
         'views/snippets/s_fitness_testimonial.xml',
         'views/snippets/s_fitness_activities.xml',
         'views/snippets/s_fitness_form_with_img.xml',
-        
+
         # Furniture template
         'views/snippets/s_furniture_banner.xml',
         'views/snippets/s_furniture_about.xml',
@@ -252,7 +252,7 @@
         'views/snippets/s_furniture_about_with_img.xml',
         'views/snippets/s_furniture_services-section-two.xml',
         'views/snippets/s_furniture_team_section.xml',
-        
+
         # Mini appoinment template
         'views/snippets/s_mini_appoinment_four.xml',
         'views/snippets/s_mini_appoinment_five.xml',
@@ -267,7 +267,7 @@
         'views/snippets/s_contact_with_bg_color_map.xml',
         'views/snippets/s_contact_us_five.xml',
         'views/snippets/s_coming_soon_banner.xml',
-        
+
         # New Homepage Template
         'views/snippets/s_new_homepage_banner.xml',
         'views/snippets/s_new_homepage_img_gallery.xml',
@@ -290,15 +290,19 @@
         'views/pages/soft_template.xml',
         'views/pages/centric_template.xml',
 
+        # custom snippet
+        'views/snippets/s_1_article_blog.xml',
+
+
     ],
     'demo': [
 
-        ],
-    
+    ],
+
     'live_test_url': 'http://bit.ly/theme-centric',
     'images': [
-         'static/description/centric_desc.png',
-         'static/description/centric_screenshot.png',
+        'static/description/centric_desc.png',
+        'static/description/centric_screenshot.png',
     ],
     'installable': True,
     'auto_install': False,
