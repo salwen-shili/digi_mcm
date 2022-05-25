01ec3bc89368c39d424f92c0a30b00c979f34702
Salwen-sh <salwen.shili+github@gmail.com>
2022-05-25 13:21:13 +0100

    set bolt popup

--- mcm_website_theme/static/src/js/popup_page_bolt.js
+++ mcm_website_theme/static/src/js/popup_page_bolt.js
@@ -74,10 +74,18 @@ var btnContiner;
 var cartIsEmpty;
 
 function setPopup() {
-  return;
   if (document.getElementById('notifMessage')) {
     const btnContinuer = document.getElementById('btn-inside-popup');
     const textDescription = document.getElementById('notifMessage');
+    btnContinuer.innerHTML = `\n <button id="btn-action" class="rkmd-btn btn-black ripple-effect ripple-yellow" type="submit" style="font-size: 11px;" onclick="closePopup()">
+                                            Fermer
+                                        </button>`;
+    textDescription.innerHTML = `Cher(e)s candidat(e)s, nous vous annonçons que l'offre relative à la formation VTC à 20 euros en collaboration avec BOLT a touchée à sa fin. <br>
+Nous clôturons les inscriptions dés aujourd'hui.
+Mais ne soyez pas découragés ! car vous aurez la possibilité d'en profiter de nouveau, lors de la prochaine session de formation prévue pour le 29/09/2022 et dont les inscriptions seront ouvertes sur notre site web à partir du 04/07/2022.`;
+
+    openPopup();
+    return;
 
     // condition = document.getElementById('exam_state');
 
