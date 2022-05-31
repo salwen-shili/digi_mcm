f866b1cc0feb33e0bf12401ba12d9a3eba6a80ac
Test <45214298+khouloud97@users.noreply.github.com>
2022-05-31 13:57:59 +0200

    update liste coach

--- mcm_openedx/models/update.py
+++ mcm_openedx/models/update.py
@@ -6,12 +6,24 @@ from odoo import models, fields, api
 class Update(models.Model):
     _name = 'mcm_openedx.update'
 
-
     email = fields.Char()
     coach = fields.Char()
 
+    def update(self):
+        # chercher les coaches
+        for coach in self.env['res.partner'].sudo().search(
+                [('est_coach', '=', 'True')]):
+            # chercher dans la fiche excel les information
+            for cour in self.env['mcm_openedx.update'].sudo().search(
+                    []):
+                # chercher dans fiche client les personne ayant la liste
+                # des personnes ayant le meme mail que la liste dans excel
+                for rec in self.env['res.partner'].sudo().search(
+                        [('email', "=", cour.email)]):
+                    # Si la personne dans la fiche excel possede le meme mail
+                    # affecter le nom de coach sur la fiche excel
+                    if (cour.email == rec.email):
+                         rec.coach_peda = coach.id
 
-
-
-
-
+                    else:
+                        print("non")
