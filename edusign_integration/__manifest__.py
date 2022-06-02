20d505733129d2206f29e6b5877e7c7bbf6b263f
Salwen-sh <salwen.shili+github@gmail.com>
2022-06-02 17:34:04 +0100

    check by id if group already exist

--- edusign_integration/__manifest__.py
+++ edusign_integration/__manifest__.py
@@ -2,24 +2,16 @@
     "name": "Edusign Integration",
     "version": "13.0",
     "category": "Examen DIGIMOOV",
-    'summary': 'Edusign Integration',
-
-    "depends": ["mcm_contact_documents", "mcm_session", "base"],
-
-    'data': [
-        'views/company.xml',
-        'views/session.xml'
-    ],
-
-    'images': [],
-
+    "summary": "Edusign Integration",
+    "depends": ["mcm_contact_documents", "mcm_session", "base", "auto_mass_mailing_marketing"],
+    "data": ["views/company.xml", "views/session.xml"],
+    "images": [],
     "author": "SALWEN SHILI",
-    'maintainer': 'SALWEN SHILI',
+    "maintainer": "SALWEN SHILI",
     "description": "Integration of edusign api to automate the creation of exam presence sheet",
-
-    'demo': [],
-    'auto_install': False,
+    "demo": [],
+    "auto_install": False,
     "installable": True,
-    'application': True,
-    'qweb': [],
+    "application": True,
+    "qweb": [],
 }
--- edusign_integration/models/mcm_session.py
+++ edusign_integration/models/mcm_session.py
@@ -3,6 +3,9 @@
 
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError
+from odoo.osv import osv
+
+
 from datetime import date, datetime
 import requests
 import logging
@@ -12,71 +15,117 @@ _logger = logging.getLogger(__name__)
 
 
 class mcmSession(models.Model):
-    _inherit = "mcmacademy.self"
+    _inherit = "mcmacademy.session"
     id_group_edusign = fields.Char(string="ID Group Edusign", readonly=True)
 
-    # post request to create a group using https://ext.edusign.fr/v1/group
-    # with a body :
-    # {
-    #     "group": {
-    #         "NAME": "testing",
-    #         "DESCRIPTION": "testing",
-    #         "STUDENTS": [""],
-    #         "PARENT": "",
-    #         "API_ID": "",
-    #         "API_TYPE": ""
-    #     }
-    # }
-
     def createGroup(self):
-        base_url = self.env["ir.config_parameter"].sudo().get_param("web.base.url")
-        # if "localhost" not in str(base_url) and "dev.odoo" not in str(base_url):
-        company = self.env["res.company"].sudo().search([("id", "=", 2)], limit=1)
-        api_key = ""
+        # iterate self in case it returns more than one object.
+        for res in self:
 
-        if company:
-            api_key = company.edusign_api_key
+            print("Edusign | call Create group on session : ", res.name, res.id_group_edusign)
+            _logger.info("Edusign | call Create group on session : ", res.name)
 
-            # get session name
-            # session = self.env["mcmacademy.session"].sudo().search([("name", "=", "Lyon 20 Juillet 2022")], limit=1)
+            base_url = self.env["ir.config_parameter"].sudo().get_param("web.base.url")
+            # if "localhost" not in str(base_url) and "dev.odoo" not in str(base_url):
+            company = self.env["res.company"].sudo().search([("id", "=", 2)], limit=1)
+            api_key = ""
 
-            print("Edusign add group post request to create : ", self.name)
-            _logger.info("Edusign add group post request to create : ", self.name)
-            headers = {
-                "Authorization": "Bearer %s" % (str(api_key)),
-                "Content-Type": "application/json",
-            }
+            if company:
+                api_key = company.edusign_api_key
+                # check if group exist already by id
 
-            url = "https://ext.edusign.fr/v1/group"
-
-            data = {
-                "group": {
-                    "NAME": "testing",
-                    "DESCRIPTION": "testing",
-                    "STUDENTS": [""],
-                    "PARENT": "",
-                    "API_ID": "",
-                    "API_TYPE": "",
+                headers = {
+                    "Authorization": "Bearer %s" % (str(api_key)),
+                    "Content-Type": "application/json",
                 }
-            }
 
-            result = requests.post(url, data=json.dumps(data), headers=headers)
-            status_code = result.status_code
+                getResult = requests.get("https://ext.edusign.fr/v1/group/" + res.id_group_edusign, headers=headers)
+                getContent = json.loads(getResult.content)
 
-            if status_code == 200:
-                resultContent = json.loads(result.content)
-                print(
-                    "Edusign add group response :",
-                    resultContent,
-                    "| status_code:",
-                    status_code,
-                )
-                _logger.info(
-                    "Edusign add group response :",
-                    resultContent,
-                    "| status_code:",
-                    status_code,
-                )
-                if resultContent["status"] == "success":
-                    print(resultContent["result"]["ID"])
-                    # add group id to self
+                groupIsCreated = True if getContent["status"] == "success" else False
+                print("Edusign group is created? : ", groupIsCreated)
+                print(getContent)
+
+                if not groupIsCreated:
+                    # Session does not exist, post request to
+                    postUrl = "https://ext.edusign.fr/v1/group/:id"
+
+                    data = {
+                        "group": {
+                            "NAME": res.name,
+                            "DESCRIPTION": "",
+                            "STUDENTS": [""],
+                            "PARENT": "",
+                            "API_ID": "",
+                            "API_TYPE": "",
+                        }
+                    }
+                    _logger.info("Edusign createGroup start post request...")
+                    print("Edusign createGroup start post request...")
+                    result = requests.post(postUrl, data=json.dumps(data), headers=headers)
+                    status_code = result.status_code
+
+                    if status_code == 200:
+                        resultContent = json.loads(result.content)
+                        print(
+                            "Edusign Create response :",
+                            resultContent,
+                            "| status_code:",
+                            status_code,
+                        )
+                        _logger.info(
+                            "Edusign Create response :",
+                            resultContent,
+                            "| status_code:",
+                            status_code,
+                        )
+                        if resultContent["status"] == "success":
+
+                            print("Groupe created with ID: ", resultContent["result"]["ID"])
+                            _logger.info("Groupe created with ID: ", resultContent["result"]["ID"])
+                            # add group id to self
+                            res.id_group_edusign = resultContent["result"]["ID"]
+
+                            # return {
+                            #     "type": "ir.actions.client",
+                            #     "tag": "display_notification",
+                            #     "params": {
+                            #         "title": _("i"),
+                            #         "message": _("Groupe ajouté avec succès!"),
+                            #         "sticky": True,
+                            #         "className": "bg-success",
+                            #     },
+                            # }
+
+                    else:
+                        # group can't be created.
+                        print("Exit Edusign CreateGroup post request with status code ", status_code)
+                        _logger.info("Exit Edusign CreateGroup post request with status code ", status_code)
+
+                        return {
+                            "type": "ir.actions.client",
+                            "tag": "display_notification",
+                            "params": {
+                                "title": _("Erreur"),
+                                "message": _(
+                                    "Impossible de créer le groupe dans edusign. API Edusign a enovye un erreur"
+                                ),
+                                "sticky": True,
+                                "className": "bg-success",
+                            },
+                        }
+                else:
+                    # group exists
+                    print("Exit Edusign CreateGroup get request with status code ", getContent)
+                    _logger.info("Exit Edusign CreateGroup get request with status code ", getContent)
+
+                    return {
+                        "type": "ir.actions.client",
+                        "tag": "display_notification",
+                        "params": {
+                            "title": _("i"),
+                            "message": _("Un groupe avec ce nom existe déjà!"),
+                            "sticky": True,
+                            "className": "bg-success",
+                        },
+                    }
