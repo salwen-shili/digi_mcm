3553ded1c93c0c1645c3eea98f72e0e21b2316f2
ines <ilahmar@digimoov.fr>
2022-06-09 12:09:06 +0100

    add condition of firstname

--- plateforme_pedagogique/models/partner.py
+++ plateforme_pedagogique/models/partner.py
@@ -683,24 +683,24 @@ class partner(models.Model):
     # Extraire firstName et lastName à partir du champs name
     def diviser_nom(self, partner):
         # _logger.info('name au debut  %s' %partner.name)
-        if partner.name == "":
-            partner.firstName = partner.name
-            partner.lastName = partner.name
-        # Cas d'un nom composé
-        else:
-
-            if " " in partner.name:
-
-                name = partner.name.split(" ", 1)
-                if name:
-                    partner.firstName = name[0]
-                    partner.lastName = name[1]
-            # Cas d'un seul nom
-            else:
-
+        if not partner.firstName and not partner.lastName:
+            if partner.name == "":
                 partner.firstName = partner.name
                 partner.lastName = partner.name
-                print('first', partner.firstName)
+            # Cas d'un nom composé
+            else:
+                    if " " in partner.name:
+
+                        name = partner.name.split(" ", 1)
+                        if name:
+                            partner.firstName = name[0]
+                            partner.lastName = name[1]
+                    # Cas d'un seul nom
+                    else:
+
+                        partner.firstName = partner.name
+                        partner.lastName = partner.name
+                        print('first', partner.firstName)
 
     """recuperer les dossier avec état accepté apartir d'api wedof,
     puis faire le parcours pour chaque dossier,
