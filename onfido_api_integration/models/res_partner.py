0723d969d4980e39a5840c8cb476e84eea9b4cc8
ines <ilahmar@digimoov.fr>
2022-06-10 16:17:31 +0100

    trst api key

--- onfido_api_integration/models/res_partner.py
+++ onfido_api_integration/models/res_partner.py
@@ -9,7 +9,7 @@ class InheritConfig(models.Model):
         url_post="https://api.eu.onfido.com/v3.4/applicants"
         token_test="api_sandbox.SwW0JwK3WRL.4ntnjwCs6IWIbVv882-QPjQ0Q_UiPSCz"
         headers = {
-            'Authorization':token_test,
+            'Authorization':'Token token='+token_test,
             # Already added when you pass json= but not when you pass data=
         #     'Content-Type': 'application/json',
         }
@@ -20,6 +20,6 @@ class InheritConfig(models.Model):
             'cross_device_url': 'https://example.com',
         }
 
-        response = requests.post('https://api.eu.onfido.com/v3.4/sdk_token', headers=headers, json=json_data)
+        response = requests.post(url_post, headers=headers, json=json_data)
 
         print('ressssssssssppp',response.json())
