6caf278cb7e55bea52d2ae2f885015da66d6c006
ines <ilahmar@digimoov.fr>
2022-06-14 13:57:56 +0100

    fix firstname erreur

--- onfido_api_integration/models/res_partner.py
+++ onfido_api_integration/models/res_partner.py
@@ -1,13 +1,34 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, tools
 import requests
-
+import onfido
+import urllib.request
+from odoo.modules.module import get_resource_path
+from PIL import Image
 class InheritConfig(models.Model):
     _inherit = "res.partner"
 
 
     def test_api_onfido(self):
+        # api_instance = onfido.Api('api_sandbox.SwW0JwK3WRL.4ntnjwCs6IWIbVv882-QPjQ0Q_UiPSCz','region')
         url_post="https://api.eu.onfido.com/v3.4/applicants"
         token_test="api_sandbox.SwW0JwK3WRL.4ntnjwCs6IWIbVv882-QPjQ0Q_UiPSCz"
+
+        # applicant_details = {
+        #     "first_name": "Jane",
+        #     "last_name": "Consider",
+        #     "dob": "1990-01-31",
+        #     "address": {
+        #     "building_number": "100",
+        #     "street": "Main Street",
+        #     "town": "London",
+        #     "postcode": "SW4 6EH",
+        #     "country": "GBR",
+        #
+        # },}
+        #
+        # res=api_instance.applicant.create(applicant_details)
+        # print('ressssssssssppp',res)
+
         headers = {
             'Authorization':'Token token='+token_test,
             # Already added when you pass json= but not when you pass data=
@@ -15,11 +36,51 @@ class InheritConfig(models.Model):
         }
 
         json_data = {
-            'applicant_id': '<APPLICANT_ID>',
-            'referrer': 'https://*.example.com/example_page/*',
-            'cross_device_url': 'https://example.com',
+            "first_name": "Test",
+            "last_name": "Ines",
+            "dob": "1990-01-31",
+            "address": {
+            "building_number": "100",
+            "street": "Main Street",
+            "town": "London",
+            "postcode": "SW4 6EH",
+            "country": "GBR",
+            }
+        }
+        response = requests.post(url_post, headers=headers, json=json_data)
+        applicant=response.json()
+        print('ressssssssssppp',applicant)
+        applicant_id=applicant['id']
+        self.upload_document(headers,applicant_id)
+        # self.upload_photo(headers,applicant_id)
+
+
+    def upload_document(self,headers,applicant_id):
+
+
+        files = {
+            'file': open('/home/ines/PycharmProjects/site_mcm_academy/onfido_api_integration/static/src/sample_driving_licence.png','rb'),
+
+        }
+        data={
+            'applicant_id': applicant_id
         }
 
-        response = requests.post(url_post, headers=headers, json=json_data)
+        post_document = requests.post('https://api.eu.onfido.com/v3.4/documents/', headers=headers, files=files,data=data)
+        print('document',post_document)
 
-        print('ressssssssssppp',response.json())
+    def upload_photo(self,headers,applicant_id):
+        files = {
+            'file': open(
+                '/home/ines/PycharmProjects/site_mcm_academy/onfido_api_integration/static/src/sample_photo-e5e3561c.png;type=image/png',
+                ),
+
+        }
+        data = {
+            'applicant_id': applicant_id
+        }
+
+        post_photo = requests.post('https://api.eu.onfido.com/v3.4/live_photos', headers=headers, files=files,
+                                      data=data)
+        print('photo', post_photo)
+        
