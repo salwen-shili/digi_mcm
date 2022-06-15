3f65e191302b24d6a90c5f7b268a0a076604b219
takwa <tmejri@digimoov.fr>
2022-06-15 14:28:07 +0100

    Update field statut calendly

--- calendly_api_integration/models/res_partner.py
+++ calendly_api_integration/models/res_partner.py
@@ -7,30 +7,32 @@ from odoo import api, fields, models
 class resPartner(models.Model):
     _inherit = "res.partner"
 
-    formation_type=fields.Selection(selection=[
+    formation_type = fields.Selection(selection=[
         ('taxi', 'TAXI'),
         ('vtc', 'VTC'),
     ], string='Formation')
-    pole_emploi=fields.Char('ID POLE EMPLOI')
-    social_security_number=fields.Char('Numéro securité social')
-    funding_type=fields.Selection(selection=[
+    pole_emploi = fields.Char('ID POLE EMPLOI')
+    social_security_number = fields.Char('Numéro securité social')
+    funding_type = fields.Selection(selection=[
         ('cpf', 'CPF'),
         ('passformation', 'Pass formation '),
         ('perso', 'Perso'),
         ('pole_emploi', 'Pôle emploi(AIF)'),
     ], string='Type de financement')
-    driver_licence=fields.Boolean('3 ans de permis ou plus')
-    license_suspension=fields.Boolean('Aucun retrait définitif du permis ces 10 dernières années ')
-    criminal_record=fields.Boolean('Casier judiciaire vierge B2')
-    statut_calendly=fields.Selection(selection=[
+    driver_licence = fields.Boolean('3 ans de permis ou plus')
+    license_suspension = fields.Boolean('Aucun retrait définitif du permis ces 10 dernières années ')
+    criminal_record = fields.Boolean('Casier judiciaire vierge B2')
+    statut_calendly = fields.Selection(selection=[
         ('waiting', 'En attente de validation'),
         ('valid', 'Validé'),
+        ('request', 'Demande de documents'),
+        ('remove', 'Annulé'),
+        ('postponed', 'Reporté'),
     ], string='Statut client')
 
     rendezvous_count = fields.Integer(compute='compute_rendezvous_count')
-    
+
     def compute_rendezvous_count(self):
         for record in self:
             record.rendezvous_count = self.env['calendly.rendezvous'].search_count(
                 [('partner_id', 'child_of', self.id)])
-
