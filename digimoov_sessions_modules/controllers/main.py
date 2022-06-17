581041affdebb6534dc7d0ab7d9ac474d88ca947
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2022-06-17 15:34:01 +0100

    [UPD] bolt

--- digimoov_sessions_modules/controllers/main.py
+++ digimoov_sessions_modules/controllers/main.py
@@ -1611,7 +1611,7 @@ class WebsiteSale(WebsiteSale):
                 if check_transaction and order.state == 'sent':
                     return request.redirect("/my/orders/%s?access_token=%s" % (order.id, order.access_token))
                 elif not check_transaction and order.state == 'sent' :
-                    
+                    _logger.info('shop confirmation transaction  :  %s and order %s' % (str(check_transaction), str(order.name)))
         return super(WebsiteSale, self).payment_confirmation(**post)
 
 
