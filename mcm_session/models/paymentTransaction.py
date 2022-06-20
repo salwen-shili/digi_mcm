b123c1820bdc735069feb784bbd24bf6002efd8c
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2022-06-20 17:48:26 +0100

    [UPD] set statut done

--- mcm_session/models/paymentTransaction.py
+++ mcm_session/models/paymentTransaction.py
@@ -26,13 +26,13 @@ class PaymentTransaction(models.Model):
                     for partner in sale.session_id.client_ids:
                         list.append(partner.id)
                     list.append(sale.partner_id.id)
-                    sale.session_id.write({'client_ids': [(6, 0, list)]})
+                    sale.session_id.sudo().write({'client_ids': [(6, 0, list)]})
                     canceled_list=[]
                     for partner in sale.session_id.panier_perdu_ids:
                         if (partner.id != sale.partner_id.id):
                             canceled_list.append(partner.id)
-                    sale.session_id.write({'panier_perdu_ids': [(6, 0, canceled_list)]})
-                    sale.partner_id.statut='won'
+                    sale.session_id.sudo().write({'panier_perdu_ids': [(6, 0, canceled_list)]})
+                    sale.partner_id.sudo().write({'statut' : 'won'})
                     sale.partner_id.mode_de_financement = 'particulier'
                     sale.partner_id.mcm_session_id=sale.session_id
                     sale.partner_id.module_id=sale.module_id
--- mcm_website_theme/models/payment_transaction.py
+++ mcm_website_theme/models/payment_transaction.py
@@ -25,6 +25,7 @@ class PaymentTransaction(models.Model):
                 Session = self.env['mcm.session']
                 sale.partner_id.mcm_session_id = sale.session_id
                 sale.partner_id.module_id = sale.module_id
+                sale.partner_id.sudo().write({'statut': 'won'})
                 sale.partner_id.mode_de_financement = 'particulier'
                 for order in sale.order_line:
                     session = Session.sudo().search([('product_id', '=', order.product_id.product_tmpl_id.id)])
@@ -57,6 +58,7 @@ class PaymentTransaction(models.Model):
                 sale.action_confirm()
                 sale.partner_id.mcm_session_id=sale.session_id
                 sale.partner_id.module_id=sale.module_id
+                sale.partner_id.sudo().write({'statut': 'won'})
                 sale.partner_id.mode_de_financement ='particulier'
                 moves = sale._create_invoices(final=False)
                 for move in moves:
