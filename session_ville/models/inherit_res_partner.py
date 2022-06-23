e4c09c02ea5f1b81280833fde240e6487e887396
takwa <tmejri@digimoov.fr>
2022-06-23 17:09:24 +0100

    Ajouter field country_of_birth_id

--- session_ville/models/inherit_res_partner.py
+++ session_ville/models/inherit_res_partner.py
@@ -5,7 +5,9 @@ class InheritResPartner(models.Model):
     _inherit = "res.partner"
 
     # lier les clients cpf par une ville
-    session_ville_id = fields.Many2one('session.ville', string="Ville")
+    session_ville_id = fields.Many2one('session.ville', string="Ville", track_visibility='onchange')
     # add relation with class adress for partner
     salle_id = fields.Many2one('session.adresse.examen', related='mcm_session_id.session_adresse_examen',
      readonly=True, help="Choisir une adresse pour la salle d'examen!")
+    # add pays de naissance dans la fiche client
+    country_of_birth_id = fields.Many2one('res.country', track_visibility='onchange')
