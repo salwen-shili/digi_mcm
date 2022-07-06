b428a8bfd9218e0dcb0ceb129138bb9faecec566
Salwen-sh <salwen.shili+github@gmail.com>
2022-07-06 15:46:53 +0100

    show opup and redirect to bolt inscription

--- mcm_website_theme/static/src/js/redirectbolt.js
+++ mcm_website_theme/static/src/js/redirectbolt.js
@@ -1,7 +1,20 @@
 console.log(!window.location.search);
 document.addEventListener('DOMContentLoaded', function () {
-  console.log(!window.location.search, '  asd');
-  if (!window.location.search) {
-    window.location.href = 'https://www.academy.bolt.eu/examen-theorique-pratique';
-  }
+  openPopup();
 });
+
+function openPopup() {
+  setTimeout(() => {
+    document.getElementById('popup1').style.display = 'flex';
+    if (document.getElementById('JotFormIFrame-221713712251546'))
+      document.getElementById('JotFormIFrame-221713712251546').remove(0);
+    if (document.getElementById('JotFormIFrame-221803597636059'))
+      document.getElementById('JotFormIFrame-221803597636059').remove(0);
+  }, 2000);
+}
+
+function redirection() {
+  if (!window.location.search || window.location.search.includes('&email={email}')) {
+    window.open('https://www.academy.bolt.eu/examen-theorique-pratique', '_blank');
+  }
+}
