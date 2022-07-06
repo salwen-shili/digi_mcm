6e5d3680ffbb3db25e799ae86b11d3aec0e4d960
Salwen-sh <salwen.shili+github@gmail.com>
2022-07-06 16:07:49 +0100

    fix show popup condition

--- mcm_website_theme/static/src/js/redirectbolt.js
+++ mcm_website_theme/static/src/js/redirectbolt.js
@@ -4,17 +4,22 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function openPopup() {
-  setTimeout(() => {
-    document.getElementById('popup1').style.display = 'flex';
-    if (document.getElementById('JotFormIFrame-221713712251546'))
-      document.getElementById('JotFormIFrame-221713712251546').remove(0);
-    if (document.getElementById('JotFormIFrame-221803597636059'))
-      document.getElementById('JotFormIFrame-221803597636059').remove(0);
-  }, 2000);
+  console.log(
+    !window.location.search || window.location.search.includes('&email={email}'),
+    !window.location.search,
+    window.location.search.includes('&email={email}')
+  );
+  if (!window.location.search || window.location.search.includes('&email={email}')) {
+    setTimeout(() => {
+      document.getElementById('popup1').style.display = 'flex';
+      if (document.getElementById('JotFormIFrame-221713712251546'))
+        document.getElementById('JotFormIFrame-221713712251546').remove(0);
+      if (document.getElementById('JotFormIFrame-221803597636059'))
+        document.getElementById('JotFormIFrame-221803597636059').remove(0);
+    }, 2000);
+  }
 }
 
 function redirection() {
-  if (!window.location.search || window.location.search.includes('&email={email}')) {
-    window.open('https://www.academy.bolt.eu/examen-theorique-pratique', '_blank');
-  }
+  window.open('https://www.academy.bolt.eu/examen-theorique-pratique', '_blank');
 }
