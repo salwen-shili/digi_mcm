c59ff64f73a4567d77e44f40a65601d8a55f57c0
Salwen-sh <salwen.shili+github@gmail.com>
2022-07-06 13:57:54 +0100

    redirect to page bolt if no params

---
+++ mcm_website_theme/static/src/js/redirectbolt.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+  if (!window.location.search) {
+    window.location.href = 'https://www.academy.bolt.eu/examen-theorique-pratique';
+  }
+});
