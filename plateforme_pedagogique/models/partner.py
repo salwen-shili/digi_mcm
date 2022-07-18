fd9b397744fdb81b7cd667fd4513687adb0913b4
takwa <tmejri@digimoov.fr>
2022-07-18 15:26:01 +0100

    Fix invalid literal for int() with base 10

--- plateforme_pedagogique/models/partner.py
+++ plateforme_pedagogique/models/partner.py
@@ -75,22 +75,24 @@ class partner(models.Model):
     @api.onchange('total_time_visio_min', 'total_time_appels_min')
     def convert_minutes_to_hours(self):
         """ Convert Minutes To Hours And Minutes """
-        if self.total_time_visio_min:
+        if self.total_time_visio_min != 0:
             hours = self.total_time_visio_min // 60
             minutes = self.total_time_visio_min % 60
             self.total_time_visio_hour = str(hours) + "h" + str(minutes) + "min"
             self.total_time_min = int(self.total_time_visio_min) + int(self.total_time_appels_min)
-            tot_min = int(self.total_time_min) // 60
-            tot_hour = int(self.total_time_min) % 60
-            self.total_time_hours = str(tot_min) + "h" + str(tot_hour) + "min"
-        if self.total_time_appels_min:
+            if self.total_time_min != 0:
+                tot_min = int(self.total_time_min) // 60
+                tot_hour = int(self.total_time_min) % 60
+                self.total_time_hours = str(tot_min) + "h" + str(tot_hour) + "min"
+        if self.total_time_appels_min != 0:
             hour = self.total_time_appels_min // 60
             min = self.total_time_appels_min % 60
             self.total_time_appels_hour = str(hour) + "h" + str(min) + "min"
             self.total_time_min = int(self.total_time_visio_min) + int(self.total_time_appels_min)
-            tot_min = int(self.total_time_min) // 60
-            tot_hour = int(self.total_time_min) % 60
-            self.total_time_hours = str(tot_min) + "h" + str(tot_hour) + "min"
+            if self.total_time_min != 0:
+                tot_min = int(self.total_time_min) // 60
+                tot_hour = int(self.total_time_min) % 60
+                self.total_time_hours = str(tot_min) + "h" + str(tot_hour) + "min"
 
     def write(self, vals):
         """Changer login d'apprenant au moment de changement d'email sur la fiche client"""
