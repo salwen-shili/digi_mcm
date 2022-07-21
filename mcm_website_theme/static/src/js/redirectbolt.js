56d6692a4561f59c0bd1fea3ca0b9c7cc1f38885
Front-END <sshili@digimoov.fr>
2022-07-21 14:26:13 +0200

    block inscription - load documents

--- mcm_website_theme/static/src/js/redirectbolt.js
+++ mcm_website_theme/static/src/js/redirectbolt.js
@@ -4,12 +4,22 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function openPopup() {
-  console.log(
-    !window.location.search || window.location.search.includes('&email={email}'),
-    !window.location.search,
-    window.location.search.includes('&email={email}')
-  );
-  if (!window.location.search || window.location.search.includes('&email={email}')) {
+  // console.log(
+  //   !window.location.search || window.location.search.includes('&email={email}'),
+  //   !window.location.search,
+  //   window.location.search.includes('&email={email}')
+  // );
+  // if (!window.location.search || window.location.search.includes('&email={email}')) {
+  //   setTimeout(() => {
+  //     document.getElementById('popup1').style.display = 'flex';
+  //     if (document.getElementById('JotFormIFrame-221713712251546'))
+  //       document.getElementById('JotFormIFrame-221713712251546').remove(0);
+  //     if (document.getElementById('JotFormIFrame-221803597636059'))
+  //       document.getElementById('JotFormIFrame-221803597636059').remove(0);
+  //   }, 2000);
+  // }
+
+
     setTimeout(() => {
       document.getElementById('popup1').style.display = 'flex';
       if (document.getElementById('JotFormIFrame-221713712251546'))
@@ -17,7 +27,7 @@ function openPopup() {
       if (document.getElementById('JotFormIFrame-221803597636059'))
         document.getElementById('JotFormIFrame-221803597636059').remove(0);
     }, 2000);
-  }
+  
 }
 
 function redirection() {
