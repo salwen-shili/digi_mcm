0810da026f852261708ddefeb6b61c2233c6cbd5
takwa <tmejri@digimoov.fr>
2022-08-09 16:40:47 +0100

    add logger info

--- partner_exam/models/information_examen.py
+++ partner_exam/models/information_examen.py
@@ -503,8 +503,8 @@ class NoteExamen(models.Model):
             info_exam = self.env['info.examen'].sudo().search(
                 [('partner_id', '=', res.partner_id.id), ('id', "!=", res.id)], order="id desc", limit=1)
             if res.partner_id.module_id.product_id.default_code == 'examen' or info_exam.presence == 'Absence justifiée':
-                _logger.info('info_exam µµµµµµµµµµµµµ************ %s', info_exam.partner_id.display_name)
-                _logger.info('info_exam µµµµµµµµµµµµµ************ %s', info_exam.presence)
+                _logger.info('info_exam µµµµµµµµµµµµµµ************ %s', info_exam.partner_id.display_name)
+                _logger.info('info_exam µµµµµµµµµµµµµµ************ %s', info_exam.presence)
 
                 if info_exam:
                     if info_exam.nombre_de_passage == 'premier':
