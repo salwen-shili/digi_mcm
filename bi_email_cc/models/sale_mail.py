7dd536aff3c6658cb2d71a6673354c73b82cd3b3
takwa <tmejri@digimoov.fr>
2022-08-16 16:32:37 +0100

    add condition to bcc field based on template

--- bi_email_cc/models/sale_mail.py
+++ bi_email_cc/models/sale_mail.py
@@ -12,19 +12,12 @@ _logger = logging.getLogger(__name__)
 class MailComposeMessage(models.TransientModel):
     _inherit = 'mail.compose.message'
 
-    @api.model
-    def _get_default_bcc(self):
-        if self.template_id:
-            a = self.env['res.partner'].sudo().search([('email', "=", "digimoov.fr+25e168c414@invite.trustpilot.com")]).ids
-            _logger.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%ù test %s" % a)
-            return a
-
     cc_partner_ids = fields.Many2many(
         'res.partner', 'mail_compose_message_res_cc_partner_rel',
         'wizard_id', 'cc_partner_id', 'cc', readonly=False)
     bcc_partner_ids = fields.Many2many(
         'res.partner', 'mail_compose_message_res_bcc_partner_rel',
-        'wizard_id', 'bcc_partner_id', 'BCC TEST', readonly=False, default=_get_default_bcc)
+        'wizard_id', 'bcc_partner_id', 'BCC TEST', readonly=False)
     rply_partner_id = fields.Many2one('res.partner', string='Default Reply-To', readonly=False)
     is_cc = fields.Boolean(string='Enable Email CC')
     is_bcc = fields.Boolean(string='Enable Email BCC')
@@ -36,7 +29,11 @@ class MailComposeMessage(models.TransientModel):
         active_ids = self._context.get('active_ids')
         rply_partner_id = self.env["ir.config_parameter"].sudo().get_param("bi_email_cc.rply_partner_id")
         cc_partner_ids = self.env["ir.config_parameter"].sudo().get_param("bi_email_cc.cc_partner_ids")
-        bcc_partner_ids = self.env["ir.config_parameter"].sudo().get_param("bi_email_cc.bcc_partner_ids")
+        if (res.template_id, 'ilike', "DIGIMOOV RÉSULTATS FAVORABLES"):
+            bcc_partner_ids = self.env['res.partner'].sudo().search([('email', "=", "digimoov.fr+25e168c414@invite.trustpilot.com")])
+            _logger.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%ù test %s" % self.template_id)
+        else:
+            bcc_partner_ids = self.env["ir.config_parameter"].sudo().get_param("bi_email_cc.bcc_partner_ids")
         is_cc = self.env["ir.config_parameter"].sudo().get_param("bi_email_cc.email_cc")
         is_bcc = self.env["ir.config_parameter"].sudo().get_param("bi_email_cc.email_bcc")
         is_reply = self.env["ir.config_parameter"].sudo().get_param("bi_email_cc.email_reply")
@@ -44,7 +41,7 @@ class MailComposeMessage(models.TransientModel):
             res.update({
                 'rply_partner_id': int(rply_partner_id),
                 'cc_partner_ids': [(6, 0, literal_eval(cc_partner_ids))],
-                'bcc_partner_ids': [(6, 0, literal_eval(bcc_partner_ids))],
+                'bcc_partner_ids': [(6, 0, bcc_partner_ids.ids)],
                 'is_cc': is_cc,
                 'is_bcc': is_bcc,
                 'is_reply': is_reply,
