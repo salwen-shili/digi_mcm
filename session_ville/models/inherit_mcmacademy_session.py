c5db37ec2f9b1809c0f155a20c62f8ca9d0538d4
takwa <tmejri@digimoov.fr>
2022-08-18 14:17:14 +0100

    add coach to session

--- session_ville/models/inherit_mcmacademy_session.py
+++ session_ville/models/inherit_mcmacademy_session.py
@@ -14,7 +14,7 @@ class McmacademySessionVille(models.Model):
     lien = fields.Char(related="session_adresse_examen.lien", string="Lien d'accées au centre d'examen")
     num_agrement_jury = fields.Many2one(related="session_ville_id.num_agrement_jury", copy=True)
     state = fields.Selection(related='session_ville_id.state', string="Région", store=True, readonly=False)
-    coach_id = fields.Many2many('res.partner', track_visibility='always')
+    coach_id = fields.Many2many('res.partner', ralated="session_ville_id.coach_id", track_visibility='always')
 
     @api.onchange('session_ville_id')
     def onchange_session_ville_id(self):
