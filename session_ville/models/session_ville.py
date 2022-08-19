1ed2b062d5e3267906f2f0de37e54fb7cc581312
takwa <tmejri@digimoov.fr>
2022-08-19 09:01:35 +0100

    feature_add_coach_field

--- session_ville/models/session_ville.py
+++ session_ville/models/session_ville.py
@@ -1,7 +1,4 @@
 from odoo import api, fields, models, _
-import logging
-
-_logger = logging.getLogger(__name__)
 
 
 class SessionVille(models.Model):
@@ -15,7 +12,7 @@ class SessionVille(models.Model):
     # Ce champ "active" pour permettre a l'utilisateur d'archiver un enregistrement(ville)
     active = fields.Boolean('Active', default=True)
     description = fields.Text()
-    ville_formation = fields.Boolean('Habilitation electrique',default=False,track_visibility=True)
+    ville_formation = fields.Boolean('Habilitation electrique', default=False, track_visibility=True)
     session_adresse_examen_ids = fields.One2many('session.adresse.examen', 'session_ville_id')
     company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company)
     state = fields.Selection([
@@ -34,15 +31,23 @@ class SessionVille(models.Model):
         ('ile_de_france', "Île-de-France"),
     ], default=False, tracking=True)
     num_agrement_jury = fields.Many2one('approval.number', string="Numéro d'agrément")
-    coach_id = fields.Many2many('res.partner', track_visibility='always')
+    coach_id = fields.Many2many('res.partner', track_visibility='always', readonly=True, store=True)
 
     @api.onchange('name_ville')
     def set_default_coach_id(self):
-        users_paris = self.env["res.partner"].sudo().search([("email", "=", "mbensaad@digimoov.fr")], order="id desc",
-                                                            limit=1)
-        _logger.info("¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨set_default_coach_id  %s" % users_paris.email)
-        if self.name_ville == "Paris":
+        """ Affectation des coachs selon les villes dans l'interface de session """
+        # get partner record based on email
+        users_paris = self.env["res.partner"].sudo().search([("email", "=", "mbensaad@digimoov.fr")], limit=1)
+        users_lyon = self.env["res.partner"].sudo().search([("email", "=", "amahjoub@digimoov.fr")], limit=1)
+        users_nantes = self.env["res.partner"].sudo().search([("email", "=", "starchoun@digimoov.fr")], limit=1)
+        # get values of partners records in coach_id field based on name_ville
+        # This will append the values in related field coach_id in session view
+        if self.name_ville == "Paris" or self.name_ville == "Guadeloupe" or self.name_ville == "Metz":
             self.update({"coach_id": [(6, 0, users_paris.ids)]})
+        elif self.name_ville == "Lyon" or self.name_ville == "Marseille" or self.name_ville == "Lille" or self.name_ville == "Bordeaux":
+            self.update({"coach_id": [(6, 0, users_lyon.ids)]})
+        elif self.name_ville == "Nantes" or self.name_ville == "Toulouse" or self.name_ville == "Nice":
+            self.update({"coach_id": [(6, 0, users_nantes.ids)]})
 
 
 class SessionApprovalNumber(models.Model):
@@ -52,4 +57,3 @@ class SessionApprovalNumber(models.Model):
     name = fields.Char()
     company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company)
     description = fields.Char()
-
