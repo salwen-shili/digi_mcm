bf3a5475e14b8ac15f7808d5c70cf8eb28c3360a
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2022-08-23 16:54:32 +0100

    [UPD] mail thread message post customize reply to

--- helpdesk_create_ticket/models/mail_thread.py
+++ helpdesk_create_ticket/models/mail_thread.py
@@ -148,3 +148,17 @@ class MailThread(models.AbstractModel):
                 res['email_from'] = tools.formataddr(
                     (user.partner_id.name or u"False", user_signature.email_from or u"False"))  # change the sender mail
         return res
+
+    @api.returns('mail.message', lambda value: value.id)
+    def message_post(self, *,
+                     body='', subject=None, message_type='notification',
+                     email_from=None, author_id=None, parent_id=False,
+                     subtype_id=False, subtype=None, partner_ids=None, channel_ids=None,
+                     attachments=None, attachment_ids=None,
+                     add_sign=True, record_name=False,
+                     **kwargs):
+        new_message = super(MailThread, self).message_post(body,subject,message_type,email_from,author_id,parent_id,subtype_id,subtype,partner_ids
+                                                           ,channel_ids,attachments,attachment_ids,add_sign,record_name,kwargs)
+        _logger.info('message_post : %s' % str(new_message))
+
+        return new_message
