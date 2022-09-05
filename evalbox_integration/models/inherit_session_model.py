677833963d8f97f883a19f2436e35de4cc350899
takwa <tmejri@digimoov.fr>
2022-09-05 14:45:44 +0100

    add check if date exam == True

--- evalbox_integration/models/inherit_session_model.py
+++ evalbox_integration/models/inherit_session_model.py
@@ -11,15 +11,17 @@ class InheritSaleOrder(models.Model):
     def _get_visible_create_class_evalbox(self):
         """ Add fuction to control show and hide button in session view "Exporter une classe vers Evalbox" """
         date_today = date.today()
-        if self.date_exam > date_today:
-            self.show_hide_button = True
-        else:
-            self.show_hide_button = False
+        if self.date_exam:
+            if self.date_exam > date_today:
+                self.show_hide_button = True
+            else:
+                self.show_hide_button = False
 
     def _get_visible_import_note_evalbox(self):
         """ Add fuction to control show and hide button in session view "Importer les notes depuis Evalbox" """
         date_today = date.today()
-        if self.date_exam + timedelta(days=7) > date_today:
-            self.hide_import_note_evalbox = True
-        else:
-            self.hide_import_note_evalbox = False
+        if self.date_exam:
+            if self.date_exam + timedelta(days=7) > date_today:
+                self.hide_import_note_evalbox = True
+            else:
+                self.hide_import_note_evalbox = False
