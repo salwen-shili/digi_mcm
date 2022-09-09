84dfd22d94d61541cae01a7b41a0fee0f23382eb
ines <ilahmar@digimoov.fr>
2022-09-09 16:16:58 +0100

    add motif refus

--- onfido_api_integration/controllers/webhook_onfido.py
+++ onfido_api_integration/controllers/webhook_onfido.py
@@ -133,107 +133,109 @@ class OnfidoController(http.Controller):
         return True
 
     """get event workflowrund is completed with webhook """
+
     @http.route(['/workflow_webhook'], type='json', auth="public", csrf=False)
     def completed_workflow_event(self, **kw):
         values = {}
-        motif=""
-        _logger.info("webhoooooooooook onfido %s" %str(kw))
+        motif = ""
+        _logger.info("webhoooooooooook onfido %s" % str(kw))
         data = json.loads(request.httprequest.data)
         # data = json.loads(kw)
         workflow_run_id = data['payload']['object']['id']
         _logger.info("workflow_run_id onfido %s" % str(workflow_run_id))
-       
+
         website = request.env['website'].get_current_website()
-        partner=request.env.user.partner_id
+        partner = request.env.user.partner_id
         workflow_runs = partner.get_workflow_runs(workflow_run_id, website.onfido_api_key_live)
         _logger.info("workflow_run onfido response %s" % str(workflow_runs))
         applicant_id = workflow_runs['applicant_id']
-        currentUser = request.env['res.partner'].sudo().search([('onfido_applicant_id',"=",applicant_id)])
-        data_onfido = request.env['onfido.info'].sudo().search([('partner_id','=',currentUser.id)],limit=1,order="id desc")
+        currentUser = request.env['res.partner'].sudo().search([('onfido_applicant_id', "=", applicant_id)])
+        data_onfido = request.env['onfido.info'].sudo().search([('partner_id', '=', currentUser.id)], limit=1,
+                                                               order="id desc")
         list_document = partner.get_listDocument(applicant_id, website.onfido_api_key_live)
         _logger.info('*************************************DOCUMENT***************** %s' % str(list_document))
         if currentUser:
             """get report document"""
-            check=currentUser.get_checks(applicant_id,website.onfido_api_key_live)
+            check = currentUser.get_checks(applicant_id, website.onfido_api_key_live)
             report_id = check['checks'][0]['report_ids'][0]
-            _logger.info("report_id %s" %str(report_id))
-            report = currentUser.get_report(report_id,website.onfido_api_key_live)
+            _logger.info("report_id %s" % str(report_id))
+            report = currentUser.get_report(report_id, website.onfido_api_key_live)
             _logger.info("reppooort %s" % str(report))
             """get result of validation, if fail we check the reason in brakdown and notify the user"""
-            if str(workflow_runs['finished'])=='True' and workflow_runs['state'] == 'fail':
-                _logger.info('state document %s' %str(workflow_runs['state']))
-                currentUser.validation_onfido="fail"
+            if str(workflow_runs['finished']) == 'True' and workflow_runs['state'] == 'fail':
+                _logger.info('state document %s' % str(workflow_runs['state']))
+                currentUser.validation_onfido = "fail"
                 breakdown_origin = report['breakdown']['visual_authenticity']
-                breakdown_quality = report['breakdown']['picture_face_integrity']
-                breakdown_expiration = report['breakdown']['document_expiration']
+                breakdown_quality = report['breakdown']['image_integrity']
+                breakdown_expiration = report['breakdown']['data_validation']['breakdown']['document_expiration']
                 _logger.info('breakdown %s' % str(breakdown_origin['result']))
                 if breakdown_origin['result'] != 'clear':
-                    motif = "original_document"
-                    motif_fiche="document non original"
+                    motif_fiche = "Documents non originaux."
                     message_ticket = "Motif: type de documents scannés ou capture d'écran."
                     _logger.info('breakdown %s' % str(breakdown_origin['result']))
                 if breakdown_quality['result'] != 'clear':
-                    motif = "quality_document"
-                    motif_fiche="Documents de mauvaise qualité"
+                    motif_fiche = "Documents de mauvaise qualité."
                     message_ticket = "Motif: Documents de mauvaise qualité."
                     _logger.info('breakdown %s' % str(breakdown_origin['result']))
                 if breakdown_expiration['result'] != 'clear':
-                    motif = "expired_document"
-                    motif_fiche="Documents expirés"
+                    motif_fiche = "Documents expirés"
                     message_ticket = "Motif: Documents expirés."
                     _logger.info('breakdown %s' % str(breakdown_expiration['result']))
             if data_onfido:
-                    data_onfido.validation_onfido="fail"
-                    _logger.info('*************************************currentUser.validation_onfido***************** %s' % str(currentUser.id))
-                    documents = request.env['documents.document'].sudo().search([('partner_id', "=", currentUser.id)])
+                data_onfido.validation_onfido = "fail"
+                data_onfido.motif = motif_fiche
+                _logger.info(
+                    '*************************************currentUser.validation_onfido***************** %s' % str(
+                        currentUser.id))
+                documents = request.env['documents.document'].sudo().search([('partner_id', "=", currentUser.id)])
+                _logger.info("document %s" % str(documents))
+                if documents:
+                    for document in documents:
+                        document.state = "refused"
+                # self.create_document(data_onfido.id_document_front,"front",data_onfido.type_front,"refused",currentUser)
+                # self.create_document(data_onfido.id_document_back,"back",data_onfido.type_back,"refused",currentUser)
+                #
+                else:
+                    time.sleep(9)
+                    _logger.info(
+                        '*************************************after waite***************** %s' % str(
+                            currentUser.id))
+                    documents = request.env['documents.document'].sudo().search(
+                        [('partner_id', "=", currentUser.id)])
                     _logger.info("document %s" % str(documents))
                     if documents:
                         for document in documents:
                             document.state = "refused"
-                    # self.create_document(data_onfido.id_document_front,"front",data_onfido.type_front,"refused",currentUser)
-                    # self.create_document(data_onfido.id_document_back,"back",data_onfido.type_back,"refused",currentUser)
-                    # 
-                    else :
-                        time.sleep(9)
-                        _logger.info(
-                            '*************************************after waite***************** %s' % str(
-                                currentUser.id))
-                        documents = request.env['documents.document'].sudo().search(
-                            [('partner_id', "=", currentUser.id)])
-                        _logger.info("document %s" % str(documents))
-                        if documents:
-                            for document in documents:
-                                document.state = "refused" 
-                    """créer ticket pour service client"""
-                    vals = {
-                        'partner_email': '',
-                        'partner_id': False,
-                        'description': currentUser.name+":"+ message_ticket ,
-                        'name': 'Documents refusés',
-                        'team_id': request.env['helpdesk.team'].sudo().search(
-                            [('name', "like", _('Client')), ('company_id', "=", 1)],
-                            limit=1).id,
-                    }
-                    description = currentUser.name+":"+ message_ticket
-                    ticket = self.env['helpdesk.ticket'].sudo().search(
-                        [('description', 'ilike', description)])
-                    if not ticket:
-                        new_ticket = self.env['helpdesk.ticket'].sudo().create(
-                            vals)
-                    return motif
-            if str(workflow_runs['finished'])=='True' and workflow_runs['state'] == 'clear':
+                """créer ticket pour service client"""
+                vals = {
+                    'partner_email': '',
+                    'partner_id': False,
+                    'description': currentUser.name + ":" + message_ticket,
+                    'name': 'Documents refusés',
+                    'team_id': request.env['helpdesk.team'].sudo().search(
+                        [('name', "like", _('Client')), ('company_id', "=", 1)],
+                        limit=1).id,
+                }
+                description = currentUser.name + ":" + message_ticket
+                ticket = self.env['helpdesk.ticket'].sudo().search(
+                    [('description', 'ilike', description)])
+                if not ticket:
+                    new_ticket = self.env['helpdesk.ticket'].sudo().create(
+                        vals)
+                return True
+            if str(workflow_runs['finished']) == 'True' and workflow_runs['state'] == 'clear':
                 _logger.info('else state document %s' % str(workflow_runs['state']))
                 currentUser.validation_onfido = "clear"
                 if data_onfido:
-                    data_onfido.validation_onfido="clear"
-                    documents=request.env['documents.document'].sudo().search([('partner_id',"=",currentUser.id)])
-                    _logger.info("document %s" %str(documents))
+                    data_onfido.validation_onfido = "clear"
+                    documents = request.env['documents.document'].sudo().search([('partner_id', "=", currentUser.id)])
+                    _logger.info("document %s" % str(documents))
                     if documents:
                         for document in documents:
-                            document.state="validated"
+                            document.state = "validated"
                     # self.create_document(data_onfido.id_document_front,"front",data_onfido.type_front,"validated",currentUser)
                     # self.create_document(data_onfido.id_document_back,"back",data_onfido.type_back,"validated",currentUser)
-                    else :
+                    else:
                         time.sleep(9)
                         _logger.info(
                             '*************************************after waite clear  ***************** %s' % str(
@@ -248,18 +250,21 @@ class OnfidoController(http.Controller):
             return True
 
     """send state of document to frontend """
-    @http.route(['/onfido/get_state_document'],methods=["POST"] ,type='json', auth="user", csrf=False)
-    def sendStateDocument(self):
-        partner_id=request.env.user.partner_id
-        partner=request.env['res.partner'].sudo().search([('id',"=",partner_id.id)])
-        _logger.info("request.env.user.partner_id %s name=%s" %(str(partner_id.id),str(partner_id.name)))
-        _logger.info("partner.validation_onfido %s" %str(partner.validation_onfido))
-    
-        if partner:
-           return {'validation_onfido': partner.validation_onfido }
-        else :
-            return {'validation_onfido': "partner not found" }
 
+    @http.route(['/onfido/get_state_document'], methods=["POST"], type='json', auth="user", csrf=False)
+    def sendStateDocument(self):
+        partner_id = request.env.user.partner_id
+        partner = request.env['res.partner'].sudo().search([('id', "=", partner_id.id)])
+        data_onfido = request.env['onfido.info'].sudo().search([('partner_id', "=", partner_id.id)],
+        limit = 1, order = "id desc")
+        _logger.info("request.env.user.partner_id %s name=%s" % (str(partner_id.id), str(partner_id.name)))
+        _logger.info("partner.validation_onfido %s" % str(partner.validation_onfido))
+        if partner:
+            if partner.validation_onfido == "fail":
+                if data_onfido and data_onfido.motif:
+                    return {'validation_onfido': partner.validation_onfido, 'motif': data_onfido.motif}
+        else:
+            return {'validation_onfido': "partner not found", 'motif': False}
     
 
     def create_document(self,document_id,side,type,state,currentUser):
