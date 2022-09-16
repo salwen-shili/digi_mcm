000ec1c529da65c3f0d23f8581aa964102d34bfe
khouloudachour <45214298+khouloud97@users.noreply.github.com>
2022-09-16 15:14:32 +0200

    add get event

--- mcm_openedx/models/calendlyapi.py
+++ mcm_openedx/models/calendlyapi.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 import logging
+from datetime import date
+
+import werkzeug
 
 from odoo import models, http, fields
 import http.client
@@ -16,11 +19,11 @@ class calendly_integration(models.Model):
     name = fields.Char(string="name")
     slug = fields.Char(string="nom de cour")
 
-    active = fields.Char(string="active")
+    active = fields.Boolean(string="active")
     created_at = fields.Date(string="created_at")
     owner = fields.Char(string="owner")
     scheduling_url = fields.Char(string="scheduling_ur")
-    updated_at = fields.Date(string="updated_ae")
+    updated_at = fields.Date(string="updated_at")
     uri = fields.Char(string="uri ")
 
     def khou(self):
@@ -37,30 +40,131 @@ class calendly_integration(models.Model):
             print("Test send mail calendly APPII TESTT", events)
             active = events['active']
             name = events['name']
+            owner = name.split("-")
+            print("naaaaaaaaaaaaaaaaaaame",owner[2])
             slug = events['slug']
             created_at = events['created_at']
-            owner = events['profile']['owner']
+            # owner = events['profile']['owner']
             scheduling_url = events['scheduling_url']
             updated_at = events['updated_at']
             uri = events['uri']
+            uuid_eventtype = uri.split("/")
+            print("okookokookook", uuid_eventtype[4])
+
             for existt in self.env['mcm_openedx.calendly_integration'].sudo().search(
-                    []):
-                print("aaaaaaaaaaaaaaaaaaaaaaaa", existt.name)
+                    [('id', '!=', False)]):
+                print("poooooooooooooooooo", existt.name)
+                existe = self.env['mcm_openedx.calendly_integration'].sudo().search(
+                    [('name', "like", events['name'])])
+                print(existe.name)
 
-                if (existt):
-                    print("deja existant")
-                    print("aaaaaaaaaaaaaaaaaaaaaaaa", existt.name)
-
-                if not existt:
+                if not existe:
                     print("dont exist")
                     new = self.env['mcm_openedx.calendly_integration'].sudo().create({
                         'name': name,
                         'slug': slug,
                         'active': active,
                         'created_at': created_at,
-                        'owner': owner,
+                        'owner': owner[2],
                         'scheduling_url': scheduling_url,
                         'updated_at': updated_at,
                         'uri': uri,
                     })
                     print(new)
+
+    def test_url(self):
+            print("ooooooooooooooooooooooooooo")
+
+            return {
+                "url": self.scheduling_url,
+                "type": "ir.actions.act_url"
+            }
+
+
+
+class event_calendly(models.Model):
+    _name = 'mcm_openedx.calendly_event'
+
+    scheduling_url_name = fields.Char(string="name")
+    scheduling_url_created_at = fields.Date(string="created_at")
+    scheduling_url_updated_at = fields.Date(string="updated_at")
+    scheduling_url_uri = fields.Char(string="uri")
+
+    event_name = fields.Char(string="name")
+    location = fields.Char(string="Location")
+    start_at = fields.Date(string="start_at")
+    status = fields.Boolean(string="active")
+
+    def event(self):
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer eyJraWQiOiIxY2UxZTEzNjE3ZGNmNzY2YjNjZWJjY2Y4ZGM1YmFmYThhNjVlNjg0MDIzZjdjMzJiZTgzNDliMjM4MDEzNWI0IiwidHlwIjoiUEFUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJodHRwczovL2F1dGguY2FsZW5kbHkuY29tIiwiaWF0IjoxNjYzMTQ4NjA2LCJqdGkiOiJkZDUwYWIxNy04ZDM3LTQyMjYtOGMzYy02NzMyNzI1MTM2NmUiLCJ1c2VyX3V1aWQiOiI1YWE5NWU3Mi0zNWFiLTQzOTEtOGZmNi0zNGNkZDRlMzRmODYifQ.TKiAMPGQFUdBODBHq8H-0LgQnbkldxW5V_hFacDyJgn53B-MbTQcBHLqwPx8uN_CiLfJahF_NJ1V4cc0Z5gmqg"
+        }
+        querystring = {"user": "https://api.calendly.com/users/5aa95e72-35ab-4391-8ff6-34cdd4e34f86",
+                       "organization": "https://api.calendly.com/organizations/c7e28d20-f7eb-475f-954a-7ae1a36705e3",
+                       "status": "active", "min_start_time": date.today()}
+
+        r = requests.get('https://api.calendly.com/scheduled_events',
+                         headers=headers, params=querystring)
+        print("hahah", r.json()["collection"])
+        shevent = r.json()["collection"]
+        for shevents in shevent:
+
+            scheduling_url_uri = shevents['uri']
+            scheduling_url_name = shevents['name']
+            print(scheduling_url_name)
+            scheduling_url_created_at = shevents['created_at']
+            scheduling_url_updated_at = shevents['updated_at']
+            uuid_eventtype = scheduling_url_uri.split("/")
+            print("okookokookook", uuid_eventtype[4])
+
+            params = {
+                'statuts': 'active'
+            }
+            headers = {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer eyJraWQiOiIxY2UxZTEzNjE3ZGNmNzY2YjNjZWJjY2Y4ZGM1YmFmYThhNjVlNjg0MDIzZjdjMzJiZTgzNDliMjM4MDEzNWI0IiwidHlwIjoiUEFUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJodHRwczovL2F1dGguY2FsZW5kbHkuY29tIiwiaWF0IjoxNjYzMTQ4NjA2LCJqdGkiOiJkZDUwYWIxNy04ZDM3LTQyMjYtOGMzYy02NzMyNzI1MTM2NmUiLCJ1c2VyX3V1aWQiOiI1YWE5NWU3Mi0zNWFiLTQzOTEtOGZmNi0zNGNkZDRlMzRmODYifQ.TKiAMPGQFUdBODBHq8H-0LgQnbkldxW5V_hFacDyJgn53B-MbTQcBHLqwPx8uN_CiLfJahF_NJ1V4cc0Z5gmqg"
+            }
+
+            rep = requests.get('https://api.calendly.com/scheduled_events/%s' % (uuid_eventtype[4]),
+                               headers=headers, params=params)
+            response = rep.json()['resource']
+
+            print("okookokreponseeni", response)
+            event_name = response['name']
+            print("w", event_name)
+            location = response['location']['location']
+            start_at = response['start_time']
+            status = response['status']
+
+            for existt in self.env['mcm_openedx.calendly_event'].sudo().search(
+                    [('id', '!=', False)]):
+                print("mcm_openedx.calendly_event", existt.scheduling_url_name)
+                existe = self.env['mcm_openedx.calendly_event'].sudo().search(
+                    [('scheduling_url_name', "like", shevents['name'])])
+
+
+                print(existe.scheduling_url_name)
+
+                if not existe:
+                    print("dont exist")
+                    new = self.env['mcm_openedx.calendly_event'].sudo().create({
+                        'scheduling_url_name': scheduling_url_name,
+                        'scheduling_url_created_at': scheduling_url_created_at,
+                        'scheduling_url_updated_at': scheduling_url_updated_at,
+                        'scheduling_url_uri': scheduling_url_uri,
+                        'event_name': event_name,
+                        'location': location,
+                        'start_at': start_at,
+                        'status': status,
+
+                    })
+                    print(new)
+
+    def test_url(self):
+        if self.id:
+            return {
+                'type': 'ir.actions.act_url',
+                'url': 'https://www.abc.com?statementid=%s' % (self.id),
+                'target': 'new',
+            }
