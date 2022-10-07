2010f4a595ad6b921da34e04ae6fd404b7d30d22
takwa <tmejri@digimoov.fr>
2022-10-07 08:56:11 +0100

    Inherit sign views

--- digimoov_sessions_modules/__manifest__.py
+++ digimoov_sessions_modules/__manifest__.py
@@ -28,7 +28,7 @@
         'views/template.xml',
         'views/sale_portal_templates.xml',
         'views/sale_order_report.xml',
-        'views/sign_request.xml',
+        #'views/sign_request.xml',
         'views/sign_template.xml',
         #'views/document_sign.xml',
         'data/sign_data.xml',
