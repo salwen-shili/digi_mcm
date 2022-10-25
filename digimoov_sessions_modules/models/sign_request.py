a9d16dbb167c076d3cca514dcc7f831d9d14eb6b
takwa <tmejri@digimoov.fr>
2022-10-25 15:00:42 +0100

    update send_signature_accesses function

--- digimoov_sessions_modules/models/sign_request.py
+++ digimoov_sessions_modules/models/sign_request.py
@@ -39,25 +39,25 @@ class InheritSignRequest(models.Model):
                 force_send=True
             )
 
-class SignRequest(models.Model):
-    _inherit = "sign.request"
-
-    company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company)
-
-    def action_resend(self):
-        user = self.env.user
-        user.company_id = self.env.company.id
-        self.action_draft()
-        subject = _("%s vous a envoyé un document à remplir et à signer") % (self.company_id.name)
-        self.action_sent(subject=subject)
-
-class SignSendRequest(models.TransientModel):
-    _inherit = 'sign.send.request'
-
-    @api.model
-    def default_get(self, fields):
-        user = self.env.user
-        user.company_id = self.env.company.id
-        res = super(SignSendRequest, self).default_get(fields)
-        res['subject'] =  _("%s vous a envoyé un document à remplir et à signer") % (self.env.company.name)
-        return res
+# class SignRequest(models.Model):
+#     _inherit = "sign.request"
+#
+#     company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company)
+#
+#     def action_resend(self):
+#         user = self.env.user
+#         user.company_id = self.env.company.id
+#         self.action_draft()
+#         subject = _("%s vous a envoyé un document à remplir et à signer") % (self.company_id.name)
+#         self.action_sent(subject=subject)
+#
+# class SignSendRequest(models.TransientModel):
+#     _inherit = 'sign.send.request'
+#
+#     @api.model
+#     def default_get(self, fields):
+#         user = self.env.user
+#         user.company_id = self.env.company.id
+#         res = super(SignSendRequest, self).default_get(fields)
+#         res['subject'] =  _("%s vous a envoyé un document à remplir et à signer") % (self.env.company.name)
+#         return res
