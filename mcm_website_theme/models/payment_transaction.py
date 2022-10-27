47985cc0c975ac2892f233c58755dda3d7d46828
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2022-10-27 17:10:21 +0100

    comment create move

--- mcm_website_theme/models/payment_transaction.py
+++ mcm_website_theme/models/payment_transaction.py
@@ -49,49 +49,50 @@ class PaymentTransaction(models.Model):
 
     def _reconcile_after_transaction_done(self):
         transaction=super(PaymentTransaction,self)._reconcile_after_transaction_done()
-        _logger.info("***************after transaction done")
-        invoices = self.mapped('invoice_ids')
-        template = self.env['mail.template'].sudo().search([('model', '=', 'account.move')])
-        if self.reference:
-            data = self.reference.split("-")
-            sale = self.env['sale.order'].sudo().search([('name', 'ilike', data[0])])
-            _logger.info("_reconcile_after_transaction_done state %s and state_of_transaction %s and sale is : %s" % (str(self.stripe_payment_intent), str(self.state), str(sale)))
-            if (self.stripe_payment_intent and self.state == 'done' and sale):
-                sale.action_confirm()
-                sale.partner_id.mcm_session_id=sale.session_id
-                sale.partner_id.module_id=sale.module_id
-                sale.partner_id.sudo().write({'statut': 'won'})
-                sale.partner_id.mode_de_financement ='particulier'
-                moves = sale._create_invoices(final=False)
-                _logger.info("_reconcile_after_transaction_done moves %s" %str(moves))
-                for move in moves:
-                    move.type_facture='web'
-                    move.methodes_payment = 'cartebleu'
-                    move.stripe_sub_reference=self.stripe_sub_reference # s'il sagit d'un paiement sur plusieur fois recupérer l'id d'abonnement sur stripe
-                    move.module_id=sale.module_id
-                    move.session_id=sale.session_id
-                    sale.partner_id.mcm_session_id = sale.session_id
-                    sale.partner_id.module_id = sale.module_id
-                    if sale.pricelist_id.code:
-                        move.pricelist_id=sale.pricelist_id
-                    move.company_id=sale.company_id
-                    move.action_post()
-                sale.action_cancel()
-                sale.sale_action_sent()
+        return transaction
+        # _logger.info("***************after transaction done")
+        # invoices = self.mapped('invoice_ids')
+        # template = self.env['mail.template'].sudo().search([('model', '=', 'account.move')])
+        # if self.reference:
+        #     data = self.reference.split("-")
+        #     sale = self.env['sale.order'].sudo().search([('name', 'ilike', data[0])])
+        #     _logger.info("_reconcile_after_transaction_done state %s and state_of_transaction %s and sale is : %s" % (str(self.stripe_payment_intent), str(self.state), str(sale)))
+        #     if (self.stripe_payment_intent and self.state == 'done' and sale):
+        #         sale.action_confirm()
+        #         sale.partner_id.mcm_session_id=sale.session_id
+        #         sale.partner_id.module_id=sale.module_id
+        #         sale.partner_id.sudo().write({'statut': 'won'})
+        #         sale.partner_id.mode_de_financement ='particulier'
+        #         moves = sale._create_invoices(final=False)
+        #         _logger.info("_reconcile_after_transaction_done moves %s" %str(moves))
+        #         for move in moves:
+        #             move.type_facture='web'
+        #             move.methodes_payment = 'cartebleu'
+        #             move.stripe_sub_reference=self.stripe_sub_reference # s'il sagit d'un paiement sur plusieur fois recupérer l'id d'abonnement sur stripe
+        #             move.module_id=sale.module_id
+        #             move.session_id=sale.session_id
+        #             sale.partner_id.mcm_session_id = sale.session_id
+        #             sale.partner_id.module_id = sale.module_id
+        #             if sale.pricelist_id.code:
+        #                 move.pricelist_id=sale.pricelist_id
+        #             move.company_id=sale.company_id
+        #             move.action_post()
+        #         sale.action_cancel()
+        #         sale.sale_action_sent()
 
-                for move in moves.with_user(SUPERUSER_ID):
-                    move.message_post_with_template(int(template),
-                                                       composition_mode='comment',
-                                                       email_layout_xmlid="portal_contract.mcm_mail_notification_paynow_online"
-                                                      )
+                # for move in moves.with_user(SUPERUSER_ID):
+                #     move.message_post_with_template(int(template),
+                #                                        composition_mode='comment',
+                #                                        email_layout_xmlid="portal_contract.mcm_mail_notification_paynow_online"
+                #                                       )
                 
-                if sale.partner_id.renounce_request == False:
-                    """Envoyer sms pour renoncer au droit de rétractation """
-                    url = '%smy' % str(sale.partner_id.company_id.website)
-                    short_url = pyshorteners.Shortener()
-                    short_url = short_url.tinyurl.short(
-                        url)  # convert the url to be short using pyshorteners library
-                    sms_body_ = "Afin d'intégrer notre plateforme de formation de suite, veuillez renoncer à votre droit de rétractation sur votre espace client %s" % (
-                        short_url) # content of sms
-
-                    sale.partner_id.send_sms(sms_body_, sale.partner_id)
+                # if sale.partner_id.renounce_request == False:
+                #     """Envoyer sms pour renoncer au droit de rétractation """
+                #     url = '%smy' % str(sale.partner_id.company_id.website)
+                #     short_url = pyshorteners.Shortener()
+                #     short_url = short_url.tinyurl.short(
+                #         url)  # convert the url to be short using pyshorteners library
+                #     sms_body_ = "Afin d'intégrer notre plateforme de formation de suite, veuillez renoncer à votre droit de rétractation sur votre espace client %s" % (
+                #         short_url) # content of sms
+                # 
+                #     sale.partner_id.send_sms(sms_body_, sale.partner_id)
