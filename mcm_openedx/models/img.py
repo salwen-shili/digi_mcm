377d8eb8097ce9ff54323dae79cb44c1010fc537
khouloudachour <45214298+khouloud97@users.noreply.github.com>
2022-11-07 13:13:11 +0100

    add-form

--- mcm_openedx/models/img.py
+++ mcm_openedx/models/img.py
@@ -1,8 +1,13 @@
 import base64
 import requests
+from jotform import *
 
 from odoo import models, fields
 from odoo.tools import json
+
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class img(models.Model):
@@ -14,6 +19,9 @@ class img(models.Model):
     statut = fields.Char(string="Statut Formulaire")
     url = fields.Char(string="Titre Formulaire")
 
-
     def get_form(self):
         print("get events")
+        jotformAPIClient = JotformAPIClient("98b07bd5ae3cd7054da0c386c4f699df")
+        forms = jotformAPIClient.get_forms()
+        for form in forms:
+            _logger.info(form["title"])
