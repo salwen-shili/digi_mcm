4235711343603d75b352b3f06b8d45cb1c356514
takwa <tmejri@digimoov.fr>
2022-11-07 10:11:38 +0100

    update create methode add condition with model

--- partner_exam/models/inherit_mail_template.py
+++ partner_exam/models/inherit_mail_template.py
@@ -3,6 +3,7 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
+
 class InheritMailTemplate(models.Model):
     _inherit = "mail.template"
 
@@ -11,30 +12,35 @@ class InheritMailTemplate(models.Model):
         """ Pour définir le nom PDF automatiquement lors de l'envoi d'un e-mail,
          la configuration basée sur le champ du rapport optionnel à imprimer à joindre et nom du fichier du rapport. """
         result = super(InheritMailTemplate, self).create(vals)
-        if vals.get('model_id'):
-            _logger.info('Test model_id %s ' % str(vals.get('model_id')))
-        if "CONVOCATION" in vals.get('name'): #Si mot CONVOCATION existe dans le nom de rapport
-            result.auto_delete = False
-            result.report_template = self.env['ir.actions.report'].search([('name', '=', str("Convocation"))], limit=1)
-            result.report_name = str(result.report_template.name) + "- ${object.display_name} - ${object.mcm_session_id.session_ville_id.display_name} - ${object.mcm_session_id.date_exam.strftime('%d/%m/%Y')}"
-        elif "Cerfa" in vals.get('name'):
-            result.auto_delete = False
-            result.report_template = self.env['ir.actions.report'].search([('name', '=', str("Cerfa"))], limit=1)
-            result.report_name = str(result.report_template.name) + "- ${object.display_name} - ${object.mcm_session_id.session_ville_id.display_name} - ${object.mcm_session_id.date_exam.strftime('%d/%m/%Y')}"
-        elif "Attestation Suivi Formation" in vals.get('name'):
-            result.auto_delete = False
-            result.report_template = self.env['ir.actions.report'].search([('name', '=', str("Attestation Suivi Formation"))], limit=1)
-            result.report_name = str(result.report_template.name) + "- ${object.display_name} - ${object.mcm_session_id.session_ville_id.display_name} - ${object.mcm_session_id.date_exam.strftime('%d/%m/%Y')}"
-        elif "Générer les relevés de notes" in vals.get('name'):
-            result.auto_delete = False
-            result.report_template = self.env['ir.actions.report'].search([('name', '=', str("Générer les relevés de notes"))], limit=1)
-            result.report_name = str(result.report_template.name) + "- ${object.display_name} - ${object.mcm_session_id.session_ville_id.display_name} - ${object.mcm_session_id.date_exam.strftime('%d/%m/%Y')}"
-        elif "Générer un relevé de connexion" in vals.get('name'):
+        report_name = "- ${object.display_name} - ${object.mcm_session_id.session_ville_id.display_name} - ${object.mcm_session_id.date_exam.strftime('%d/%m/%Y')}"
+        if result.model_id.name == "Contact":
+            if "Convocation" in vals.get('name'):  # Si mot CONVOCATION existe dans le nom de rapport
                 result.auto_delete = False
-                result.report_template = self.env['ir.actions.report'].search([('name', '=', str("Générer un relevé de connexion"))], limit=1)
-                result.report_name = str(result.report_template.name) + "- ${object.display_name} - ${object.mcm_session_id.session_ville_id.display_name} - ${object.mcm_session_id.date_exam.strftime('%d/%m/%Y')}"
-        elif "RAPPORT DE SESSION D’EXAMEN" in vals.get('name'):
-            result.auto_delete = False
-            result.report_template = self.env['ir.actions.report'].search([('name', '=', str("RAPPORT DE SESSION D’EXAMEN"))], limit=1)
-            result.report_name = str(result.report_template.name) + "- ${object.display_name} - ${object.mcm_session_id.session_ville_id.display_name} - ${object.mcm_session_id.date_exam.strftime('%d/%m/%Y')}"
+                result.report_template = self.env['ir.actions.report'].search([('name', '=', str("Convocation"))],
+                                                                              limit=1)
+                result.report_name = str(result.report_template.name) + report_name
+            elif "Cerfa" in vals.get('name'):
+                result.auto_delete = False
+                result.report_template = self.env['ir.actions.report'].search([('name', '=', str("Cerfa"))], limit=1)
+                result.report_name = str(result.report_template.name) + report_name
+            elif "Attestation de fin de formation" in vals.get('name'):
+                result.auto_delete = False
+                result.report_template = self.env['ir.actions.report'].search(
+                    [('name', '=', str("Attestation Suivi Formation"))], limit=1)
+                result.report_name = str(result.report_template.name) + report_name
+            elif "Relevé de notes" in vals.get('name'):
+                result.auto_delete = False
+                result.report_template = self.env['ir.actions.report'].search(
+                    [('name', '=', str("Générer les relevés de notes"))], limit=1)
+                result.report_name = str(result.report_template.name) + report_name
+            elif "Relevé de connexion" in vals.get('name'):
+                result.auto_delete = False
+                result.report_template = self.env['ir.actions.report'].search(
+                    [('name', '=', str("Générer un relevé de connexion"))], limit=1)
+                result.report_name = str(result.report_template.name) + report_name
+            elif "Procès verbal" in vals.get('name'):
+                result.auto_delete = False
+                result.report_template = self.env['ir.actions.report'].search(
+                    [('name', '=', str("RAPPORT DE SESSION D’EXAMEN"))], limit=1)
+                result.report_name = str(result.report_template.name) + report_name
         return result
