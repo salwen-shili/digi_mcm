5eb37a3034b2b789b723462d6158e81839da928e
khouloudachour <45214298+khouloud97@users.noreply.github.com>
2022-11-08 17:35:52 +0100

    add-sub-form

--- mcm_openedx/models/img.py
+++ mcm_openedx/models/img.py
@@ -1,6 +1,5 @@
 import base64
 import requests
-from jotform import *
 
 from odoo import models, fields
 from odoo.tools import json
@@ -25,7 +24,7 @@ class img(models.Model):
 
     def get_form(self):
         print("get events")
-        response = requests.get('https://eu-api.jotform.com/user/folders?apikey=98b07bd5ae3cd7054da0c386c4f699df')
+        response = requests.get('https://eu-api.jotform.com/user/folders?apikey=98b07bd5ae3cd7054da0c386c4f699df&limit=1000&orderby=status')
         form = response.json()["content"]
         formm = form["forms"]
         for formms in formm:
@@ -38,16 +37,8 @@ class img(models.Model):
             print(new)
             for existe in self.env['mcm_openedx.img'].sudo().search(
                     [('title', "like",self.title)]):
-                _logger.info("-----get form info-------")
-                response_form = requests.get(
-                    'https://eu-api.jotform.com/form/%s/submissions?apikey=98b07bd5ae3cd7054da0c386c4f699df' % (
-                        form_id))
-                form_info = response_form.json()["content"]
-                _logger.info("---------FORM submission--------")
-                _logger.info(form_info)
-                # for partner in self.env['res.partner'].search(
-                #        [('email', '=', email)]):
-                #   print()
+
+
                 if not existe:
                     new = self.env['mcm_openedx.img'].sudo().create({
                         'form_id': url.split("/")[3],
@@ -67,3 +58,18 @@ class form_info(models.Model):
 
     partner_id = fields.Many2one('res.partner')
     email = fields.Char(string="EMAIL")
+
+    def form_sub(self):
+        for existe in self.env['mcm_openedx.img'].sudo().search(
+                [('title', "like", self.title)]):
+            _logger.info("-----get form info-------")
+            response_form = requests.get(
+                'https://eu-api.jotform.com/form/%s/submissions?apikey=98b07bd5ae3cd7054da0c386c4f699df' % (
+                    existe.form_id))
+            form_info = response_form.json()["content"]
+            _logger.info("---------FORM submission--------")
+            _logger.info(form_info)
+            # for partner in self.env['res.partner'].search(
+            #        [('email', '=', email)]):
+            #   print()
+
