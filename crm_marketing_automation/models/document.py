74624425979950839639ecd4b06d29ce9c7bb3f9
takwa <tmejri@digimoov.fr>
2022-11-08 11:42:08 +0100

    Update state document if model source is sign.request

--- crm_marketing_automation/models/document.py
+++ crm_marketing_automation/models/document.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 #
@@ -6,19 +5,23 @@ from odoo import api, fields, models, _
 import calendar
 from datetime import date, datetime
 import logging
+
 _logger = logging.getLogger(__name__)
+
+
 class Document(models.Model):
     _inherit = 'documents.document'
+
     def write(self, vals):
         _logger.info(' write ')
         if 'state' in vals and not ('partner_id' in vals):
             if vals['state'] == 'waiting':
                 partner = self.partner_id
-                partner_=self.env['res.partner'].sudo().search([('id',"=",int(partner))])
+                partner_ = self.env['res.partner'].sudo().search([('id', "=", int(partner))])
                 if partner_ and not partner_.bolt:
                     _logger.info('if state in write  %s' % partner_.name)
                     self.change_stage_lead("Document non Validé", partner_)
-                if partner_ and  partner_.bolt:
+                if partner_ and partner_.bolt:
                     self.change_stage_lead("Bolt-Document non Validé", partner_)
             # """si les documents sont refusés, on classe l'apprenant bolt sous Non éligible"""
             # if vals['state'] == 'refused' :
@@ -34,7 +37,7 @@ class Document(models.Model):
                 partner_ = self.env['res.partner'].sudo().search([('id', "=", int(partner))])
                 if partner_ and not partner_.bolt:
                     self.change_stage_lead("Document non Validé", partner_)
-                if partner_ and  partner_.bolt:
+                if partner_ and partner_.bolt:
                     self.change_stage_lead("Bolt-Document non Validé", partner_)
         if not ('state' in vals) and 'partner_id' in vals:
             print('vals partner_id ')
@@ -43,10 +46,11 @@ class Document(models.Model):
                 partner_ = self.env['res.partner'].sudo().search([('id', "=", int(partner))])
                 if partner_ and not partner_.bolt:
                     self.change_stage_lead("Document non Validé", partner_)
-                if partner_ and  partner_.bolt:
+                if partner_ and partner_.bolt:
                     self.change_stage_lead("Bolt-Document non Validé", partner_)
         record = super(Document, self).write(vals)
         return record
+
     def change_stage_lead(self, statut, partner):
         if partner.name:
             partner.diviser_nom(partner)
@@ -56,8 +60,8 @@ class Document(models.Model):
             lead = self.env['crm.lead'].sudo().search([('partner_id', '=', partner.id)], limit=1)
             if lead and _(lead.stage_id.name) != statut:
                 lead.sudo().write({
-                     'prenom': partner.firstName if partner.firstName else "",
-                     'nom': partner.lastName if partner.lastName else "",
+                    'prenom': partner.firstName if partner.firstName else "",
+                    'nom': partner.lastName if partner.lastName else "",
                     'partner_name': partner.name,
                     'num_dossier': partner.numero_cpf if partner.numero_cpf else "",
                     'num_tel': partner.phone,
@@ -72,7 +76,7 @@ class Document(models.Model):
                 })
             if not lead:
                 lead = self.env['crm.lead'].sudo().create({
-                    'name':  partner.name if partner.name else "",
+                    'name': partner.name if partner.name else "",
                     'partner_name': partner.name,
                     'prenom': partner.firstName if partner.firstName else "",
                     'nom': partner.lastName if partner.lastName else "",
@@ -87,9 +91,14 @@ class Document(models.Model):
                 lead.mcm_session_id = partner.mcm_session_id if partner.mcm_session_id else False
                 lead.module_id = partner.module_id if partner.module_id else False
                 lead.company_id = partner.company_id if partner.company_id else False
+
     @api.model
     def create(self, vals):
         new_record = super(Document, self).create(vals)
-        if new_record.partner_id and new_record.datas :
-            new_record.partner_id.step = "financement" #change step of client to financement when the documents of client uploaded manually by our services
+        if new_record.partner_id and new_record.datas:
+            new_record.partner_id.step = "financement"  # change step of client to financement when the documents of client uploaded manually by our services
+        # res_model : Modèle de la ressource
+        # res_id : ID de la ressource
+        if new_record.res_model == "sign.request" and new_record.res_id is True:
+            new_record.state == 'validated'
         return new_record
