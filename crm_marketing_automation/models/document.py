e4a72195621c05310002a2c6427776fc0b8c2fb2
takwa <tmejri@digimoov.fr>
2022-11-09 13:28:42 +0100

    remplir contact

--- crm_marketing_automation/models/document.py
+++ crm_marketing_automation/models/document.py
@@ -107,7 +107,5 @@ class Document(models.Model):
                 sign_id = self.env['sign.request'].sudo().search(
                     [('id', '=', new_record['res_id'])
                      ], limit=1)
-                _logger.info('7777777777777 Sign ID 777777777777  %s' % sign_id)
                 new_record['partner_id'] = sign_id.request_item_ids.partner_id[0]
-                _logger.info('7777777777777 Partner ID 777777777777  %s' % sign_id.partner_id)
         return new_record
