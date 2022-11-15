360fe6993bbdfde624fce95fb5da4442868c17ea
Salwen-Shili <sshili@digimoov.fr>
2022-11-15 11:24:30 +0100

    adjust select option

--- mcm_website_theme/static/src/js/repassage_mcm.js
+++ mcm_website_theme/static/src/js/repassage_mcm.js
@@ -1,63 +1,20 @@
+var vtcAccess = null;
+var TaxiAccess = null;
+var btnRepassage = null;
+
 document.addEventListener('DOMContentLoaded', function () {
-    //get message and url redirection from backend
-    var url;
-    var message;
-    if (document.getElementById('url')) {
-      url = document.getElementById('url').value;
-    }
-  
-    if (document.getElementById('message')) {
-      message = document.getElementById('message').value;
-    }
-    //check in case of error to prevent displaying odoo js error
-    if (!document.getElementById('echec_examen').value) {
-      //change popup to the adequate message
-      //setting the url
-      document.getElementById('notifMessage').innerHTML = message;
-      document.getElementById('btnRepassage').href = url;
-    }
-    return;
-  });
-  // to open popup
-  function openPopup() {
-    document.getElementById('popup1').style.display = 'flex';
-  }
-    // to close popup
-  function closePopup() {
-    document.getElementById('popup1').style.display = 'none';
-  }
+  //State
+  var vtcAccess = null;
+  var TaxiAccess = null;
+  btnRepassage = document.getElementById("btn-repassage");
+
+
+
   
 
   //
 
-  //xmlhttprequest
-const sendHttpRequest = (method, url, data) => {
-  const promise = new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(method, url);
 
-    xhr.responseType = 'json';
-
-    if (data) {
-      xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-    }
-
-    xhr.onload = () => {
-      if (xhr.status >= 400) {
-        reject(xhr.response);
-      } else {
-        resolve(xhr.response);
-      }
-    };
-
-    xhr.onerror = () => {
-      reject('Something went wrong!');
-    };
-
-    xhr.send(JSON.stringify(data));
-  });
-  return promise;
-};
 
 // const getInfoRepassage = (condition) => {
 //   sendHttpRequest("POST", "/shop/payment/update_condition", {
@@ -68,28 +25,78 @@ const sendHttpRequest = (method, url, data) => {
 //     .then((responseData) => {})
 //     .catch((err) => {});
 // };
+       // to open popup
+     
 
+  });
+
+
+
+  
 function getRepassageInfo(){
   console.log("get")
 }
 
-function onChangeSelect(formation, index){
-  console.log(formation, index)
- switch (formation) {
-  case "VTC":
-    checkVTC();
-    break;
-  case "TAXI":
-    checkTAXI();
-  default:
-    break;
- }
+function openPopup() {
+  document.getElementById('popup1').style.display = 'flex';
+}
+  // to close popup
+function closePopup() {
+  document.getElementById('popup1').style.display = 'none';
 }
 
-function checkVTC(){
+function onChangeSelect(formation, index){
+console.log(formation, index)
+switch (formation) {
+case "VTC":
+checkVTC();
+break;
+case "TAXI":
+checkTAXI();
+default:
+break;
+}
+}
 
+
+function checkVTC(){
+  vtcAccess? btnRepassage.classList.remove('hide') : btnRepassage.classList.add('hide')
 }
 
 function checkTAXI(){
-  
+  TaxiAccess? btnRepassage.classList.remove('hide') : btnRepassage.classList.add('hide')
 }
+
+
+
+ 
+  //xmlhttprequest
+  const sendHttpRequest = (method, url, data) => {
+    const promise = new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+  
+      xhr.responseType = 'json';
+  
+      if (data) {
+        xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+      }
+  
+      xhr.onload = () => {
+        if (xhr.status >= 400) {
+          reject(xhr.response);
+        } else {
+          resolve(xhr.response);
+        }
+      };
+  
+      xhr.onerror = () => {
+        reject('Something went wrong!');
+      };
+  
+      xhr.send(JSON.stringify(data));
+    });
+    return promise;
+  };
+
+
