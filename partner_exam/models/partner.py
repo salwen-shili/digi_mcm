2419ace6929bfebfb6cc6aa882d08e959f5ef102
takwa <tmejri@digimoov.fr>
2022-11-24 11:59:35 +0100

    Add logger info for evalbox

--- partner_exam/models/partner.py
+++ partner_exam/models/partner.py
@@ -45,7 +45,9 @@ class resComapny(models.Model):
     def print_report_name_partner(self):
         """ La fonction sera utilisée dans les noms de rapport en format PDF de l'interface de contact."""
         self.ensure_one()
-        return '- %s - %s - %s' % (self.display_name, self.mcm_session_id.session_ville_id.display_name, self.mcm_session_id.date_exam.strftime(
+        return '- %s - %s - %s' % (
+            self.display_name, self.mcm_session_id.session_ville_id.display_name,
+            self.mcm_session_id.date_exam.strftime(
                 '%d/%m/%Y'))
 
     def _get_report_base_filename(self):
@@ -133,9 +135,9 @@ class resComapny(models.Model):
             jours = relativedelta(date_exam, dt).months
             self.age = str(rd) + "ans" + " " + str(months) + "mois" + " " + str(
                 jours) + "jours"  # Affectation de l'age au champ age dans res.partner (année + mois)
-            _logger.info('rec.age date of birth-------------11111111111111111111-------- %s', self.age)
         if (
                 'nom_evalbox' in values or 'prenom_evalbox' in values or 'mcm_session_id' in values) and self.company_id.id == 2:  # If we have changed this fields
+            _logger.info("First if Evalbox in write function")
             if 'mcm_session_id' in values:
                 eval_name_actuel = self.nom_evalbox[1:0] if self.nom_evalbox else ''
                 eval_name = str(self.mcm_session_id.session_ville_id.name_ville[
@@ -143,9 +145,10 @@ class resComapny(models.Model):
                 self.nom_evalbox = eval_name
                 self.code_evalbox = eval_name + str(
                     self.prenom_evalbox)  # Update code evalbox and # To concatenate (combine) multiple fields
-                _logger.info("Get first three characters of a string session ville %s" % str(eval_name_actuel))
+                _logger.info("Self prénom evalbox §§§§§ if mcm_session_id §§§§§ %s" % str(self.prenom_evalbox))
             else:
                 self.code_evalbox = str(self.nom_evalbox) + str(self.prenom_evalbox)
+                _logger.info("Self Code evalbox ##### else ##### %s" % str(self.code_evalbox))
         return session
 
     @api.model
@@ -153,7 +156,6 @@ class resComapny(models.Model):
         """ Lors de la création d'une nouvelle fiche client le nom evalbox sera rempli par
         un alphabet et prénom evalbox par séquence des nombres puis ça sera concaténé dans le champ evalbox"""
         res = super(resComapny, self).create(vals)
-        _logger.info("res %s" % str(res))
         if res.company_id.id == 2:
             res.prenom_evalbox = self.env['ir.sequence'].next_by_code(
                 'res.partner') or '/'  # Affectation: Generate a sequence number to prenom_evalbox field
@@ -166,7 +168,8 @@ class resComapny(models.Model):
                 res.prenom_evalbox = ir_sequence.number_next_actual
 
                 char = ir_sequence.alphabet  # Global variable char to get alphabet from the search in sequence class
-                print("char ///", char)
+                _logger.info(
+                    "ir_sequence.number_next_actual evalbox ##### if (create function)##### %s" % str(self.char))
                 if chr(ord(char) + 1) == "[":  # refaire boucle apres "Z" ==> "[" : le champ alphabet sera égale à "A"
                     ir_sequence.alphabet = "A"
                     char = ir_sequence.alphabet
@@ -175,31 +178,43 @@ class resComapny(models.Model):
                         res.nom_evalbox = str(res.mcm_session_id.session_ville_id.name_ville[0:3]).upper() + "-" + char
                         res.code_evalbox = str(res.nom_evalbox) + str(
                             res.prenom_evalbox)  # To concatenate (combine) multiple fields
+                        _logger.info(
+                            "Create function €€€€€ if res.mcm_session_id €€€€€€ %s" % str(
+                                res.code_evalbox))
                     else:
                         res.code_evalbox = str(res.nom_evalbox) + str(
                             res.prenom_evalbox)  # To concatenate (combine) multiple fields
+                        _logger.info(
+                            "Create function %%%%% else res.mcm_session_id %%%%%% %s" % str(
+                                res.code_evalbox))
                 else:
                     char = chr(ord(char) + 1)
                     res.nom_evalbox = char
+                    _logger.info("else %% create" % str(res.nom_evalbox))
                     if res.mcm_session_id:
                         res.nom_evalbox = str(res.mcm_session_id.session_ville_id.name_ville[0:3]).upper() + "-" + char
                         res.code_evalbox = str(res.nom_evalbox) + str(
                             res.prenom_evalbox)  # To concatenate (combine) multiple fields
+                        _logger.info("code_evalbox µµµ       create       µµµ %s" % str(res.code_evalbox))
                     else:
                         res.code_evalbox = str(res.nom_evalbox) + str(
                             res.prenom_evalbox)  # To concatenate (combine) multiple fields
+                        _logger.info("else code_evalbox ££££       else      ££££ %s" % str(res.code_evalbox))
 
             else:  # If number_next_actual != 100000
                 char = ir_sequence.alphabet
                 # ir_sequence.alphabet = char
                 res.nom_evalbox = char  # Get alphabet from ir.sequence class
                 res.prenom_evalbox = ir_sequence.number_next_actual
+                _logger.info("else prenom_evalbox +++++++++      else      ++++++++ %s" % str(res.prenom_evalbox))
                 if res.mcm_session_id:
                     res.nom_evalbox = str(res.mcm_session_id.session_ville_id.name_ville[0:3]).upper() + "-" + char
                     res.code_evalbox = str(res.nom_evalbox) + str(
                         res.prenom_evalbox)  # To concatenate (combine) multiple fields
+                    _logger.info("IF code_evalbox ££££       IF      ££££ %s" % str(res.code_evalbox))
                 else:
                     res.code_evalbox = str(
                         res.nom_evalbox) + str(
                         res.prenom_evalbox)  # To concatenate (combine) multiple fields
+                    _logger.info("else not session_id evalbox ££££       else      ££££ %s" % str(res.code_evalbox))
         return res
