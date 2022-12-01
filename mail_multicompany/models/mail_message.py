9d228853fee75f3befcbf0ea35dae7154492ade0
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2022-12-01 14:42:22 +0100

    fix concurrent update ir config parametre

--- mail_multicompany/models/mail_message.py
+++ mail_multicompany/models/mail_message.py
@@ -1,7 +1,9 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import api, fields, models, exceptions
+import logging
 
+_logger = logging.getLogger(__name__)
 
 class MailMessage(models.Model):
 
@@ -15,12 +17,19 @@ class MailMessage(models.Model):
         alias=self.env['res.config.settings'].sudo().write({
             'alias_domain':self.env.company.alias_domain,
         })
+        _logger.info("res config setting alias domain : % et ")
         # alias_domain= self.env['ir.config_parameter'].sudo().search([('key', "=",'mail.catchall.domain')])
         # if alias_domain:
         #     alias_domain.sudo().write({'value':self.env.company.alias_domain})
         icp_domain = self.env["ir.config_parameter"].sudo().get_param("mail.catchall.domain") #get mail.catchall.domain parameter
+        _logger.info("res config setting alias domain : % et ir config paramètres mail catchall domain : %s" %(str(alias),str(icp_domain)))
         if alias != icp_domain: #check difference between parametre mail.catchall.domain and alias domain
-            self.env["ir.config_parameter"].sudo().set_param("mail.catchall.domain", self.env.company.alias_domain)
+            try :
+                self.env["ir.config_parameter"].sudo().set_param("mail.catchall.domain", self.env.company.alias_domain)
+                self._cr.commit()
+            except Exception:
+                _logger.exception("Failure to update mail catchall domain from %s to %s" %(str(icp_domain),str(self.env.company.alias_domain)))
+                
         for vals in values_list:
             if vals.get("model") and vals.get("res_id"):
                 current_object = self.env[vals["model"]].browse(vals["res_id"])
