32f30be6fc80596f084fc5280155c461031fca63
takwa <tmejri@digimoov.fr>
2022-12-06 16:49:11 +0100

    Add new condition in cretae function mail template

--- partner_exam/models/inherit_mail_template.py
+++ partner_exam/models/inherit_mail_template.py
@@ -14,21 +14,21 @@ class InheritMailTemplate(models.Model):
         result = super(InheritMailTemplate, self).create(vals)
         report_name = "- ${object.display_name} - ${object.mcm_session_id.session_ville_id.display_name} - ${object.mcm_session_id.date_exam.strftime('%d/%m/%Y')}"
         report_name_exam = "- ${object.partner_id.display_name} - ${object.session_id.session_ville_id.display_name} - ${object.session_id.date_exam.strftime('%d/%m/%Y')}"
-        if result.model_id.model == "res.partner" or result.model_id.model == "	info.examen":
+        if result.model_id.model == "res.partner" or result.model_id.model == "info.examen":
             if "Convocation" in vals.get('name'):  # Si mot CONVOCATION existe dans le nom de rapport
                 result.auto_delete = False
                 result.report_template = self.env['ir.actions.report'].search([('name', '=', str("Convocation"))],
                                                                               limit=1)
                 if result.model_id.model == "res.partner":
                     result.report_name = str(result.report_template.name) + report_name
-                elif result.model_id.model == "	info.examen":
+                elif result.model_id.model == "info.examen":
                     result.report_name = str(result.report_template.name) + report_name_exam
             elif "Cerfa" in vals.get('name'):
                 result.auto_delete = False
                 result.report_template = self.env['ir.actions.report'].search([('name', '=', str("Cerfa"))], limit=1)
                 if result.model_id.model == "res.partner":
                     result.report_name = str(result.report_template.name) + report_name
-                elif result.model_id.model == "	info.examen":
+                elif result.model_id.model == "info.examen":
                     result.report_name = str(result.report_template.name) + report_name_exam
             elif "Attestation de fin de formation" in vals.get('name'):
                 result.auto_delete = False
@@ -36,14 +36,15 @@ class InheritMailTemplate(models.Model):
                     [('name', '=', str("Attestation Suivi Formation"))], limit=1)
                 if result.model_id.model == "res.partner":
                     result.report_name = str(result.report_template.name) + report_name
-                result.report_name = str(result.report_template.name) + report_name_exam
+                elif result.model_id.model == "info.examen":
+                    result.report_name = str(result.report_template.name) + report_name_exam
             elif "Relevé de notes" in vals.get('name'):
                 result.auto_delete = False
                 result.report_template = self.env['ir.actions.report'].search(
                     [('name', '=', str("Générer les relevés de notes"))], limit=1)
                 if result.model_id.model == "res.partner":
                     result.report_name = str(result.report_template.name) + report_name
-                elif result.model_id.model == "	info.examen":
+                elif result.model_id.model == "info.examen":
                     result.report_name = str(result.report_template.name) + report_name_exam
             elif "Relevé de connexion" in vals.get('name'):
                 result.auto_delete = False
@@ -51,7 +52,7 @@ class InheritMailTemplate(models.Model):
                     [('name', '=', str("Générer un relevé de connexion"))], limit=1)
                 if result.model_id.model == "res.partner":
                     result.report_name = str(result.report_template.name) + report_name
-                elif result.model_id.model == "	info.examen":
+                elif result.model_id.model == "info.examen":
                     result.report_name = str(result.report_template.name) + report_name_exam
             elif "Procès verbal" in vals.get('name'):
                 result.auto_delete = False
@@ -59,6 +60,6 @@ class InheritMailTemplate(models.Model):
                     [('name', '=', str("RAPPORT DE SESSION D’EXAMEN"))], limit=1)
                 if result.model_id.model == "res.partner":
                     result.report_name = str(result.report_template.name) + report_name
-                elif result.model_id.model == "	info.examen":
+                elif result.model_id.model == "info.examen":
                     result.report_name = str(result.report_template.name) + report_name_exam
         return result
