d2c0179ffb6eb1df73d6a013976e5b549eb145c3
takwa <tmejri@digimoov.fr>
2022-12-09 12:23:01 +0100

    Add pack ubereats in report proces verbal

--- digimoov_sessions_modules/models/sign_request.py
+++ digimoov_sessions_modules/models/sign_request.py
@@ -133,25 +133,26 @@ class InheritSignRequest(models.Model):
         return True
 
 
-class InheritMcmacademySession(models.Model):
-    _inherit = "mcmacademy.session"
-
-    def send_cerfa_to_sign(self):
-        """ 1- Génèrer un rapport cerfa"""
-        partner = self.env['res.partner'].sudo().search(
-                [('email', '=', 'tmejri@digimoov.fr')], limit=1)
-        if partner:
-            # Attach report to the Bank statement
-            content, content_type = self.env.ref('partner_exam.report_cerfa').render_qweb_pdf(
-                partner.id)
-            self.env['ir.attachment'].create({
-                'name': "Takwa cerfa 2022 Test 1",
-                'type': 'binary',
-                'datas': base64.encodestring(content),
-                'res_model': partner._name,
-                'res_id': partner.id
-            })
-            _logger.info('----send_cerfa_to_sign ---- %s' % content)
+# class InheritMcmacademySession(models.Model):
+#     _inherit = "mcmacademy.session"
+#
+#     def send_cerfa_to_sign(self):
+#         """ 1- Génèrer un rapport cerfa"""
+#         partner = self.env['res.partner'].sudo().search(
+#                 [('email', '=', 'tmejri@digimoov.fr')], limit=1)
+#         if partner:
+#             # Attach report to the Bank statement
+#             content, content_type = self.env.ref('partner_exam.report_cerfa').render_qweb_pdf(
+#                 partner.id)
+#             self.env['ir.attachment'].create({
+#                 'name': "Takwa cerfa 2022 Test 1",
+#                 'type': 'binary',
+#                 'datas': base64.encodestring(content),
+#                 'res_model': partner._name,
+#                 'res_id': partner.id
+#             })
+#             _logger.info('----send_cerfa_to_sign ---- %s' % content)
+#             self.env['sign.template'].create({})
 
 class InheritSignRequestItem(models.Model):
     _inherit = "sign.request.item"
