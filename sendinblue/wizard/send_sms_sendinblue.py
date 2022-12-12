7e09e65d11e5fd86cfb6c2502e952ce7ac9d5365
khouloudachour <45214298+khouloud97@users.noreply.github.com>
2022-12-12 14:48:22 +0100

    add-wizard

--- sendinblue/wizard/send_sms_sendinblue.py
+++ sendinblue/wizard/send_sms_sendinblue.py
@@ -23,3 +23,22 @@ class sms_sendinblue(models.Model):
                              ])
     def sendsms(self):
         _logger.info("testttttttttttt")
+
+        url = "https://api.sendinblue.com/v3/transactionalSMS/sms"
+        partner_data =self.env['res.partner'].sudo().search([])
+        payload = {
+            "type": "marketing",
+            "unicodeEnabled": False,
+            "sender":  partner_data.company_id.name,
+            "recipient":  partner_data.phone,
+            "content": "test api sendinblue"
+        }
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "api-key": "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"
+        }
+
+        response = requests.post(url, json=payload, headers=headers)
+
+        print(response.text)
