ae26e0fc9c26237b914865228315ee46ee7b3720
khouloudachour <45214298+khouloud97@users.noreply.github.com>
2022-12-12 14:16:09 +0100

    rm_sendinblue_wizzard

--- sendinblue/wizard/__init__.py
+++ sendinblue/wizard/__init__.py
@@ -2,3 +2,4 @@ from . import import_export_operation_wizard
 from . import test_mailing
 from . import mass_mailing_schedule_date
 from . import partner_export_update_wizard
+from . import send_sms_sendinblue
---
+++ sendinblue/wizard/send_sms_sendinblue.py
@@ -0,0 +1,25 @@
+import base64
+from urllib import parse
+
+import requests
+
+from odoo import models, fields
+from odoo.tools import json
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class sms_sendinblue(models.Model):
+    _name = 'sendinblue.sendinbluesms'
+    _description = "Sendinblue"
+
+    sender = fields.Char(string="ID")
+    recipient = fields.Char(string="ID")
+    content = fields.Char(string="ID")
+    type = fields.Selection([('marketing', 'Marketing'),
+                             ('transactional', ' Transactional'),
+                             ])
+    def sendsms(self):
+        _logger.info("testttttttttttt")
