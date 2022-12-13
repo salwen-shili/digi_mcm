fe8c85d363a9121a3de932ef6fab3eb552624e14
khouloudachour <45214298+khouloud97@users.noreply.github.com>
2022-12-13 09:43:55 +0100

    add-currentuser

--- sendinblue/wizard/send_sms_sendinblue.py
+++ sendinblue/wizard/send_sms_sendinblue.py
@@ -16,26 +16,36 @@ class sms_sendinblue(models.TransientModel):
     _name = 'sendinblue.sendinbluesms'
     _description = "Sendinblue"
 
-    sender = fields.Char(string="Sender")
     recipient = fields.Char(string="Recipient")
     content = fields.Text(string="Body")
     type = fields.Selection([('marketing', 'Marketing'),
                              ('transactional', ' Transactional'),
                              ])
     # recipients
-    partner_ids = fields.Many2one('res.users','Current User', default=lambda self: self.env.uid)
-
+    partner_ids = fields.Many2one('res.users', 'Current User', default=lambda self: self.env.uid)
     company_ids = fields.Many2many('res.company')
-    numbers = fields.Char('Recipients (Numbers)')
     sanitized_numbers = fields.Char('Sanitized Number', compute='_compute_sanitized_numbers', compute_sudo=False)
 
-    def get_user_id(record):
-        return record.env.uid
+    def get_user_phone(self):
+        user_phone = self.env['res.partner'].browse(self.env.context.get('active_ids'))
+        return user_phone.phone
 
-    current_user = fields.Many2one('res.users', 'Current User', default= get_user_id)
+    numbers = fields.Integer('Recipients (Numbers)', default=get_user_phone)
+
+    def get_user_id(self):
+        user_phone = self.env['res.partner'].browse(self.env.context.get('active_ids'))
+
+        return user_phone
+
+    current_user = fields.Many2one('res.partner', 'Current User', default=get_user_id)
+
+    def get_sneder(self):
+        sender_name = self.env['res.partner'].browse(self.env.context.get('active_ids'))
+        return sender_name.company_id.name
+
+    sender = fields.Char(string="Sender", default= get_sneder)
 
     def sendsms(self):
-
         _logger.info("sendinblue sms")
         url = "https://api.sendinblue.com/v3/transactionalSMS/sms"
         payload = {
