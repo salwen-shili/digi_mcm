ef566d9e8d2b65a271e9139b064ef5c16edcaaed
takwa <tmejri@digimoov.fr>
2022-12-15 11:10:43 +0100

    Affectation folders to sign template

--- digimoov_sessions_modules/models/sign_template.py
+++ digimoov_sessions_modules/models/sign_template.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models,_
+from odoo import fields, models, _
 import logging
 
 from odoo import fields, models, _, api, http
 
 _logger = logging.getLogger(__name__)
-
 
 
 class SignRequestTemplate(models.Model):
@@ -23,4 +22,37 @@ class SignRequestTemplate(models.Model):
         if "CERFA" in res.name:
             res.redirect_url = str("https://form.jotform.com/222334146537352")
             res.redirect_url_text = str("Importer vos documents")
+
+        folder_exist = self.env['documents.folder'].sudo().search(
+            [('name', '=', "CERFA")], limit=1)
+
+        # Create a folder with date exam if not exist in document module to save signed document
+        # If folder CERFA exist
+        if folder_exist:
+            # if folder CERFA exist
+            res.template_id.folder_id = folder_exist.id
+        else:
+            folder_list = {
+                'name': "CERFA",
+                'parent_folder_id': False,
+                'company_id': self.env.company.id,
+            }
+            cerfa_f_name = self.env['documents.folder'].sudo().create(folder_list)
+        _logger.info('----Folder name---- %s' % folder_exist)
+
+        folder_name = res.template_id.name.split()
+        # Get last text element(folder_name) = date exam
+        f_name = folder_name[-1]
+        f_name_date_exam = self.env['documents.folder'].sudo().search(
+            [('name', '=', f_name)], limit=1).id
+        if f_name_date_exam:
+            res.template_id.folder_id.parent_folder_id = f_name_date_exam
+        else:
+            folder_list = {
+                'name': f_name,
+                'parent_folder_id': folder_exist.id,
+                'company_id': self.env.company.id,
+            }
+            create_folder = self.env['documents.folder'].sudo().create(folder_list)
+            res.template_id.folder_id = create_folder
         return res
