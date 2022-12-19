241595f9e66f8410fb2933f73f5ada54e1423b69
Houssem Ben Mbarek <houssem.ben.mbarek@gmail.com>
2022-12-19 15:21:18 +0100

    comment _notify_get_reply_to

--- prt_email_from/models/mail.py
+++ prt_email_from/models/mail.py
@@ -90,66 +90,66 @@ class MailMessage(models.Model):
 class MailThread(models.AbstractModel):
     _inherit = "mail.thread"
 
-    def _notify_get_reply_to(self, default=None, records=None, company=None, doc_names=None):
-        """
-        Cetmix. Based on settings prepend company name with user's name
-        This is a copy-paste of the base method. Need to check it regularly for possible changes
-        """
-        _records = self if self and self._name != 'mail.thread' else records
-        model = _records._name if _records and _records._name != 'mail.thread' else False
-        res_ids = _records.ids if _records and model else []
-        _res_ids = res_ids or [False]  # always have a default value located in False
-        try :
-            alias_domain = self.env['ir.config_parameter'].sudo().get_param("mail.catchall.domain")
-        except Exception as e:
-            alias_domain = False
-            _logger.exception("Failure get_param mail.catchall.domain %s" %(str(e)))
-        result = dict.fromkeys(_res_ids, False)
-        result_email = dict()
-        doc_names = doc_names if doc_names else dict()
-
-        if alias_domain:
-            if model and res_ids:
-                if not doc_names:
-                    doc_names = dict((rec.id, rec.display_name) for rec in _records)
-
-                mail_aliases = self.env['mail.alias'].sudo().search([
-                    ('alias_parent_model_id.model', '=', model),
-                    ('alias_parent_thread_id', 'in', res_ids),
-                    ('alias_name', '!=', False)])
-                # take only first found alias for each thread_id, to match order (1 found -> limit=1 for each res_id)
-                for alias in mail_aliases:
-                    result_email.setdefault(alias.alias_parent_thread_id, '%s@%s' % (alias.alias_name, alias_domain))
-
-            # left ids: use catchall
-            left_ids = set(_res_ids) - set(result_email)
-            if left_ids:
-                catchall = self.env['ir.config_parameter'].sudo().get_param("mail.catchall.alias")
-                if catchall:
-                    result_email.update(dict((rid, '%s@%s' % (catchall, alias_domain)) for rid in left_ids))
-
-            # compute name of reply-to - TDE tocheck: quotes and stuff like that
-            # Cetmix
-            company_id = company if company else self.env.user.company_id
-
-            # Prepend with user's name
-            if company_id.add_sender_reply_to:
-                if company_id.email_joint:
-                    company_name = " ".join((self.env.user.name, company_id.email_joint, company_id.name))
-                else:
-                    company_name = " ".join((self.env.user.name, company_id.name))
-            else:
-                company_name = company_id.name
-
-            for res_id in result_email.keys():
-                name = '%s%s%s' % (company_name, ' ' if doc_names.get(res_id) else '', doc_names.get(res_id, ''))
-                result[res_id] = formataddr((name, result_email[res_id]))
-
-        left_ids = set(_res_ids) - set(result_email)
-        if left_ids:
-            result.update(dict((res_id, default) for res_id in left_ids))
-
-        return result
+    # def _notify_get_reply_to(self, default=None, records=None, company=None, doc_names=None):
+    #     """
+    #     Cetmix. Based on settings prepend company name with user's name
+    #     This is a copy-paste of the base method. Need to check it regularly for possible changes
+    #     """
+    #     _records = self if self and self._name != 'mail.thread' else records
+    #     model = _records._name if _records and _records._name != 'mail.thread' else False
+    #     res_ids = _records.ids if _records and model else []
+    #     _res_ids = res_ids or [False]  # always have a default value located in False
+    #     try :
+    #         alias_domain = self.env['ir.config_parameter'].sudo().get_param("mail.catchall.domain")
+    #     except Exception as e:
+    #         alias_domain = False
+    #         _logger.exception("Failure get_param mail.catchall.domain %s" %(str(e)))
+    #     result = dict.fromkeys(_res_ids, False)
+    #     result_email = dict()
+    #     doc_names = doc_names if doc_names else dict()
+    # 
+    #     if alias_domain:
+    #         if model and res_ids:
+    #             if not doc_names:
+    #                 doc_names = dict((rec.id, rec.display_name) for rec in _records)
+    # 
+    #             mail_aliases = self.env['mail.alias'].sudo().search([
+    #                 ('alias_parent_model_id.model', '=', model),
+    #                 ('alias_parent_thread_id', 'in', res_ids),
+    #                 ('alias_name', '!=', False)])
+    #             # take only first found alias for each thread_id, to match order (1 found -> limit=1 for each res_id)
+    #             for alias in mail_aliases:
+    #                 result_email.setdefault(alias.alias_parent_thread_id, '%s@%s' % (alias.alias_name, alias_domain))
+    # 
+    #         # left ids: use catchall
+    #         left_ids = set(_res_ids) - set(result_email)
+    #         if left_ids:
+    #             catchall = self.env['ir.config_parameter'].sudo().get_param("mail.catchall.alias")
+    #             if catchall:
+    #                 result_email.update(dict((rid, '%s@%s' % (catchall, alias_domain)) for rid in left_ids))
+    # 
+    #         # compute name of reply-to - TDE tocheck: quotes and stuff like that
+    #         # Cetmix
+    #         company_id = company if company else self.env.user.company_id
+    # 
+    #         # Prepend with user's name
+    #         if company_id.add_sender_reply_to:
+    #             if company_id.email_joint:
+    #                 company_name = " ".join((self.env.user.name, company_id.email_joint, company_id.name))
+    #             else:
+    #                 company_name = " ".join((self.env.user.name, company_id.name))
+    #         else:
+    #             company_name = company_id.name
+    # 
+    #         for res_id in result_email.keys():
+    #             name = '%s%s%s' % (company_name, ' ' if doc_names.get(res_id) else '', doc_names.get(res_id, ''))
+    #             result[res_id] = formataddr((name, result_email[res_id]))
+    # 
+    #     left_ids = set(_res_ids) - set(result_email)
+    #     if left_ids:
+    #         result.update(dict((res_id, default) for res_id in left_ids))
+    # 
+    #     return result
 
 
 ###############
