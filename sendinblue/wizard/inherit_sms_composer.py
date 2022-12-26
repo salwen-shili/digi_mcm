a2cf5e2e00feb81280bbc9b51a7d80a8675a6df9
khouloudachour <45214298+khouloud97@users.noreply.github.com>
2022-12-26 11:05:18 +0100

    inherit-sms-function

--- sendinblue/wizard/inherit_sms_composer.py
+++ sendinblue/wizard/inherit_sms_composer.py
@@ -17,14 +17,15 @@ class InheritSmsComposer(models.TransientModel):
         # recuperer les clé api
         api_key = self.env['sendinblue.accounts'].sudo().search([('api_key', '!=', False)])
         _logger.info(api_key.api_key)
+        records = self._get_records()
 
         url = "https://api.sendinblue.com/v3/transactionalSMS/sms"
 
         payload = {
             'type': "transactional",
             'unicodeEnabled': False,
-            'sender': "DIGIMOOV",
-            'recipient':  self.partner_ids.mobile  or self.partner_ids.phone,
+            'sender':  records.company_id.phone,
+            'recipient': self.partner_ids.mobile or self.partner_ids.phone or records.phone,
             'content': self.body
         }
         headers = {
@@ -34,14 +35,35 @@ class InheritSmsComposer(models.TransientModel):
         }
 
         response = requests.post(url, json=payload, headers=headers)
+        note_tag = "<b>" + " Sent 📨📨 À :  " + records.name + " " "</b><br/>"
+        # if 201 message envoyée
+        # add message id-track
+        response_text = response.json()
 
-        _logger.info(self.partner_ids[0].mobile or self.partner_ids[0].phone)
+        if (response.status_code == 201):
+            messeageid = response_text["messageId"]  # if 201 message envoyée
+
+            values = {
+                'record_name': records.name,
+                'model': 'res.partner',
+                'subject': messeageid,
+                'message_type': 'comment',
+                'subtype_id': records.env['mail.message.subtype'].search([('name', '=', 'Note')]).id,
+                'res_id': records.id,
+                'author_id': records.env.user.partner_id.id,
+                'date': datetime.now(),
+                'body': note_tag + "\n" + self.body
+            }
+            records.env['mail.message'].sudo().create(values)
+        _logger.info(records.name)
+        _logger.info(records.phone)
         _logger.info(response.status_code)
         statut_code_sendinblue = response.status_code
         return statut_code_sendinblue
 
     def _action_send_sms(self):
         if self.sendsms() == 201:
+
             return False
             _logger.info("_____________SMS.COMPoSER___________ %s", self.sendsms())
         else:
