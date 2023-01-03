2dcfca49bc86ad31708840a24015dd54919cdb5a
takwa <tmejri@digimoov.fr>
2023-01-03 15:56:54 +0100

    Comment page inherit to add ville

--- session_ville/__manifest__.py
+++ session_ville/__manifest__.py
@@ -28,7 +28,7 @@
         'views/inherit_info_exam.xml',
         'views/inherit_res_partner.xml',
         'views/res_country_state.xml',
-        'views/inherit_mcmacademy_module.xml',
+        # 'views/inherit_mcmacademy_module.xml',
         'views/res_partner_adresse_view.xml',
         'views/approval_number.xml',
     ],
