6231464895f591af00407da9a10270ee42190aea
khouloudachour <45214298+khouloud97@users.noreply.github.com>
2023-01-12 17:09:23 +0100

    add many recipients

--- sendinblue/wizard/inherit_sms_composer.py
+++ sendinblue/wizard/inherit_sms_composer.py
@@ -20,45 +20,50 @@ class InheritSmsComposer(models.TransientModel):
         records = self._get_records()
 
         url = "https://api.sendinblue.com/v3/transactionalSMS/sms"
+        selected_ids = self.env.context.get('active_ids', [])
+        selected_records = self.env['res.partner'].browse(selected_ids)
+        for i_sms in selected_records:
+            _logger.info(i_sms.name)
 
-        payload = {
-            'type': "transactional",
-            'unicodeEnabled': False,
-            'sender': records.company_id.name,
-            'recipient': self.partner_ids.mobile or self.partner_ids.phone or records.phone,
-            'content': self.body
-        }
-        headers = {
-            "accept": "application/json",
-            "content-type": "application/json",
-            "api-key": api_key.api_key
-        }
-
-        response = requests.post(url, json=payload, headers=headers)
-        note_tag = "<b>" + " Sent 📨📨 À :  " + records.name + " " "</b><br/>"
-        # if 201 message envoyée
-        # add message id-track
-        response_text = response.json()
-
-        if (response.status_code == 201):
-            messeageid = response_text["messageId"]  # if 201 message envoyée
-
-            values = {
-                'record_name': records.name,
-                'model': 'res.partner',
-                'subject': messeageid,
-                'message_type': 'comment',
-                'subtype_id': records.env['mail.message.subtype'].search([('name', '=', 'Note')]).id,
-                'res_id': records.id,
-                'author_id': records.env.user.partner_id.id,
-                'date': datetime.now(),
-                'body': note_tag + "\n" + self.body
+        for i_sms in selected_records:
+            payload = {
+                'type': "transactional",
+                'unicodeEnabled': False,
+                'sender': i_sms.company_id.name,
+                'recipient': i_sms.phone ,
+                'content':self.body
             }
-            records.env['mail.message'].sudo().create(values)
-        _logger.info(records.name)
-        _logger.info(records.phone)
-        _logger.info(response.status_code)
-        statut_code_sendinblue = response.status_code
+            headers = {
+                "accept": "application/json",
+                "content-type": "application/json",
+                "api-key": api_key.api_key
+            }
+
+            response = requests.post(url, json=payload, headers=headers)
+            note_tag = "<b>" + " Sent 📨📨 À :  " + i_sms.name + " " "</b><br/>"
+            # if 201 message envoyée
+            # add message id-track
+            response_text = response.json()
+
+            if (response.status_code == 201):
+                messeageid = response_text["messageId"]  # if 201 message envoyée
+
+                values = {
+                    'record_name': i_sms.name,
+                    'model': 'res.partner',
+                    'subject': messeageid,
+                    'message_type': 'comment',
+                    'subtype_id': i_sms.env['mail.message.subtype'].search([('name', '=', 'Note')]).id,
+                    'res_id': i_sms.id,
+                    'author_id': i_sms.env.user.partner_id.id,
+                    'date': datetime.now(),
+                    'body': note_tag + "\n" + self.body
+                }
+                records.env['mail.message'].sudo().create(values)
+            _logger.info(i_sms.name)
+            _logger.info(i_sms.phone)
+            _logger.info(response.status_code)
+            statut_code_sendinblue = response.status_code
         return statut_code_sendinblue
 
     def _action_send_sms(self):
