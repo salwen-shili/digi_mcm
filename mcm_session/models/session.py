a441a1c97b497315f621d6980c2ed46839e5ddf7
takwa <tmejri@digimoov.fr>
2023-01-26 16:02:27 +0100

    Add date exam in staging

--- mcm_session/models/session.py
+++ mcm_session/models/session.py
@@ -706,7 +706,7 @@ class Session(models.Model):
         """
         stage_ids = self.env['mcmacademy.stage'].search(
             [('name', "!=", _('Planifiées')), ('name', "!=", _('Terminées'))],
-            order="date_exam asc")
+            order="date_exam desc")
         return stage_ids
 
     @api.depends('client_ids', 'prospect_ids', 'canceled_prospect_ids')
