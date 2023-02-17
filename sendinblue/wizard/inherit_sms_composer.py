d80d3a15fa8770fdfb12398f556d9d2008b7341e
khouloudachour <45214298+khouloud97@users.noreply.github.com>
2023-02-17 17:56:29 +0100

    rm record name

--- sendinblue/wizard/inherit_sms_composer.py
+++ sendinblue/wizard/inherit_sms_composer.py
@@ -49,7 +49,7 @@ class InheritSmsComposer(models.TransientModel):
                 }
                 sms = self.env['mail.message'].sudo().search(
                     [("body", "ilike", self.body), ("message_type", "=", 'comment'),
-                     ('model', "=", "res.partner"),('record_name','like', i_sms.name)])
+                     ('model', "=", "res.partner")])
 
 
                 _logger.info("sms")
